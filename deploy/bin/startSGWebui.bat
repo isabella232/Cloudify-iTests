c7b23f762afd17059dbe23275caa0bcae2147d66
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-10-27 17:25:21 +0000

    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@101810 eb64e737-3616-4df0-8941-5ee2ae88103d

--- deploy/bin/startSGWebui.bat
+++ deploy/bin/startSGWebui.bat
@@ -53,7 +53,7 @@ call ant -buildfile %LOCAL_SGPATH%\bin\run.xml relocate-build -DBUILD_VERSION=%B
 
 @echo starting agent on local machine
 @set LOOKUPGROUPS=sgwebui-cloudify
-@start %LOCAL_SGPATH%\deploy\local-builds\%BUILD_NUMBER%\%BUILD_FOLDER%\bin\gs-agent.bat gsa.global.lus 2 gsa.global.gsm 0 gsa.gsc 0
+@start %LOCAL_SGPATH%\deploy\local-builds\%BUILD_NUMBER%\gigaspaces\bin\gs-agent.bat gsa.global.lus 2 gsa.global.gsm 0 gsa.gsc 0
 @echo starting agent on remote macihne : pc-lab73
 @start %LOCAL_SGPATH%\src\test\webui\resources\psexec.exe \\pc-lab73 -u GSPACES\ca -p password -c -f %LOCAL_SGPATH%\src\test\webui\resources\scripts\start-agent.bat %BUILD_FOLDER% %USER_HOME%
 

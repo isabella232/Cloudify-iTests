731cbc5c8a445a4a8f6ce333e3ee78752e8be993
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-10-27 23:16:26 +0000

    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@101826 eb64e737-3616-4df0-8941-5ee2ae88103d

---
+++ apps/MHEDS/common/src/com/gigaspaces/ps/hibernateimpl/common/ProcessedAccount.java
@@ -0,0 +1,59 @@
+package com.gigaspaces.ps.hibernateimpl.common;
+
+import com.gigaspaces.annotation.pojo.SpaceClass;
+import com.gigaspaces.annotation.pojo.SpaceId;
+import com.gigaspaces.annotation.pojo.SpaceProperty;
+import com.gigaspaces.annotation.pojo.SpaceRouting;
+import com.gigaspaces.annotation.pojo.SpaceProperty.IndexType;
+
+@SpaceClass
+public class ProcessedAccount{
+	private String firstName;
+
+	private String lastName;
+
+	private Integer id;
+
+	// must have empty constructor
+	public ProcessedAccount() {
+	}
+
+	public ProcessedAccount(String firstName, String lastName, Integer id) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.id = id;
+		
+	}
+
+	public String toString() {
+		return "ID:" + id + " firstName:" + firstName + " lastName:" + lastName;
+	}
+
+	@SpaceProperty(index=IndexType.BASIC)
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	@SpaceRouting
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@SpaceId
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+}

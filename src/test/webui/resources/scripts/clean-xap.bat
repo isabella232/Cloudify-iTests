f6ac5f2714b17909d3adfe0d56da4a2d427d8754
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-10-28 23:03:06 +0000

    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@101864 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/webui/resources/scripts/clean-xap.bat
+++ src/test/webui/resources/scripts/clean-xap.bat
@@ -1,4 +1,4 @@
 @set VERSION=%1
 @set MILESTONE=%2
 
-@if exist C:\Users\ca\gigaspaces-clooudify-%VERSION%-%MILESTONE% rmdir C:\Users\ca\gigaspaces-clooudify-%VERSION%-%MILESTONE% /s /q
+@if exist C:\Users\ca\gigaspaces-cloudify-%VERSION%-%MILESTONE% rmdir C:\Users\ca\gigaspaces-cloudify-%VERSION%-%MILESTONE% /s /q

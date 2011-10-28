c09a74c9ad7981f1282112364735daf90c04e790
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-10-28 21:49:21 +0000

    disable test for now. it uses classes compiled with java 6 and including them in the runtime classpath kills SGTest at the nightly runs since it is running with java 5
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@101861 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/InstallAndUninstallServiceTest.java
+++ src/test/cli/cloudify/InstallAndUninstallServiceTest.java
@@ -65,7 +65,7 @@ public class InstallAndUninstallServiceTest extends AbstractCommandTest {
 		testRestApiInstall(DATAGRID_SERVICE_NAME, getUsmServicePath(DATAGRID_FOLDER_NAME));
 	}
 	
-	@Test(timeOut = DEFAULT_TEST_TIMEOUT, groups = "1", enabled = true)
+	@Test(timeOut = DEFAULT_TEST_TIMEOUT, groups = "1", enabled = false)
 	public void testMirrorRecipeInstall() throws IOException, InterruptedException {
 		testRestApiInstall(MIRROR_SERVICE_NAME, getUsmServicePath(MIRROR_SERVICE_FOLDER_NAME));
 	}

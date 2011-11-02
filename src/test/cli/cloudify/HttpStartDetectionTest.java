905118b2cc7a02cdf2fe873fdd71c6395e1e3763
adaml <adaml@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-11-02 10:12:40 +0000

    fixed assertion test
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@102234 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/HttpStartDetectionTest.java
+++ src/test/cli/cloudify/HttpStartDetectionTest.java
@@ -1,9 +1,11 @@
 package test.cli.cloudify;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import org.openspaces.admin.machine.Machine;
 import org.openspaces.admin.pu.DeploymentStatus;
+import org.openspaces.admin.pu.ProcessingUnit;
 import org.testng.annotations.Test;
 
 import framework.tools.SGTestHelper;
@@ -32,7 +34,10 @@ public class HttpStartDetectionTest extends AbstractCommandTest {
 			
 			LogUtils.log("Installing tomcat on port 8081");
 			runCommand(command);
-			assertTrue("tomcat service should not be installed", admin.getProcessingUnits().getProcessingUnit("tomcat").getStatus().equals(DeploymentStatus.BROKEN));
+			assertTrue("tomcat service processing unit does not exist", admin.getProcessingUnits().getProcessingUnit("tomcat") != null);
+			assertTrue("tomcat service should not be installed.", !admin.getProcessingUnits().getProcessingUnit("tomcat").getStatus().equals(DeploymentStatus.INTACT));
+			ProcessingUnit tomcatPu = admin.getProcessingUnits().waitFor("tomcat", Constants.PROCESSINGUNIT_TIMEOUT_SEC, TimeUnit.SECONDS);
+			assertTrue("Expected processing unit instances of tomcat service is not 0", !tomcatPu.waitFor(1, Constants.PROCESSINGUNIT_TIMEOUT_SEC, TimeUnit.SECONDS));
 		
 		} catch (IOException e) {
 			e.printStackTrace();

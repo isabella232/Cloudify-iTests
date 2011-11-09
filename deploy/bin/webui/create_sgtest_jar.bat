9d2569a073ae3d5380cbcdd7df9a74b23487e3e7
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-11-09 15:51:11 +0000

    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@102748 eb64e737-3616-4df0-8941-5ee2ae88103d

--- deploy/bin/webui/create_sgtest_jar.bat
+++ deploy/bin/webui/create_sgtest_jar.bat
@@ -12,6 +12,6 @@ set LOCAL_SGPATH=%USER_HOME%\sgwebui-cloudify
 @copy %SGTEST_CHECKOUT_FOLDER%\src\test\webui\resources\scripts\build.xml %SGTEST_CHECKOUT_FOLDER%
 
 @echo building sgtest.jar...
-call ant -buildfile %LOCAL_SGPATH%\build.xml jar1_6 -DBUILD_VERSION=%BUILD_VERSION% -DBUILD_NUMBER=%BUILD_NUMBER% -Dbuild.folder=%BUILD_FOLDER% -DgsHome.dir=%USER_HOME%\%BUILD_FOLDER%
+call ant -buildfile %SGTEST_CHECKOUT_FOLDER%\build.xml jar1_6 -DBUILD_VERSION=%BUILD_VERSION% -DBUILD_NUMBER=%BUILD_NUMBER% -Dbuild.folder=%BUILD_FOLDER% -DgsHome.dir=%USER_HOME%\%BUILD_FOLDER%
 @echo moving jar to lib folder in local sgtest folder
 @move %SGTEST_CHECKOUT_FOLDER%\output\gs-sgtest.jar %LOCAL_SGPATH%\lib

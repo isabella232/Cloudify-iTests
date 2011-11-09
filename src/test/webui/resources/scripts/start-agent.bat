a6fa1c4de9e7260db676b851d126bddd78861f49
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-11-09 21:49:04 +0000

    fix scripts
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@102763 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/webui/resources/scripts/start-agent.bat
+++ src/test/webui/resources/scripts/start-agent.bat
@@ -1,16 +1,17 @@
-@set REMOTE_BUILD_DIR=\\tarzan\users\ca\Eli_Test
-
-@set BUILD_FOLDER=%1
-@set USER_HOME=%2
+@set VERSION=%1
+@set MILESTONE=%2
+@set BUILD_VERSION=%3
+@set BUILD_NMBER=%4 
+@set USER_HOME=C:\Users\ca
+set REMOTE_BUILD_DIR=\\tarzan\builds\%VERSION%\%BUILD_NUMBER%
 
 @echo retrieving build from tarzan...
-@mkdir %USER_HOME%\%BUILD_FOLDER%
-@xcopy /e %REMOTE_BUILD_DIR%\%BUILD_FOLDER% %USER_HOME%\%BUILD_FOLDER%
+xcopy %REMOTE_BUILD_DIR%\cloudify\1.5\gigaspaces-cloudify-%VERSION%-%MILESTONE%-b%BUILD_VERSION%.zip %USER_HOME%
+@extracting build file to local-builds folder
+@7z e %USER_HOME%\gigaspaces-cloudify-%VERSION%-%MILESTONE%-b%BUILD_VERSION%.zip %USER_HOME%
+@del %USER_HOME%\gigaspaces-cloudify-%VERSION%-%MILESTONE%-b%BUILD_VERSION%.zip
 
 @echo starting agent
 @set LOOKUPGROUPS=sgwebui-cloudify
+@set BUILD_FOLDER=gigaspaces-cloudify-%VERSION%-%MILESTONE%
 @start %USER_HOME%\%BUILD_FOLDER%\bin\gs-agent.bat gsa.global.lus 2 gsa.global.gsm 0 gsa.gsc 0
-
-@echo deleting build from tarzan\users\ca
-@rmdir %REMOTE_BUILD_DIR%\%BUILD_FOLDER% /s /q
-@taskkill /im conhost.exe /im cmd.exe /F

c3a1e2dbac718361689849b0676a1d7252b94cf6
barakme <barakme@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-11-30 12:19:09 +0000

    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@103931 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/usm/USMTestUtils.java
+++ src/test/usm/USMTestUtils.java
@@ -79,7 +79,7 @@ public class USMTestUtils {
 
 	private static Service packAndDeploy(final String folderPath, final String serviceFileName, Service service, String absolutePuName) throws IOException,
 			PackagingException {
-		final File puZipFile = Packager.pack(new File(folderPath));
+		final File puZipFile = Packager.pack(new File(folderPath, serviceFileName));
 
     	final ProcessingUnitDeployment processingUnitDeployment = new ProcessingUnitDeployment(puZipFile).numberOfInstances(service.getNumInstances()).name(absolutePuName);    	
     	deploy(processingUnitDeployment, puZipFile, serviceFileName);

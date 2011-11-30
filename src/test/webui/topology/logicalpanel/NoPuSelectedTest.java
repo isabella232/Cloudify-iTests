82200a5c6f42e1ee8dec2e8599cb7a95a5a3ec48
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-11-30 11:55:27 +0000

    GS-9726 added test
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@103923 eb64e737-3616-4df0-8941-5ee2ae88103d

---
+++ src/test/webui/topology/logicalpanel/NoPuSelectedTest.java
@@ -0,0 +1,85 @@
+package test.webui.topology.logicalpanel;
+
+import org.openspaces.admin.gsa.GridServiceAgent;
+import org.openspaces.admin.gsm.GridServiceManager;
+import org.openspaces.admin.machine.Machine;
+import org.openspaces.admin.pu.DeploymentStatus;
+import org.openspaces.admin.pu.ProcessingUnit;
+import org.openspaces.admin.pu.ProcessingUnitInstance;
+import org.openspaces.admin.space.SpaceDeployment;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import test.webui.AbstractSeleniumTest;
+import test.webui.objects.LoginPage;
+import test.webui.objects.topology.ApplicationMap;
+import test.webui.objects.topology.LogicalPanel;
+import test.webui.objects.topology.TopologyTab;
+import framework.utils.AdminUtils;
+import framework.utils.LogUtils;
+import framework.utils.ProcessingUnitUtils;
+
+public class NoPuSelectedTest extends AbstractSeleniumTest {
+	
+	Machine machineA;
+	ProcessingUnit Pu;
+	
+	@BeforeMethod(alwaysRun = true)
+	public void startSetUp() {	
+		LogUtils.log("waiting for 1 machine");
+		admin.getMachines().waitFor(1);
+
+		LogUtils.log("waiting for 1 GSA");
+		admin.getGridServiceAgents().waitFor(1);
+
+		GridServiceAgent[] agents = admin.getGridServiceAgents().getAgents();
+		GridServiceAgent gsaA = agents[0];
+
+		machineA = gsaA.getMachine();
+
+		LogUtils.log("starting: 1 GSM and 2 GSC's on 1 machine");
+		GridServiceManager gsmA = AdminUtils.loadGSM(machineA); 
+		AdminUtils.loadGSCs(machineA, 2);
+		
+		LogUtils.log("deploying processing unit...");
+		SpaceDeployment deployment = new SpaceDeployment("Test").partitioned(1, 1).maxInstancesPerVM(1).setContextProperty("com.gs.application", "MyApp");
+		Pu = gsmA.deploy(deployment.setContextProperty(APPLICATION_CONTEXT_PROPERY, "App"));
+		ProcessingUnitUtils.waitForDeploymentStatus(Pu, DeploymentStatus.INTACT);
+		
+		LogUtils.log("deploying processing unit...");
+		deployment = new SpaceDeployment("Test1").partitioned(1, 1).maxInstancesPerVM(1).setContextProperty("com.gs.application", "MyApp");
+		Pu = gsmA.deploy(deployment.setContextProperty(APPLICATION_CONTEXT_PROPERY, "App"));
+		ProcessingUnitUtils.waitForDeploymentStatus(Pu, DeploymentStatus.INTACT);
+	}
+	
+	@Test(timeOut = DEFAULT_TEST_TIMEOUT)
+	public void goTologsTest() throws InterruptedException {
+		
+		// get new login page
+		LoginPage loginPage = getLoginPage();
+		
+		// get new topology tab
+		TopologyTab topologyTab = loginPage.login().switchToTopology();
+		
+		topologyTab.getApplicationMap().selectApplication("App");
+		
+		LogicalPanel logicalPanel = topologyTab.getTopologySubPanel().swtichToLogicalPanel();
+		
+		ApplicationMap applicationMap = topologyTab.getApplicationMap();
+		
+		applicationMap.deselectAllNodes();
+		
+		ProcessingUnit test = admin.getProcessingUnits().getProcessingUnit("Test");
+		ProcessingUnit test1 = admin.getProcessingUnits().getProcessingUnit("Test1");
+		
+		for (ProcessingUnitInstance puInstance : test.getInstances()) {
+			assertTrue(logicalPanel.getProcessingUnitInstance(puInstance.getProcessingUnitInstanceName()) != null);
+		}
+		
+		for (ProcessingUnitInstance puInstance : test1.getInstances()) {
+			assertTrue(logicalPanel.getProcessingUnitInstance(puInstance.getProcessingUnitInstanceName()) != null);
+		}
+		
+	}
+
+}

c55f667d51e64c0f50006bdbb8855ae24c0b8c77
adaml <adaml@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-12-08 13:33:59 +0000

    added operating system check
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@104513 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/PetClinicApplicationTest.java
+++ src/test/cli/cloudify/PetClinicApplicationTest.java
@@ -1,6 +1,8 @@
 package test.cli.cloudify;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -8,23 +10,53 @@ import org.testng.annotations.Test;
 import com.gigaspaces.cloudify.dsl.utils.ServiceUtils;
 import com.gigaspaces.cloudify.shell.commands.CLIException;
 
+import framework.utils.AssertUtils;
 import framework.utils.LogUtils;
 import framework.utils.ScriptUtils;
 
 public class PetClinicApplicationTest extends AbstractLocalCloudTest {
-	
+
 	private static final String PETCLINIC_APPLICTION_NAME = "petclinic-mongo";
-//	Machine[] machines;
-	
+	//	Machine[] machines;
+
 	@Override
 	@BeforeMethod
 	public void beforeTest() {
+		try {
+			checkOSSupportForMongo();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		super.beforeTest();	
+	}
+
+	private void checkOSSupportForMongo() throws IOException, InterruptedException {
+		if (!isWindows()){
+			String cmd = "uname -a";
+			Runtime run = Runtime.getRuntime();
+			Process pr = run.exec(cmd);
+			pr.waitFor();
+			BufferedReader buf = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+			String out = "";
+			String line = "";
+			while ((line=buf.readLine())!=null) {
+				out += line + System.getProperty("line.separator");
+			}
+			if (out.contains("2007") && out.contains("linux")){
+				AssertUtils.AssertFail("Mongo service is not supported in this version of linux");
+			}
+		}
+	}
+
+	private static boolean isWindows() {
+		return System.getProperty("os.name").toLowerCase().startsWith("win");
 	}
 	
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, groups = "1", enabled = true)
 	public void testPetClinincApplication() throws CLIException {
-		
+
 		String applicationDir = ScriptUtils.getBuildPath() + "/examples/petclinic";
 		String command = "connect " + this.restUrl + ";" + "install-application " + "--verbose -timeout 25 " + applicationDir;
 		try {
@@ -32,16 +64,16 @@ public class PetClinicApplicationTest extends AbstractLocalCloudTest {
 			int currentNumberOfInstances;
 			currentNumberOfInstances = getProcessingUnitInstanceCount(ServiceUtils.getAbsolutePUName(PETCLINIC_APPLICTION_NAME, "mongod"));
 			assertTrue("Expected 2 PU instances. Actual number of instances is " + currentNumberOfInstances, currentNumberOfInstances == 2);
-			
+
 			currentNumberOfInstances = getProcessingUnitInstanceCount(ServiceUtils.getAbsolutePUName(PETCLINIC_APPLICTION_NAME, "mongo-cfg"));
 			assertTrue("Expected 1 PU instances. Actual number of instances is " + currentNumberOfInstances, currentNumberOfInstances == 1);
-			
+
 			currentNumberOfInstances = getProcessingUnitInstanceCount(ServiceUtils.getAbsolutePUName(PETCLINIC_APPLICTION_NAME, "mongos"));
 			assertTrue("Expected 1 PU instances. Actual number of instances is " + currentNumberOfInstances,currentNumberOfInstances == 1);
 
 			currentNumberOfInstances = getProcessingUnitInstanceCount(ServiceUtils.getAbsolutePUName(PETCLINIC_APPLICTION_NAME, "tomcat"));
 			assertTrue("Expected 1 PU instances. Actual number of instances is " + currentNumberOfInstances, currentNumberOfInstances == 1);
-			
+
 		} catch (IOException e) {
 			LogUtils.log("bootstrap-localcloud failed", e);
 			afterTest();
@@ -49,7 +81,7 @@ public class PetClinicApplicationTest extends AbstractLocalCloudTest {
 			LogUtils.log("bootstrap-localcloud failed", e);
 			afterTest();
 		}
-		
+
 	}
 
 }

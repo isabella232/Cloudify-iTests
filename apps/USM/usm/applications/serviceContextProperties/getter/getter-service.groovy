235fa3da1b9afcebe98f9ca76b06023ea599be9c
gal <gal@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-12-11 16:49:38 +0000

    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@104648 eb64e737-3616-4df0-8941-5ee2ae88103d

--- apps/USM/usm/applications/serviceContextProperties/getter/getter-service.groovy
+++ apps/USM/usm/applications/serviceContextProperties/getter/getter-service.groovy
@@ -1,6 +1,7 @@
 service {
 	name "getter"
 	icon "icon.png"
+	type "WEB"
 	lifecycle {
 		
 				//init { println "This is the init event" }
@@ -19,60 +20,7 @@ service {
 				
 			}
 		
-			plugins = [
-				plugin {
-					name "jmx"
-					className "com.gigaspaces.cloudify.usm.jmx.JmxMonitor"
-					config ([
-		
-								"Details" : [
-									"org.openspaces.usm.examples.simplejavaprocess:type=SimpleBlockingJavaProcess",
-									"Details"
-								],
-								"Counter" : [
-									"org.openspaces.usm.examples.simplejavaprocess:type=SimpleBlockingJavaProcess",
-									"Counter"
-								],
-								"Type" : [
-									"org.openspaces.usm.examples.simplejavaprocess:type=SimpleBlockingJavaProcess",
-									"Type"
-								],
-								port : 9999
-							])
-				}
-			]
-		
-			userInterface {
-				metricGroups = [
-					metricGroup{
-						name = "process"
-						metrics = ["cpu", "memory"]
-					},
-					metricGroup{
-						name = "space"
-						metrics = ["reads", "writes"]
-					}
-				]
-				widgetGroups = [
-					widgetGroup{
-						name  ="cpu"
-						widgets = [
-							balanceGauge{metric = "cpu"},
-							barLineChart{metric = "cpu"}
-						]
-					},
-					widgetGroup {
-						name = "memory"
-						widgets = [
-							balanceGauge { metric = "memory" },
-							barLineChart{ metric = "memory"
-								axisYUnit Unit.PERCENTAGE
-		
-								}
-						]
-					}
-				]
-			}
+			
 	customCommands ([
 	            "cmd1" : {println context.properties.application["myKey"]},
 				"cmd2" : "get_application_context_property.groovy",
--- apps/USM/usm/applications/serviceContextProperties/setter/setter-service.groovy
+++ apps/USM/usm/applications/serviceContextProperties/setter/setter-service.groovy
@@ -1,6 +1,7 @@
 service {
 	name "setter"
 	icon "icon.png"
+	type "WEB"
 	lifecycle {
 		
 				//init { println "This is the init event" }
@@ -19,60 +20,7 @@ service {
 				shutdown {println "This is the shutdown event" }
 			}
 		
-			plugins = [
-				plugin {
-					name "jmx"
-					className "com.gigaspaces.cloudify.usm.jmx.JmxMonitor"
-					config ([
-		
-								"Details" : [
-									"org.openspaces.usm.examples.simplejavaprocess:type=SimpleBlockingJavaProcess",
-									"Details"
-								],
-								"Counter" : [
-									"org.openspaces.usm.examples.simplejavaprocess:type=SimpleBlockingJavaProcess",
-									"Counter"
-								],
-								"Type" : [
-									"org.openspaces.usm.examples.simplejavaprocess:type=SimpleBlockingJavaProcess",
-									"Type"
-								],
-								port : 9999
-							])
-				}
-			]
-		
-			userInterface {
-				metricGroups = [
-					metricGroup{
-						name = "process"
-						metrics = ["cpu", "memory"]
-					},
-					metricGroup{
-						name = "space"
-						metrics = ["reads", "writes"]
-					}
-				]
-				widgetGroups = [
-					widgetGroup{
-						name  ="cpu"
-						widgets = [
-							balanceGauge{metric = "cpu"},
-							barLineChart{metric = "cpu"}
-						]
-					},
-					widgetGroup {
-						name = "memory"
-						widgets = [
-							balanceGauge { metric = "memory" },
-							barLineChart{ metric = "memory"
-								axisYUnit Unit.PERCENTAGE
-		
-								}
-						]
-					}
-				]
-			}
+			
 	customCommands ([
 	            "cmd1" : {context.properties.application["myKey"] = "myValue"},
 				"cmd2" : "set_application_context_property.groovy"

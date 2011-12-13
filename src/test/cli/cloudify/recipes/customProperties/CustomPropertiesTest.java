3f2e4deda84535773ecea6cb503dc3f2007d12a1
gal <gal@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-12-13 17:23:18 +0000

    custom properties and recipe heritage tests
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@104841 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/recipes/customProperties/CustomPropertiesTest.java
+++ src/test/cli/cloudify/recipes/customProperties/CustomPropertiesTest.java
@@ -1,4 +1,4 @@
-package test.cli.cloudify.recipes.contextProperties;
+package test.cli.cloudify.recipes.customProperties;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -7,6 +7,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openspaces.admin.pu.ProcessingUnit;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -14,16 +15,19 @@ import test.cli.cloudify.AbstractSingleBootstrapTest;
 import test.cli.cloudify.CommandTestUtils;
 import test.usm.USMTestUtils;
 
+import com.gigaspaces.cloudify.dsl.Application;
+import com.gigaspaces.cloudify.dsl.Service;
 import com.gigaspaces.cloudify.dsl.internal.ServiceReader;
 import com.gigaspaces.cloudify.dsl.internal.packaging.PackagingException;
 import com.gigaspaces.cloudify.dsl.utils.ServiceUtils;
 
-public class contextPropertiesAsMapTest extends AbstractSingleBootstrapTest {
+public class CustomPropertiesTest extends AbstractSingleBootstrapTest {
 	
 	private final String APPLICAION_DIR_PATH = CommandTestUtils
 									.getPath("apps/USM/usm/applications/serviceContextProperties");
-	private int totalInstancesService2;
-
+	
+	private Application app;
+	
 	@Override
 	@BeforeClass
 	public void beforeClass() throws FileNotFoundException, PackagingException, IOException, InterruptedException{
@@ -40,7 +44,6 @@ public class contextPropertiesAsMapTest extends AbstractSingleBootstrapTest {
 		assertNotNull("applications was not installed", admin.getApplications().getApplication("simpleCustomCommandsMultipleInstances"));
 		assertTrue("USM Service State is NOT RUNNING", USMTestUtils.waitForPuRunningState(absolutePUNameSimple1, 60, TimeUnit.SECONDS, admin));
 		assertTrue("USM Service State is NOT RUNNING", USMTestUtils.waitForPuRunningState(absolutePUNameSimple2, 60, TimeUnit.SECONDS, admin));
-		totalInstancesService2 = pu2.getTotalNumberOfInstances();
 	}
 
 	@Override
@@ -51,18 +54,64 @@ public class contextPropertiesAsMapTest extends AbstractSingleBootstrapTest {
 				";uninstall-application --verbose serviceContextProperties");	
 		super.afterClass();
 	}
+	@Override
+	@AfterTest
+	public void afterTest(){
+		for(Service service : app.getServices())
+			service.getCustomProperties().clear();
+				
+		super.afterTest();
+	}
 //////////////////////////////////////////////////////////////////////////////////////////////////////////	
 		
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT , groups="1", enabled = false)
 	public void testSimpleApplicationSetContext() throws Exception {
 		
 		runCommand("connect " + this.restUrl + ";use-application serviceContextProperties" 
-				+ "; invoke setter cmd1");
+				+ "; invoke setter setApp");
 		
 		String simpleGet = runCommand("connect " + this.restUrl + ";use-application serviceContextProperties" 
-				+ "; invoke getter cmd1");
+				+ "; invoke getter getApp");
 		
-		assertTrue("", simpleGet.contains("myValue"));
+		assertTrue("no match", simpleGet.contains("myValue"));
+	}
+	
+	@Test(timeOut = DEFAULT_TEST_TIMEOUT , groups="1", enabled = false)
+	public void testApplicationSetContextCustomParams() throws Exception {
+		
+		runCommand("connect " + this.restUrl + ";use-application serviceContextProperties" 
+				+ "; invoke setter setAppCustom ['x=myKey1' 'y=myValue1']");
+		
+		String simpleGet = runCommand("connect " + this.restUrl + ";use-application serviceContextProperties" 
+				+ "; invoke getter getAppCustom ['x=myKey1']");
+		
+		assertTrue("no match", simpleGet.contains("myValue1"));
+	}
+	
+	@Test(timeOut = DEFAULT_TEST_TIMEOUT , groups="1", enabled = false)
+	public void testServiceSetContext() throws Exception {
+		
+		runCommand("connect " + this.restUrl + ";use-application serviceContextProperties" 
+				+ "; invoke setter setService");
+		
+		String crossServiceGet = runCommand("connect " + this.restUrl + ";use-application serviceContextProperties" 
+				+ "; invoke getter getService");
+		
+		String serviceGet = runCommand("connect " + this.restUrl + ";use-application serviceContextProperties" 
+				+ "; invoke setter getService");
+		
+		String appGet = runCommand("connect " + this.restUrl + ";use-application serviceContextProperties" 
+				+ "; invoke getter getApp");
+		
+		String getInstance1 = runCommand("connect " + this.restUrl + ";use-application serviceContextProperties" 
+				+ "; invoke -instanceId=1 setter getApp");
+		String getInstance2 = runCommand("connect " + this.restUrl + ";use-application serviceContextProperties" 
+				+ "; invoke -instanceId=2 setter getApp");
+		
+		assertTrue("setService shuold be visible to the same service", serviceGet.contains("myValue"));
+		assertTrue("setService shuold be visible to the application", appGet.contains("myValue"));
+		assertTrue("setService shuold be visible to all service instances", getInstance1.contains("myValue") && getInstance2.contains("myValue"));
+		assertTrue("setService shuold not be visible to a different service", !crossServiceGet.contains("myValue"));
 	}
 	
 ////////////////////////////////////////////////////////////////////////////////////////////////////////	
@@ -72,7 +121,7 @@ public class contextPropertiesAsMapTest extends AbstractSingleBootstrapTest {
 	private void installApplication() throws FileNotFoundException,
 		PackagingException, IOException, InterruptedException {
 		File applicationDir = new File(APPLICAION_DIR_PATH);
-		ServiceReader.getApplicationFromFile(applicationDir).getApplication();
+		app = ServiceReader.getApplicationFromFile(applicationDir).getApplication();
 		
 		runCommand("connect " + this.restUrl + ";install-application --verbose " + APPLICAION_DIR_PATH);
 	}

310f8b76346149239368e907745e9ec0f1f06602
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-12-14 21:40:14 +0000

    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@104930 eb64e737-3616-4df0-8941-5ee2ae88103d

--- deploy/bin/webui/download_processing_units.bat
+++ deploy/bin/webui/download_processing_units.bat
@@ -5,7 +5,7 @@
 if exist %LOCAL_SGPATH%\apps\archives rmdir %LOCAL_SGPATH%\apps /s /q
 
 @echo copying new apps to local sgtest folder...
-xcopy %SGTEST_CHECKOUT_FOLDER%\apps\archives %LOCAL_SGPATH%\apps /e /i
+xcopy %SGTEST_CHECKOUT_FOLDER%\apps %LOCAL_SGPATH%\apps /e /i
 
 @echo deleting current selenium jar from local sgtest folder...
 del %LOCAL_SGPATH%\lib\selenium\selenium-java-*.jar

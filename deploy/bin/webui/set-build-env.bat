9ab8326357cd9f36c9e1c66af1b553c1ee84144a
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-12-14 19:20:58 +0000

    fix script to conform to new builds hierarchy
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@104921 eb64e737-3616-4df0-8941-5ee2ae88103d

--- deploy/bin/webui/set-build-env.bat
+++ deploy/bin/webui/set-build-env.bat
@@ -3,5 +3,5 @@ set BUILD_FOLDER=gigaspaces-xap-premium-%VERSION%-%MILESTONE%
 set BUILD_LOCATION=%USER_HOME%\sgwebui-xap\deploy\local-builds\%BUILD_NUMBER%\%BUILD_FOLDER%
 set PRODUCT=xap
 set LOCAL_SGPATH=%USER_HOME%\sgwebui-xap
-set REMOTE_BUILD_DIR=\\tarzan\builds\%VERSION%\%BUILD_NUMBER%
+set REMOTE_BUILD_DIR=\\tarzan\builds\cloudify\%VERSION%\%BUILD_NUMBER%
 set BUILD_FOLDER=gigaspaces-xap-premium-%VERSION%-%MILESTONE%

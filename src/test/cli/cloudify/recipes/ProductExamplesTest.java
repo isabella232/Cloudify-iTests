7b6c335783d576c4db4ad1af7cad45488019375c
gal <gal@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-12-20 17:15:39 +0000

    tests that install travel and petclinic from the examples folder
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@105348 eb64e737-3616-4df0-8941-5ee2ae88103d

---
+++ src/test/cli/cloudify/recipes/ProductExamplesTest.java
@@ -0,0 +1,28 @@
+package test.cli.cloudify.recipes;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+import test.cli.cloudify.AbstractCommandTest;
+import framework.utils.ScriptUtils;
+
+public class ProductExamplesTest extends AbstractCommandTest{
+	private String examplesDirPath = ScriptUtils.getBuildPath() + "/examples";
+
+	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, groups = "1", enabled = true)
+	public void installTravel() throws IOException, InterruptedException{
+		super.beforeTest();
+		String travelDirPath = examplesDirPath + "/travel";
+		String cliOutput = runCommand("connect " + restUrl + ";install-application --verbose " + travelDirPath);
+		assertTrue("travel app couln't be installed", cliOutput.toLowerCase().contains("application travel installed successfully"));
+	}
+	
+	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, groups = "1", enabled = true)
+	public void installPetclinic() throws IOException, InterruptedException{
+		super.beforeTest();
+		String petclinicDirPath = examplesDirPath + "/petclinic";
+		String cliOutput = runCommand("connect " + restUrl + ";install-application --verbose " + petclinicDirPath);
+		assertTrue("petclinic app couln't be installed", cliOutput.toLowerCase().contains("application petclinic-mongo installed successfully"));
+	}
+}

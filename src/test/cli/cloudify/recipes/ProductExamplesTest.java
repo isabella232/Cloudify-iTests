60faf99fac6245d829a03a5b200b0ac9e0ba5dca
adaml <adaml@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-12-25 15:28:13 +0000

    Changed test to extend abstractLocalCloudTest Class
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@105674 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/recipes/ProductExamplesTest.java
+++ src/test/cli/cloudify/recipes/ProductExamplesTest.java
@@ -4,25 +4,23 @@ import java.io.IOException;
 
 import org.testng.annotations.Test;
 
-import test.cli.cloudify.AbstractCommandTest;
+import test.cli.cloudify.AbstractLocalCloudTest;
 import framework.utils.ScriptUtils;
 
-public class ProductExamplesTest extends AbstractCommandTest{
+public class ProductExamplesTest extends AbstractLocalCloudTest{
 	private String examplesDirPath = ScriptUtils.getBuildPath() + "/examples";
 
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, groups = "1", enabled = true)
 	public void installTravel() throws IOException, InterruptedException{
-		super.beforeTest();
 		String travelDirPath = examplesDirPath + "/travel";
-		String cliOutput = runCommand("bootstrap-localcloud;install-application --verbose " + travelDirPath);
+		String cliOutput = runCommand("connect " + this.restUrl + ";install-application --verbose " + travelDirPath);
 		assertTrue("travel app couln't be installed", cliOutput.toLowerCase().contains("application travel installed successfully"));
 	}
 	
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, groups = "1", enabled = true)
 	public void installPetclinic() throws IOException, InterruptedException{
-		super.beforeTest();
 		String petclinicDirPath = examplesDirPath + "/petclinic";
-		String cliOutput = runCommand("bootstrap-localcloud;install-application --verbose " + petclinicDirPath);
+		String cliOutput = runCommand("connect " + this.restUrl + ";install-application --verbose " + petclinicDirPath);
 		assertTrue("petclinic app couln't be installed", cliOutput.toLowerCase().contains("application petclinic-mongo installed successfully"));
 	}
 }

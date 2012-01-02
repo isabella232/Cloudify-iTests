5ef0967dac6b14b886662006dd805e1443d4a5a8
kobi <kobi@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-01-02 13:27:46 +0000

    extends AbstractLocalCloud
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@106262 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/ConfigSlurperTest.java
+++ src/test/cli/cloudify/ConfigSlurperTest.java
@@ -13,38 +13,32 @@ import test.usm.USMTestUtils;
 
 import com.gigaspaces.cloudify.dsl.utils.ServiceUtils;
 
-import framework.tools.SGTestHelper;
+public class ConfigSlurperTest extends AbstractLocalCloudTest {
 
-public class ConfigSlurperTest extends AbstractCommandTest {
-	
-	@Test
-	public void test() throws IOException, InterruptedException{
-		String serviceDir = SGTestHelper.getSGTestRootDir().replace("\\", "/") + "/apps/USM/usm/slurper";
-		String command = "connect " + restUrl + ";install-service --verbose " + serviceDir + ";exit";
+    @Test
+    public void test() throws IOException, InterruptedException {
+        final String serviceDir = CommandTestUtils.getPath("apps/USM/usm/slurper");
+        String command = "connect " + restUrl + ";install-service --verbose " + serviceDir + ";exit";
 
-			runCommand(command);
-			String absolutePuName = ServiceUtils.getAbsolutePUName(DEFAULT_APPLICTION_NAME, "slurper");
-			ProcessingUnit pu = admin.getProcessingUnits().waitFor(absolutePuName);
-			assertTrue("USM Service state is NOT RUNNING", USMTestUtils.waitForPuRunningState(absolutePuName, 60, TimeUnit.SECONDS, admin));
-			Map<String, ServiceDetails> serviceDetails = pu.getInstances()[0].getServiceDetailsByServiceId();
-			Map<String, Object> attributes = serviceDetails.get("USM").getAttributes();
-			Assert.assertTrue(attributes.get("icon").equals("icon.png"));
-			Assert.assertTrue(attributes.get("jmx-port").toString().equals("9999"));
-			Assert.assertTrue(attributes.get("name").equals("slurper"));
-			Assert.assertTrue(attributes.get("type").equals("WEB_SERVER"));
-			Assert.assertTrue(attributes.get("tripletype").equals("WEB_SERVERWEB_SERVERWEB_SERVER"));
-			Assert.assertTrue(attributes.get("placeholderProp").toString().equals("string"));
-			
-			if(admin.getMachines().getMachines()[0].getOperatingSystem().getDetails().getName().equals("Linux"))
-				Assert.assertTrue(attributes.get("systemProp").toString().equals(":"));
-			else
-				Assert.assertTrue(attributes.get("systemProp").toString().equals(";"));
-			
-			
-			command = "connect " + restUrl + ";" + "uninstall-service slurper;exit";
-			runCommand(command);
+        runCommand(command);
+        String absolutePuName = ServiceUtils.getAbsolutePUName(DEFAULT_APPLICTION_NAME, "slurper");
+        ProcessingUnit pu = admin.getProcessingUnits().waitFor(absolutePuName);
+        assertTrue("USM Service state is NOT RUNNING", USMTestUtils.waitForPuRunningState(absolutePuName, 60, TimeUnit.SECONDS, admin));
+        Map<String, ServiceDetails> serviceDetails = pu.getInstances()[0].getServiceDetailsByServiceId();
+        Map<String, Object> attributes = serviceDetails.get("USM").getAttributes();
+        Assert.assertTrue(attributes.get("icon").equals("icon.png"));
+        Assert.assertTrue(attributes.get("jmx-port").toString().equals("9999"));
+        Assert.assertTrue(attributes.get("name").equals("slurper"));
+        Assert.assertTrue(attributes.get("type").equals("WEB_SERVER"));
+        Assert.assertTrue(attributes.get("tripletype").equals("WEB_SERVERWEB_SERVERWEB_SERVER"));
+        Assert.assertTrue(attributes.get("placeholderProp").toString().equals("string"));
 
-	
+        if (admin.getMachines().getMachines()[0].getOperatingSystem().getDetails().getName().equals("Linux"))
+            Assert.assertTrue(attributes.get("systemProp").toString().equals(":"));
+        else
+            Assert.assertTrue(attributes.get("systemProp").toString().equals(";"));
 
-	}
+        command = "connect " + restUrl + ";" + "uninstall-service slurper;exit";
+        runCommand(command);
+    }
 }

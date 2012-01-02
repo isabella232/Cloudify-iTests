8386d0f3b87777cd6116f8c7379ff8ae9ee5c1a7
kobi <kobi@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-01-02 13:49:16 +0000

    extends AbstractLocalCloud
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@106265 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/recipes/CyclicDependencyTest.java
+++ src/test/cli/cloudify/recipes/CyclicDependencyTest.java
@@ -10,65 +10,55 @@ import com.gigaspaces.cloudify.dsl.utils.ServiceUtils;
 
 import framework.utils.LogUtils;
 
-import test.cli.cloudify.AbstractCommandTest;
+import test.cli.cloudify.AbstractLocalCloudTest;
 import test.cli.cloudify.CommandTestUtils;
 
-public class CyclicDependencyTest extends AbstractCommandTest {
+public class CyclicDependencyTest extends AbstractLocalCloudTest{
+
+    private void dependencyTest(String ApplicationName) throws IOException, InterruptedException {
+        String path = CommandTestUtils.getPath("apps/cloudify/recipes/" + ApplicationName);
+        CommandTestUtils.runCommandExpectedFail("connect " + this.restUrl + ";install-application " + path + ";exit");
+
+        GridServiceContainer[] gscs = admin.getGridServiceContainers().getContainers();
+        boolean foundStringInGscsLogs = false;
+
+        for (int i = 0; i < gscs.length && !foundStringInGscsLogs; i++) {
+            try {
+                LogUtils.repetativeScanContainerLogsFor(gscs[i], "contains one or more cycles", DEFAULT_TEST_TIMEOUT / gscs.length);
+                foundStringInGscsLogs = true;
+            } catch (AssertionError e) {
+                //do nothing could be found in the next gsc log
+            }
+        }
+        if (!foundStringInGscsLogs)
+            Assert.fail("none of the gscs has the right exception");
+    }
+
+    @Test(timeOut = DEFAULT_TEST_TIMEOUT, groups = "1", enabled = true)
+    public void complexTest() throws IOException, InterruptedException {
+        dependencyTest("complexCycle");
+    }
+
+    @Test(timeOut = DEFAULT_TEST_TIMEOUT, groups = "1", enabled = true)
+    public void simpleTest() throws IOException, InterruptedException {
+        dependencyTest("cycle");
+    }
 
 
-	/**
-	 * 
-	 */
-	public CyclicDependencyTest() {}
+    @Test(timeOut = DEFAULT_TEST_TIMEOUT, groups = "1", enabled = true)
+    public void diamondTest() throws IOException, InterruptedException {
 
+        String path = CommandTestUtils.getPath("apps/cloudify/recipes/diamond");
 
+        //this app is only a mock so the command to install it is not successful
+        CommandTestUtils.runCommand("connect " + this.restUrl + ";install-application " + path + ";exit");
 
+        admin.getZones().waitFor(ServiceUtils.getAbsolutePUName("diamond", "D")).getGridServiceContainers().waitFor(1);
 
-	private void dependencyTest(String ApplicationName) throws IOException, InterruptedException{
-
-		String path = CommandTestUtils.getPath("apps/cloudify/recipes/" + ApplicationName);
-		CommandTestUtils.runCommandExpectedFail("connect " + this.restUrl + ";install-application " + path + ";exit");
-
-		GridServiceContainer [] gscs = admin.getGridServiceContainers().getContainers();
-		boolean foundStringInGscsLogs = false;
-
-		for (int i = 0; i < gscs.length && !foundStringInGscsLogs; i++) {
-			try {
-				LogUtils.repetativeScanContainerLogsFor(gscs[i], "contains one or more cycles", DEFAULT_TEST_TIMEOUT / gscs.length);
-				foundStringInGscsLogs = true;
-			} catch (AssertionError e) {
-				//do nothing could be found in the next gsc log
-			}
-		}
-		if(!foundStringInGscsLogs)
-			Assert.fail("none of the gscs has the right exception");
-	}
-
-	@Test(timeOut = DEFAULT_TEST_TIMEOUT, groups = "1", enabled = true)
-	public void complexTest() throws IOException, InterruptedException{
-		dependencyTest("complexCycle");
-	}
-
-	@Test(timeOut = DEFAULT_TEST_TIMEOUT, groups = "1", enabled = true)
-	public void simpleTest() throws IOException, InterruptedException{
-		dependencyTest("cycle");
-	}
-
-
-	@Test(timeOut = DEFAULT_TEST_TIMEOUT, groups = "1", enabled = true)
-	public void diamondTest() throws IOException, InterruptedException{
-
-		String path = CommandTestUtils.getPath("apps/cloudify/recipes/diamond");
-		
-		//this app is only a mock so the command to install it is not successful
-		CommandTestUtils.runCommand("connect " + this.restUrl + ";install-application " + path + ";exit");
-
-		admin.getZones().waitFor(ServiceUtils.getAbsolutePUName("diamond", "D")).getGridServiceContainers().waitFor(1);
-
-		GridServiceContainer [] gscs = admin.getGridServiceContainers().getContainers();
-		for (GridServiceContainer gsc : gscs) {
-				if(gsc.getZones().containsKey("diamond.D"));
-				LogUtils.scanContainerLogsFor(gsc, "D1 PreStart completed successfully");
-		}
-	}
+        GridServiceContainer[] gscs = admin.getGridServiceContainers().getContainers();
+        for (GridServiceContainer gsc : gscs) {
+            if (gsc.getZones().containsKey("diamond.D")) ;
+            LogUtils.scanContainerLogsFor(gsc, "D1 PreStart completed successfully");
+        }
+    }
 }

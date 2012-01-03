6999415ae4428450f243680377c5dfd700eadc50
gal <gal@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-01-03 18:10:40 +0000

    enabled custom exception command
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@106515 eb64e737-3616-4df0-8941-5ee2ae88103d

--- apps/USM/usm/simpleCustomCommandsMultipleInstances/simpleCustomCommandsMultipleInstances-service.groovy
+++ apps/USM/usm/simpleCustomCommandsMultipleInstances/simpleCustomCommandsMultipleInstances-service.groovy
@@ -1,9 +1,7 @@
-
-//TODO: uncomment as soon as the dependency bug is resolved
-//import test.cli.cloudify.MyException;
+import test.cli.cloudify.MyException;
 
 service {
-	numInstances 4
+	numInstances 1
 	name "simpleCustomCommandsMultipleInstances"
 
 	
@@ -14,8 +12,8 @@ service {
 	customCommands ([
 		"print" : {println "This is the print custom command"},
 		"params" : {x, y -> return("this is the custom parameters command. expecting 123: "+1+x+y)},
-		//TODO: uncomment as soon as the dependency bug is resolved
-		//"exception" : { throw new MyException("This is an error test")},
+		
+		"exception" : { throw new MyException("This is an error test")},
 		"runScript" : "add.groovy",
 		"context" : {"Service Dir is: " + context.serviceDirectory}
 	])

3f406ce26a9e69422ccc78e4aea5e5ba5a1d8748
gal <gal@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-01-03 18:10:21 +0000

    updated to new custom params syntaxt
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@106514 eb64e737-3616-4df0-8941-5ee2ae88103d

--- apps/USM/usm/applications/simpleCustomCommandsMultipleInstances/simpleCustomCommandsMultipleInstances-2/simpleCustomCommandsMultipleInstances-2-service.groovy
+++ apps/USM/usm/applications/simpleCustomCommandsMultipleInstances/simpleCustomCommandsMultipleInstances-2/simpleCustomCommandsMultipleInstances-2-service.groovy
@@ -11,7 +11,7 @@ service {
 	
 	customCommands ([
 		"print" : {println "This is the print custom command"},
-		"params" : {"this is the custom parameters command. expecting 123: "+1+x+y},
+		"params" : {x, y -> return("this is the custom parameters command. expecting 123: "+1+x+y)},
 		"exception" : { throw new Exception("This is an error test")},
 		"runScript" : "add.groovy",
 		"context" : {"Service Dir is: " + context.serviceDirectory}

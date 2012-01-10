158d6a47064e470f03746d92e7655057f71f5b60
barakme <barakme@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-01-10 10:19:00 +0000

    Changed ec2 bootstrap test - uses existing ec2 file from distribution and edits it with key values. When test completes, files are restored to original state.
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@107141 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/AbstractCloudEc2Test.java
+++ src/test/cli/cloudify/AbstractCloudEc2Test.java
@@ -1,7 +1,6 @@
 package test.cli.cloudify;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.util.regex.Matcher;
@@ -12,6 +11,7 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import com.gigaspaces.cloudify.dsl.internal.DSLException;
 import test.AbstractTest;
 import framework.tools.SGTestHelper;
 import framework.utils.LogUtils;
@@ -19,19 +19,24 @@ import framework.utils.ScriptUtils;
 
 public class AbstractCloudEc2Test extends AbstractTest {
     
+	private static final String AWS_API_KEY = "fPdu7rYBF0mtdJs1nmzcdA8yA/3kbV20NgInn4NO";
+	private static final  String AWS_USER= "0VCFNJS3FXHYC7M6Y782";
+	
 	protected static final String WEBUI_PORT = String.valueOf(8099); 
 	protected static final String REST_PORT = String.valueOf(8100); 
 	private static final String IP_REGEX= "\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}"; 
 	private static final String WEBUI_URL_REGEX= "Webui service is available at: (http://" + IP_REGEX + ":" + WEBUI_PORT +")";
 	private static final String REST_URL_REGEX= "Rest service is available at: (http://" + IP_REGEX + ":" + REST_PORT + ")";
-	protected static final int NUM_OF_MANAGEMENT_MACHINES = 2;
+	protected static final int NUM_OF_MANAGEMENT_MACHINES = 1;
 	protected static final String INSTALL_TRAVEL_EXPECTED_OUTPUT = "Application travel installed successfully";
 	protected static final String UNINSTALL_TRAVEL_EXPECTED_OUTPUT = "Application travel uninstalled successfully";
 	
     protected URL[] restAdminUrl = new URL[NUM_OF_MANAGEMENT_MACHINES];
     protected URL[] webUIUrl = new URL[NUM_OF_MANAGEMENT_MACHINES];
     
-    private File[] serviceCloudFiles = new File[2];
+	private File originalEc2DslFile;
+	private File backupEc2DslFile;
+	private File targetPem;
     
 	@Override
     @BeforeMethod
@@ -58,39 +63,33 @@ public class AbstractCloudEc2Test extends AbstractTest {
         }
     }
     
-	private void bootstrapCloud() throws IOException, InterruptedException {
+	private void bootstrapCloud() throws IOException, InterruptedException, DSLException {
 		
-	    String applicationPath = (new File(ScriptUtils.getBuildPath(), "examples/travel").toString()).replace('\\', '/');
+	    //String applicationPath = (new File(ScriptUtils.getBuildPath(), "examples/travel").toString()).replace('\\', '/');
 		String ec2TestPath = (SGTestHelper.getSGTestRootDir() + "/apps/cloudify/cloud/").replace('\\', '/');
 		String sshKeyPemName = "cloud-demo.pem";
-		String ec2DslName = "ec2-cloud.groovy";
-		
-		File ec2DslFile = new File(ec2TestPath , ec2DslName);
 		
 		// ec2 plugin should include recipe that includes secret key 
 		File ec2PluginDir = new File(ScriptUtils.getBuildPath() , "tools/cli/plugins/esc/ec2/");
-		File targetPluginDsl = new File(ec2PluginDir, ec2DslName);
-        FileUtils.copyFile(ec2DslFile, targetPluginDsl);
-		assertTrue("File not found", targetPluginDsl.isFile());
+		originalEc2DslFile = new File(ec2PluginDir, "ec2-cloud.groovy");
+		backupEc2DslFile = new File(ec2PluginDir, "ec2-cloud.backup");
 		
-		// each cloudify service needs its own copy of cloud recipe
-		serviceCloudFiles[0] = new File(applicationPath, "cassandra/" + ec2DslName);
-		serviceCloudFiles[1] = new File(applicationPath, "tomcat/" + ec2DslName);
 		
-		for (int i = 0; i < serviceCloudFiles.length; i++) {
-    		FileUtils.copyFile(ec2DslFile, serviceCloudFiles[i]);
-    		assertTrue("File not found", serviceCloudFiles[i].isFile());
-		}
+		// Read file contents
+		final String originalDslFileContents = FileUtils.readFileToString(originalEc2DslFile);
+		Assert.assertTrue(originalDslFileContents.contains("ENTER_USER"), "Missing ENTER_USER statement in ec2-cloud.groovy");
+		Assert.assertTrue(originalDslFileContents.contains("ENTER_API_KEY"), "Missing ENTER_API_KEY statement in ec2-cloud.groovy");
 		
+		// first make a backup of the original file
+		FileUtils.copyFile(originalEc2DslFile, backupEc2DslFile);
+		
+		final String modifiedDslFileContents = originalDslFileContents.replace("ENTER_USER", AWS_USER).replace("ENTER_API_KEY", AWS_API_KEY);
+		FileUtils.write(originalEc2DslFile, modifiedDslFileContents);
+	
 		// upload dir needs to contain the sshKeyPem 
-		File targetPem = new File(ScriptUtils.getBuildPath(), "tools/cli/plugins/esc/ec2/upload/" + sshKeyPemName);
+		targetPem = new File(ScriptUtils.getBuildPath(), "tools/cli/plugins/esc/ec2/upload/" + sshKeyPemName);
         FileUtils.copyFile(new File(ec2TestPath, sshKeyPemName), targetPem);
         assertTrue("File not found", targetPem.isFile());
-		
-        // upload gigaspaces.zip to s3 (to the locations defined in the cloud dsl)
-		// TODO: commit pending code to CLI
-//        LogUtils.log("Uploading cloudify to S3. This might take a while");
-//        uploadCloudifyInstallationToS3();
 		
 		String output = CommandTestUtils.runCommandAndWait("bootstrap-cloud --verbose ec2");
 
@@ -124,18 +123,12 @@ public class AbstractCloudEc2Test extends AbstractTest {
         teardownCloud();
     }
 
-	private void deleteCloudFiles() {
-		if (serviceCloudFiles != null) {
-		    for (int i = 0; i < serviceCloudFiles.length; i++) {
-    			try {
-    				FileUtils.forceDelete(serviceCloudFiles[i]);
-    			} catch (FileNotFoundException e) {
-    				//ignore
-    			} catch (IOException e) {
-    				LogUtils.log("Failed to delete " + serviceCloudFiles[i] + " delete file manually");
-    			}
-		    }
-		}
+	private void deleteCloudFiles() throws IOException {
+		// undo all the changes we made in the local ec2 folder
+		FileUtils.copyFile(backupEc2DslFile, originalEc2DslFile);
+		FileUtils.deleteQuietly(backupEc2DslFile);
+		FileUtils.deleteQuietly(targetPem);
+		
 	}
 
 	private void teardownCloud() {
@@ -148,7 +141,11 @@ public class AbstractCloudEc2Test extends AbstractTest {
 			Assert.fail("teardown-cloud failed. SHUTDOWN VIRTUAL MACHINES MANUALLY !!!",e);
 		}
 		finally {
-			deleteCloudFiles();
+			try {
+				deleteCloudFiles();
+			} catch (IOException e) {
+				AssertFail("Failed to clean up after test finished: " + e.getMessage(), e);
+			}
 		}
 	}
 	

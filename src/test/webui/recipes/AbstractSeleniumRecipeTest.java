6b55a8e5c4185c1886f80df0c115b0a3164b5a1e
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-01-10 22:46:02 +0000

    move methods to parent class
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@107193 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/webui/recipes/AbstractSeleniumRecipeTest.java
+++ src/test/webui/recipes/AbstractSeleniumRecipeTest.java
@@ -1,47 +1,13 @@
 package test.webui.recipes;
 
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import org.openspaces.admin.pu.ProcessingUnit;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
 
-import test.cli.cloudify.CommandTestUtils;
 import test.webui.AbstractSeleniumTest;
 import framework.utils.LogUtils;
 
 public class AbstractSeleniumRecipeTest extends AbstractSeleniumTest {
-	
-	public static boolean bootstraped;
-	public static String METRICS_ASSERTION_SUFFIX = " metric that is defined in the dsl is not displayed in the metrics panel";
-	
-	@BeforeSuite(alwaysRun = true)
-	public void bootstrap() throws IOException, InterruptedException {
-		assertTrue(bootstrapLocalCloud());
-		bootstraped = true;
-	}
-	
-	@Override
-	@BeforeMethod(alwaysRun = true)
-	public void beforeTest() {
-		LogUtils.log("Test Configuration Started : " + this.getClass());
-	}
-	
-	@Override
-	@AfterMethod(alwaysRun = true)
-	public void afterTest() {
-		restorePreviousBrowser();
-		LogUtils.log("Test Finished : " + this.getClass());
-	}
-	
-	@AfterSuite(alwaysRun = true)
-	public void teardown() throws IOException, InterruptedException {
-		assertTrue(tearDownLocalCloud());
-		bootstraped = false;
-	}
 	
 	public void undeployNonManagementServices() {
 		for (ProcessingUnit pu : admin.getProcessingUnits().getProcessingUnits()) {
@@ -58,17 +24,5 @@ public class AbstractSeleniumRecipeTest extends AbstractSeleniumTest {
 	
 	public boolean isBootstraped() {
 		return bootstraped;
-	}
-	
-	private boolean bootstrapLocalCloud() throws IOException, InterruptedException {
-		String command = "bootstrap-localcloud --verbose";
-		String output = CommandTestUtils.runCommandAndWait(command);
-		return output.contains("Local-cloud started successfully");
-	}
-	
-	private boolean tearDownLocalCloud() throws IOException, InterruptedException {
-		String command = "teardown-localcloud --verbose";
-		String output = CommandTestUtils.runCommandAndWait(command);
-		return output.contains("Completed local-cloud teardown");
 	}
 }

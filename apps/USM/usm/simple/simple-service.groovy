bdb32b44afa42e3559f4c0e6fcc73446a894b298
adaml <adaml@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-01-11 17:08:58 +0000

    CLOUDIFY-440 Renamed all CLI source packages org.cloudifysource
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@107372 eb64e737-3616-4df0-8941-5ee2ae88103d

--- apps/USM/usm/simple/simple-service.groovy
+++ apps/USM/usm/simple/simple-service.groovy
@@ -40,7 +40,7 @@ service {
 	plugins = [
 		plugin {
 			name "jmx"
-			className "com.gigaspaces.cloudify.usm.jmx.JmxMonitor"
+			className "org.cloudifysource.usm.jmx.JmxMonitor"
 			config ([
 
 						"Details" : [

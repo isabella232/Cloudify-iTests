24b598433ffe7714059ae6d94e61d677c73b04fb
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-01-14 22:31:37 +0000

    copy hsqldb before starting tests
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@107556 eb64e737-3616-4df0-8941-5ee2ae88103d

--- deploy/bin/webui/set-deploy-env.bat
+++ deploy/bin/webui/set-deploy-env.bat
@@ -6,3 +6,4 @@ set RUNTIME_BUILD_LOCATION=%USER_HOME%\%BUILD_FOLDER%
 
 @echo copying build to user home dir, this is the build to be used in runtime
 xcopy %BUILD_LOCATION% %RUNTIME_BUILD_LOCATION% /e /i
+xcopy %LOCAL_SGPATH%\config\hsqldb.xml %RUNTIME_BUILD_LOCATION%\config\gsa

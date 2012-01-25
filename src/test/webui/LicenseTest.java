0b7f0a8f6a33c73a843cea2e1e4eb0cb43b8dfaf
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-01-25 08:17:13 +0000

    GS-9868 added test for webui version verification
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@108350 eb64e737-3616-4df0-8941-5ee2ae88103d

---
+++ src/test/webui/LicenseTest.java
@@ -0,0 +1,20 @@
+package test.webui;
+
+import org.testng.annotations.Test;
+
+import test.webui.objects.LoginPage;
+import test.webui.objects.dashboard.DashboardTab;
+
+public class LicenseTest extends AbstractSeleniumTest {
+	
+	@Test(timeOut = DEFAULT_TEST_TIMEOUT)
+	public void testXapLicense() throws InterruptedException {
+		
+		LoginPage loginPage = getLoginPage();
+		
+		DashboardTab dashboard = loginPage.login().switchToDashboard();
+		
+		assertTrue(dashboard.isCloudify());
+	}
+
+}

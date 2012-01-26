d274666b40e48e78dd3009f0d309c5b64e507198
adaml <adaml@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-01-26 11:18:38 +0000

    CLOUDIFY-361 Added regex for groovy compilation errors
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@108440 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/GroovyRuntimeExceptionInCliTest.java
+++ src/test/cli/cloudify/GroovyRuntimeExceptionInCliTest.java
@@ -6,28 +6,53 @@ import java.util.regex.Pattern;
 
 import org.testng.annotations.Test;
 /**
- * Test whether the CLI prints runtime exception details that occur in the groovy script.
- * A Cassandra service that throws an exception in the Cassandra-install.groovy file is used.
+ * Test whether the CLI prints runtime/compilation exception details that occur in the groovy script.
+ * Cassandra service that throws an exception in the Cassandra-install.groovy file is used to simulate runtime exceptions.
+ * simpleCompilation service is used to simulate a compilation error using error.groovy.
  * @author adaml
  *
  */
 public class GroovyRuntimeExceptionInCliTest extends AbstractLocalCloudTest {
-	private static final String BAD_SERVICE_PATH = CommandTestUtils.getPath("apps/USM/badUsmServices/Cassandra");
-	private static final String EXCEPTION_CAUGHT_REGEX = "Caught:.*\\s*?.*\\.groovy:[1-9]{1,}\\)";
-	
+	private static final String SERVICE_WITH_RUNTIME_EXCEPTION = CommandTestUtils.getPath("apps/USM/badUsmServices/Cassandra");
+	private static final String SERVICE_WITH_COMPILATION_EXCEPTION = CommandTestUtils.getPath("apps/USM/badUsmServices/simpleCompilation");
+	private static final String RUNTIME_EXCEPTION_CAUGHT_REGEX = "(Caught:.*\\.groovy:[1-9]{1,}\\))";
+	//This regex assumes that the char before the exception string would either
+	//be a Space char or a newLine Char. to grab the whole line use thhis regex:
+	//"((\\\\|/).+\\.groovy:\\s[1-9]+.*column\\s[1-9]+.*)$";
+	private static final String COMPILATION_EXCEPTION_CAUGHT_REGEX = "(([a-zA-Z]*:\\\\|/).+\\.groovy:\\s[1-9]+.*column\\s[1-9]+)";	
 	/**
-	 * Install a service and assert CLI exception details message. 
+	 * Install a service and assert CLI runtime exception details message. 
 	 * @throws IOException
 	 * @throws InterruptedException
 	 */
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT, groups = "1", enabled = true)
 	public void testGroovyRuntimeExceptionHandling() throws IOException, InterruptedException {
 		String commandOutput = CommandTestUtils.runCommandExpectedFail("connect " + this.restUrl 
-															+ ";install-service " + BAD_SERVICE_PATH 
+															+ ";install-service " + SERVICE_WITH_RUNTIME_EXCEPTION 
 															+ ";exit");
+
+		assertRegexFound(commandOutput, RUNTIME_EXCEPTION_CAUGHT_REGEX, Pattern.MULTILINE+Pattern.DOTALL);
 		
-		//See that the runtime exception print pattern exists in the CLI output.
-		Pattern pattern = Pattern.compile(EXCEPTION_CAUGHT_REGEX, Pattern.MULTILINE);
+	}
+	
+	/**
+	 * Install a service and assert CLI compilation exception details message. 
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	@Test(timeOut = DEFAULT_TEST_TIMEOUT, groups = "1", enabled = true)
+	public void testGroovyCompilationExceptionHandling() throws IOException, InterruptedException {
+		String commandOutput = CommandTestUtils.runCommandExpectedFail("connect " + this.restUrl 
+															+ ";install-service " + SERVICE_WITH_COMPILATION_EXCEPTION 
+															+ ";exit");
+
+		assertRegexFound(commandOutput, COMPILATION_EXCEPTION_CAUGHT_REGEX, Pattern.MULTILINE);
+		
+	}
+
+	private void assertRegexFound(String commandOutput, String regex, int regexFlags) {
+		//Check whether the exception print pattern exists in the CLI output.
+		Pattern pattern = Pattern.compile(regex, regexFlags);
 		Matcher matcher = pattern.matcher(commandOutput);
 		int beginIndex = 0;
 		int endIndex = 0;
@@ -37,6 +62,5 @@ public class GroovyRuntimeExceptionInCliTest extends AbstractLocalCloudTest {
 		}
 		//does the expression exist?
 		assertTrue("Runtime exception was not printed by the CLI.", endIndex != 0 && beginIndex != 0 );
-		
 	}
 }

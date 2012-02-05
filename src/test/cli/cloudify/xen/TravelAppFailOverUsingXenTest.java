3e873f39535cafb88b9607200b780ff73c80d22e
itaif <itaif@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-02-05 14:46:38 +0000

    Fixed and enabled test that checks cloudify eager deployment with 2 management machines.
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@109237 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/xen/TravelAppFailOverUsingXenTest.java
+++ src/test/cli/cloudify/xen/TravelAppFailOverUsingXenTest.java
@@ -1,14 +1,17 @@
 package test.cli.cloudify.xen;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import org.cloudifysource.dsl.Service;
 import org.cloudifysource.dsl.internal.CloudifyConstants;
+import org.cloudifysource.dsl.internal.DSLException;
 import org.cloudifysource.dsl.internal.ServiceReader;
+import org.cloudifysource.dsl.internal.packaging.PackagingException;
 import org.cloudifysource.dsl.utils.ServiceUtils;
-import org.junit.Assert;
 import org.openspaces.admin.pu.ProcessingUnit;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -20,9 +23,12 @@ import framework.utils.ScriptUtils;
 public class TravelAppFailOverUsingXenTest extends AbstractApplicationFailOverXenTest {
 	
 	private static final String TRAVEL_APPLICATION_NAME = "travel";
+	private static final String TOMCAT_PU_NAME = "tomcat";
+	private static final String TOMCAT_ABSOLUTE_PU_NAME = ServiceUtils.getAbsolutePUName(TRAVEL_APPLICATION_NAME,TOMCAT_PU_NAME);
+	private static final String CASSANDRA_PU_NAME = "cassandra";
+	private static final String CASSANDRA_ABSOLUTE_PU_NAME = ServiceUtils.getAbsolutePUName(TRAVEL_APPLICATION_NAME, CASSANDRA_PU_NAME);
 	private final String travelAppDirPath = ScriptUtils.getBuildPath() + "/examples/travel";
 	private int tomcatPort;
-	private int cassandraPort;
 	private String travelHostIp;
 		
 	@Override
@@ -30,103 +36,102 @@ public class TravelAppFailOverUsingXenTest extends AbstractApplicationFailOverXe
 	public void beforeTest() {
 		super.beforeTest();
 		tomcatPort = tomcatPort();
-		cassandraPort = cassandraPort();
 		
-		startAgent(0 ,"tomcat","cassandra");
+		startAgent(0 ,TOMCAT_PU_NAME,CASSANDRA_PU_NAME);
 	    repetitiveAssertNumberOfGSAsAdded(3, OPERATION_TIMEOUT);
 	    repetitiveAssertNumberOfGSAsRemoved(0, OPERATION_TIMEOUT);	 
-	    travelHostIp = admin.getZones().getByName("cassandra").getGridServiceAgents().getAgents()[0].getMachine().getHostAddress();
+	    travelHostIp = admin.getZones().getByName(CASSANDRA_PU_NAME).getGridServiceAgents().getAgents()[0].getMachine().getHostAddress();
 	}
 	
 	@Override
 	@AfterMethod
 	public void afterTest() {
-		LogUtils.log("Test Configuration Started " + this.getClass());
 		try {			
 			CommandTestUtils.runCommandAndWait("connect " + restUrl + " ;uninstall-application travel");
-		} catch (Exception e) {	}
+		} catch (Exception e) {	
+			Assert.fail("Failed to uninstall application travel",e);
+		}
 		
 		assertAppUninstalled("travel");	
 	}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	
-	@Test(timeOut = DEFAULT_TEST_TIMEOUT, groups = "1", enabled = false)
-	public void testTravelApp() throws Exception{
-		installApp(tomcatPort, travelHostIp, cassandraPort, travelHostIp, travelAppDirPath);
-		isTravelAppInstalled(cassandraPort , tomcatPort, travelHostIp);
+	@Test(timeOut = DEFAULT_TEST_TIMEOUT, groups = "1", enabled = true)
+	public void testTravelAppEagerModeNoFailover() throws Exception{
+		installTravelApplication(tomcatPort, travelHostIp, travelAppDirPath);	
 	}
 	
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT, groups = "1", enabled = false)
 	public void testTravelAppTomcatPuInstFailOver() throws Exception{
-		installApp(tomcatPort, travelHostIp, cassandraPort, travelHostIp, travelAppDirPath);
-		isTravelAppInstalled(cassandraPort , tomcatPort, travelHostIp);
+		installTravelApplication(tomcatPort, travelHostIp, travelAppDirPath);
 		
-		ProcessingUnit tomcat = admin.getProcessingUnits().getProcessingUnit(ServiceUtils.getAbsolutePUName(TRAVEL_APPLICATION_NAME, "tomcat"));
+		ProcessingUnit tomcat = admin.getProcessingUnits().getProcessingUnit(TOMCAT_ABSOLUTE_PU_NAME);
 		
 		int tomcatPuInstancesAfterInstall = tomcat.getInstances().length;
 		LogUtils.log("destroying the pu instance holding tomcat");
 		tomcat.getInstances()[0].destroy();
-		assertPuInstanceKilled("tomcat" , tomcatPort ,travelHostIp , tomcatPuInstancesAfterInstall);
-		assertPuInstanceRessurected("tomcat" , tomcatPort ,travelHostIp, tomcatPuInstancesAfterInstall) ;
+		assertPuInstanceKilled(TOMCAT_ABSOLUTE_PU_NAME , tomcatPort ,travelHostIp , tomcatPuInstancesAfterInstall);
+		assertPuInstanceRessurected(TOMCAT_ABSOLUTE_PU_NAME , tomcatPort ,travelHostIp, tomcatPuInstancesAfterInstall) ;
 	}
 	
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT, groups = "1", enabled = false)
 	public void testTravelAppTomcatGSCFailOver() throws Exception{
-		installApp(tomcatPort, travelHostIp, cassandraPort, travelHostIp, travelAppDirPath);
-		isTravelAppInstalled(cassandraPort , tomcatPort, travelHostIp);
+		installTravelApplication(tomcatPort, travelHostIp, travelAppDirPath);
 		
-		ProcessingUnit tomcat = admin.getProcessingUnits().getProcessingUnit(ServiceUtils.getAbsolutePUName(TRAVEL_APPLICATION_NAME, "tomcat"));
+		ProcessingUnit tomcat = admin.getProcessingUnits().getProcessingUnit(TOMCAT_ABSOLUTE_PU_NAME);
 		int tomcatPuInstancesAfterInstall = tomcat.getInstances().length;
 		LogUtils.log("restarting GSC containing tomcat");
 		tomcat.getInstances()[0].getGridServiceContainer().kill();
-		assertPuInstanceKilled("tomcat" , tomcatPort,travelHostIp , tomcatPuInstancesAfterInstall);
-		assertPuInstanceRessurected("tomcat" , tomcatPort,travelHostIp , tomcatPuInstancesAfterInstall);
+		assertPuInstanceKilled(TOMCAT_PU_NAME , tomcatPort,travelHostIp , tomcatPuInstancesAfterInstall);
+		assertPuInstanceRessurected(TOMCAT_PU_NAME , tomcatPort,travelHostIp , tomcatPuInstancesAfterInstall);
 	}
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 	
-	private void isTravelAppInstalled(int port1 ,int port2 ,String host) {
-		ProcessingUnit cassandra = admin.getProcessingUnits().waitFor(ServiceUtils.getAbsolutePUName(TRAVEL_APPLICATION_NAME, "cassandra"));
-		ProcessingUnit tomcat = admin.getProcessingUnits().waitFor(ServiceUtils.getAbsolutePUName(TRAVEL_APPLICATION_NAME, "tomcat"));
-		Assert.assertNotNull("cassandra was not deployed", cassandra);
-		Assert.assertNotNull("tomcat was not deployed", tomcat);
+	private void isTravelAppInstalled(int tomcatPort ,String host) {
+		ProcessingUnit cassandra = admin.getProcessingUnits().waitFor(CASSANDRA_ABSOLUTE_PU_NAME);
+		ProcessingUnit tomcat = admin.getProcessingUnits().waitFor(TOMCAT_ABSOLUTE_PU_NAME);
+		assertNotNull("cassandra was not deployed", cassandra);
+		assertNotNull("tomcat was not deployed", tomcat);
 		
 		boolean cassandraDeployed = cassandra.waitFor(cassandra.getTotalNumberOfInstances());
 		boolean tomcatDeployed = tomcat.waitFor(tomcat.getTotalNumberOfInstances());
 		
-		Assert.assertTrue("cassandra pu didn't deploy all of it's instances", cassandraDeployed);
-		Assert.assertTrue("tomcat pu didn't deploy all of it's instances", tomcatDeployed);
+		assertTrue("cassandra pu didn't deploy all of it's instances", cassandraDeployed);
+		assertTrue("tomcat pu didn't deploy all of it's instances", tomcatDeployed);
 		
-		if(port1 > 0 && port2 >0){
-			LogUtils.log("asserting needed ports for application are taken");
-			boolean port1Availible = portIsAvailible(port1 ,host);
-			boolean port2Availible = portIsAvailible(port2 ,host);
-			assertTrue("port was not occupied after installation - port number " + port1, !port1Availible);
-			assertTrue("port was not occupied after installation - port number " + port2, !port2Availible);
-		}	
+		LogUtils.log("asserting needed ports for application are taken");
+		boolean tomcatPortAvailible = portIsAvailible(tomcatPort ,host);
+		assertTrue("tomcat port was not occupied after installation - port number " + tomcatPort, !tomcatPortAvailible);
+		
 		LogUtils.log("Travel application installed");
 	}
 	
-	private int tomcatPort(){
+	private int tomcatPort() {
 		Service tomcatService = null;
 		try {
-			tomcatService = ServiceReader.getServiceFromDirectory(new File(travelAppDirPath + "/tomcat"), CloudifyConstants.DEFAULT_APPLICATION_NAME).getService();
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		} 
+			tomcatService = ServiceReader.getServiceFromDirectory(new File(travelAppDirPath + "/" + TOMCAT_PU_NAME), CloudifyConstants.DEFAULT_APPLICATION_NAME).getService();
+		} catch (FileNotFoundException e) {
+			Assert.fail("tomcat port",e);
+		} catch (PackagingException e) {
+			Assert.fail("tomcat port",e);
+		} catch (DSLException e) {
+			Assert.fail("tomcat port",e);
+		}
 		return tomcatService.getNetwork().getPort();
 	}
 	
-	private int cassandraPort(){
-		Service cassandraService = null;
-		try {
-			cassandraService = ServiceReader.getServiceFromDirectory(new File(travelAppDirPath + "/cassandra"), CloudifyConstants.DEFAULT_APPLICATION_NAME).getService();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} 
-		ArrayList<Integer> cassandraPorts = (ArrayList<Integer>) cassandraService.getPlugins().get(0).getConfig().get("Port");
-		return cassandraPorts.get(0);
+	private void installTravelApplication(int tomcatPort, String host, String appDirPath) throws IOException, InterruptedException {
+		LogUtils.log("asserting needed ports for application are availible");
+		boolean travelPortAvailible = portIsAvailible(tomcatPort, host);
+		assertTrue("port was not free before installation - port number " + tomcatPort, travelPortAvailible);
+		    
+		String commandOutput = CommandTestUtils.runCommandAndWait("connect --verbose " + restUrl + ";install-application --verbose " + appDirPath);
+		String appName = new File(appDirPath).getName();
+		assertTrue("install-application command didn't install the application" , commandOutput.contains("Application " + appName + " installed successfully"));
+		
+		isTravelAppInstalled(tomcatPort, host);
 	}
+
 }

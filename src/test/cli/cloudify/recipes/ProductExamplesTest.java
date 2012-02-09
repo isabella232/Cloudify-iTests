0dd1f4cdbf15c00cf85527a9f089290d869111d6
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-02-09 15:00:56 +0000

    add petclinic-url check after installing application
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@109665 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/recipes/ProductExamplesTest.java
+++ src/test/cli/cloudify/recipes/ProductExamplesTest.java
@@ -1,10 +1,17 @@
 package test.cli.cloudify.recipes;
 
+import static org.testng.AssertJUnit.fail;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import org.openspaces.admin.machine.Machine;
 import org.testng.annotations.Test;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 import test.cli.cloudify.AbstractLocalCloudTest;
 import framework.utils.AssertUtils;
@@ -69,5 +76,24 @@ public class ProductExamplesTest extends AbstractLocalCloudTest {
 		String cliOutput = runCommand("connect " + restUrl + ";install-application --verbose " + petclinicDirPath);
 		assertTrue("petclinic app couln't be installed",
 				cliOutput.toLowerCase().contains("application petclinic-mongo installed successfully"));
+		
+		
+	}
+	
+	private void assertPetclinicPageExists() {
+		
+		Machine localMachine = admin.getMachines().getMachines()[0];
+		
+		WebClient client = new WebClient(BrowserVersion.getDefault());
+		
+        HtmlPage page = null;
+        try {
+            page = client.getPage("http://" + localMachine.getHostAddress() + ":8080/petclinic-mongo");
+        } catch (IOException e) {
+            fail("Could not get a resposne from the petclinic URL " + e.getMessage());
+        }
+        assertEquals("OK", page.getWebResponse().getStatusMessage());
+		
+		
 	}
 }

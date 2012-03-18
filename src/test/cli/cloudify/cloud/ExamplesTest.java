556442e008ba72abb474ccb17d935d876e84467e
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-03-18 11:20:04 +0000

    added logs
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@112867 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/cloud/ExamplesTest.java
+++ src/test/cli/cloudify/cloud/ExamplesTest.java
@@ -4,18 +4,21 @@ import java.io.IOException;
 
 import org.testng.annotations.Test;
 
+import framework.utils.LogUtils;
 import framework.utils.ScriptUtils;
 
 public class ExamplesTest extends AbstractCloudTest {
 
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, enabled = true, dataProvider = "supportedClouds")
 	public void testTravel(String cloudName) throws IOException, InterruptedException {
+		LogUtils.log("installing application travel on " + cloudName);
 		setCloudToUse(cloudName);
 		doTest("travel");
 	}
 	
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, enabled = true, dataProvider = "supportedClouds")
 	public void testPetclinic(String cloudName) throws IOException, InterruptedException {
+		LogUtils.log("installing application petclinic on " + cloudName);
 		setCloudToUse(cloudName);
 		doTest("petclinic");
 	}

56b0410b18ef13f094cc12d9fb5af64564123474
gal <gal@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-03-19 16:29:58 +0000

    added success counter
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@113050 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/RepetativeInstallAndUninstallStockDemoWithProblemAtInstallTest.java
+++ src/test/cli/cloudify/RepetativeInstallAndUninstallStockDemoWithProblemAtInstallTest.java
@@ -12,21 +12,27 @@ public class RepetativeInstallAndUninstallStockDemoWithProblemAtInstallTest exte
 	private final int repetitions = 10;
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT, groups = "1", enabled = true)
 	public void testInstallAndUninstall() throws IOException, InterruptedException {
+		int scenarioSuccessCounter = 0;
 		for(int i=0 ; i < repetitions ; i++)
-			doTest();
+			if(doTest())
+				scenarioSuccessCounter++;
+		
+		int firstInstallSuccessCounter = repetitions - scenarioSuccessCounter;
+		System.out.println(firstInstallSuccessCounter + "/" + repetitions + " times the first installation succeedded, these runs are irrelavent");
+		System.out.println(scenarioSuccessCounter + "/" + repetitions + " times the second installation succeedded");
 	}
 
-	private void doTest() throws IOException, InterruptedException {
+	private boolean doTest() throws IOException, InterruptedException {
 		final String applicationDir = CommandTestUtils.getPath("apps/USM/usm/applications/stockdemo");	
 		
 		String failOutput = CommandTestUtils.runCommand("connect " + restUrl + ";install-application --verbose -timeout 4 " + applicationDir, true, true);		
 		if(!failOutput.toLowerCase().contains("operation failed"))
-			return;
+			return false;
 		
 		runCommand("connect " + restUrl + ";uninstall-application --verbose stockdemo");
 				
 		String successOutput = CommandTestUtils.runCommand("connect " + restUrl + ";install-application --verbose " + applicationDir, true, false);
 		Assert.assertTrue("stockdemo was not installed sucessfully the second time", successOutput.toLowerCase().contains("successfully installed"));
-		
+		return true;
 	}
 }

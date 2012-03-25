6ab77d5c7d032c788777e1081e9e56859a62366a
itaif <itaif@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-03-25 14:54:25 +0000

    CLOUDIFY-660 Added mock services for auto scaling tests that allow controlling a service monitor via a custom command
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@113733 eb64e737-3616-4df0-8941-5ee2ae88103d

---
+++ apps/cloudify/recipes/customServiceMonitor/customServiceMonitor-service.groovy
@@ -0,0 +1,26 @@
+/**
+ * This service is a mock for a custom service monitor
+ * That SGTest can control using custom commands.
+ */
+service {
+	name "customServiceMonitor"
+  
+  long counter = 0;
+  
+	lifecycle{
+
+    lifecycle { start "run.groovy" }
+
+		monitors (["counter":{
+      println "counter=${counter}" 
+      return counter
+    }])
+	}
+
+
+	customCommands ([
+				"add" : {x -> counter += (x as Long);},
+				"sub" : {x -> counter -= (x as Long);},
+				"set" : {x -> counter = (x as Long);},
+  ])
+}

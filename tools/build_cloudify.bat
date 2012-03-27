713b127dd7434009f3a16f9a1f984e01d7507b19
itaif <itaif@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-03-27 10:40:57 +0000

    Fixed relative paths in batch file for compilint cloudify
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@113980 eb64e737-3616-4df0-8941-5ee2ae88103d

--- tools/build_cloudify.bat
+++ tools/build_cloudify.bat
@@ -1,11 +1,14 @@
-pushd ..\..\cloudify\cloudify
+setlocal
+set CLOUDIFYHOME=..\..\..\..\cloudify
+pushd %CLOUDIFYHOME%\cloudify
 call mvn clean
 call mvn -DskipTests=true install
 popd
-copy /y ..\..\cloudify\dsl\target\dsl.jar ..\..\SGTest\tools\gigaspaces\lib\required\dsl.jar
-copy /y ..\..\cloudify\usm\target\usm.jar ..\..\SGTest\tools\gigaspaces\lib\platform\usm\usm.jar
-copy /y ..\..\cloudify\cli\target\cli.jar ..\..\SGTest\tools\gigaspaces\tools\cli\cli.jar
-copy /y ..\..\cloudify\restful\target\rest.war ..\..\SGTest\tools\gigaspaces\tools\rest\rest.war
-copy /y ..\..\cloudify\esc\target\esc.jar ..\..\SGTest\tools\gigaspaces\lib\platform\esm\esc.jar
-copy /y ..\..\cloudify\esc-commands\target\esc-commands.jar ..\..\SGTest\tools\gigaspaces\tools\cli\plugins\esc\esc-commands.jar
+copy /y %CLOUDIFYHOME%\dsl\target\dsl.jar ..\..\SGTest\tools\gigaspaces\lib\required\dsl.jar
+copy /y %CLOUDIFYHOME%\usm\target\usm.jar ..\..\SGTest\tools\gigaspaces\lib\platform\usm\usm.jar
+copy /y %CLOUDIFYHOME%\cli\target\cli.jar ..\..\SGTest\tools\gigaspaces\tools\cli\cli.jar
+copy /y %CLOUDIFYHOME%\restful\target\rest.war ..\..\SGTest\tools\gigaspaces\tools\rest\rest.war
+copy /y %CLOUDIFYHOME%\esc\target\esc.jar ..\..\SGTest\tools\gigaspaces\lib\platform\esm\esc.jar
+copy /y %CLOUDIFYHOME%\esc-commands\target\esc-commands.jar ..\..\SGTest\tools\gigaspaces\tools\cli\plugins\esc\esc-commands.jar
+endlocal
 pause

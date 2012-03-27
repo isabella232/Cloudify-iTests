398faff1f274359238a2ffa01cf3d6313cb0f5d1
itaif <itaif@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-03-27 10:40:52 +0000

    CLOUDIFY-660 removed servicecontext since it is not used
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@113978 eb64e737-3616-4df0-8941-5ee2ae88103d

--- apps/cloudify/recipes/customServiceMonitor/run.groovy
+++ apps/cloudify/recipes/customServiceMonitor/run.groovy
@@ -1,8 +1,1 @@
-import org.cloudifysource.dsl.context.ServiceContext
-import org.cloudifysource.dsl.context.ServiceContextFactory
-
-
-ServiceContext context = ServiceContextFactory.getServiceContext()
-
-
 sleep(Long.MAX_VALUE)

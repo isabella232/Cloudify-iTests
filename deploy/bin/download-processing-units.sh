4534671648e79d38c3f2bae7cd98a3d567d61dde
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-04-01 22:20:10 +0000

    GS-10079 use a different logging file for client side Admin API logging since we have to use a different handler
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@114506 eb64e737-3616-4df0-8941-5ee2ae88103d

--- deploy/bin/download-processing-units.sh
+++ deploy/bin/download-processing-units.sh
@@ -69,6 +69,10 @@ rm -f ${BUILD_DIR}/config/gs_logging.properties
 cp gs_logging.properties /export/tgrid/sgtest2.0-cloudify/config
 mv gs_logging.properties ${BUILD_DIR}/config
 
+svn export ${SVN_SGTEST_REPOSITORY}/config/sgtest_logging.properties
+rm -f ${BUILD_DIR}/config/sgtest_logging.properties
+cp sgtest_logging.properties /export/tgrid/sgtest2.0-cloudify/config
+
 rm -rf /export/tgrid/sgtest-cloudify/lib/xenserver/
 svn --force export ${SVN_SGTEST_REPOSITORY}/lib/xenserver
 mv xenserver /export/tgrid/sgtest2.0-cloudify/lib/

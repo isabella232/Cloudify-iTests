920f971bf8d078491c2ba3c46b882642778c62e3
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-04-04 14:48:24 +0000

    fix script
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@114898 eb64e737-3616-4df0-8941-5ee2ae88103d

--- deploy/bin/windows/set-deploy-env.bat
+++ deploy/bin/windows/set-deploy-env.bat
@@ -1,7 +1,6 @@
 set USER_HOME=C:\Users\ca
 set apache.port=8000
 set apache.home=%USER_HOME%\Apache Software Foundation\Apache2.2
-set LOCAL_SGPATH=%USER_HOME%\sgwebui-cloudify
 set RUNTIME_BUILD_LOCATION=%USER_HOME%\%BUILD_FOLDER%
 
 @echo copying build to user home dir, this is the build to be used in runtime

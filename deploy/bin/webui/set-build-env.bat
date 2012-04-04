b80d09b48d843548b672f4b924f1813188493605
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-04-04 13:16:55 +0000

    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@114865 eb64e737-3616-4df0-8941-5ee2ae88103d

--- deploy/bin/webui/set-build-env.bat
+++
@@ -1,7 +0,0 @@
-set USER_HOME=C:\Users\ca
-set BUILD_FOLDER=gigaspaces-cloudify-%VERSION%-%MILESTONE%
-set BUILD_LOCATION=%USER_HOME%\sgwebui-cloudify\deploy\local-builds\%BUILD_NUMBER%\%BUILD_FOLDER%
-set PRODUCT=xap
-set LOCAL_SGPATH=%USER_HOME%\sgwebui-cloudify
-set REMOTE_BUILD_DIR=\\tarzan\builds\cloudify\%VERSION%\%BUILD_NUMBER%
-set BUILD_FOLDER=gigaspaces-cloudify-%VERSION%-%MILESTONE%

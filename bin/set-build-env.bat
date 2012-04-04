47dbd9986a5254943e6057954665b04d30c6bd0e
sagib <sagib@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-04-04 12:01:48 +0000

    CLOUDIFY-668 reverted adding scripts and using gs-webui scripts
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@114842 eb64e737-3616-4df0-8941-5ee2ae88103d

--- bin/set-build-env.bat
+++
@@ -1,8 +0,0 @@
-set USER_HOME=C:\Users\ca
-set BUILD_FOLDER=gigaspaces-cloudify-%VERSION%-%MILESTONE%
-set BUILD_LOCATION=%USER_HOME%\sgtest-cloudify\deploy\local-builds\%BUILD_NUMBER%\%BUILD_FOLDER%
-set PRODUCT=xap
-set LOCAL_SGPATH=%USER_HOME%\sgtest-cloudify
-set REMOTE_BUILD_DIR=\\tarzan\builds\cloudify\%VERSION%\%BUILD_NUMBER%
-set BUILD_FOLDER=gigaspaces-cloudify-%VERSION%-%MILESTONE%
-set RUNTIME_BUILD_LOCATION=%USER_HOME%\%BUILD_FOLDER%

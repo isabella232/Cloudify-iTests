7591ca5f3389ca68bb421272663d68f467d659b2
sagib <sagib@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-04-05 07:55:36 +0000

    added methods to replace text in files
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@114965 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/framework/utils/IOUtils.java
+++ src/framework/utils/IOUtils.java
@@ -1,13 +1,17 @@
 package framework.utils;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
 
 import com.google.common.io.Files;
 
 /**
  * A set of utility methods for IO manipulation
- * @author elip
+ * @author elip, sagi
  *
  */
 public class IOUtils {
@@ -23,5 +27,33 @@ public class IOUtils {
 		old.delete();
 		Files.copy(new File(newFile), new File(oldFile));
 	}
+
+	public static void replaceTextInFile(String FileName, String oldText, String newText) throws IOException {
+		HashMap<String, String> toPass = new HashMap<String, String>();
+		toPass.put(oldText, newText);
+		replaceTextInFile(FileName, toPass);
+	}
+	
+	public static void replaceTextInFile(String FileName, HashMap<String, String> map) throws IOException {
+		File file = new File(FileName);
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line = "", oldtext = "";
+        while((line = reader.readLine()) != null)
+            {
+            oldtext += line + "\r\n";
+        }
+        reader.close();
+        String newtext = new String(oldtext);
+        for (String toReplace : map.keySet()) {
+        	newtext = newtext.replaceAll(toReplace, map.get(toReplace));
+        }
+       
+        FileWriter writer = new FileWriter(FileName);
+        writer.write(newtext);writer.close();
+		
+		
+	}
+	
+	
 
 }

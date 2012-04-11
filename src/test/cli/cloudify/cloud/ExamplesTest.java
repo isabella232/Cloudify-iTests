855545546225c7ed210bdc376cfea6ec9dec9bc2
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-04-11 11:21:24 +0000

    fix test logs
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@115702 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/cloud/ExamplesTest.java
+++ src/test/cli/cloudify/cloud/ExamplesTest.java
@@ -2,11 +2,9 @@ package test.cli.cloudify.cloud;
 
 import java.io.IOException;
 
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import test.cli.cloudify.CommandTestUtils;
-
 import framework.utils.LogUtils;
 import framework.utils.ScriptUtils;
 
@@ -15,45 +13,41 @@ public class ExamplesTest extends AbstractCloudTest {
 	public ExamplesTest() {
 		LogUtils.log("Instansiated " + ExamplesTest.class.getName());
 	}
-	
-	private String appName;
 
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, enabled = true, dataProvider = "supportedClouds")
 	public void testTravel(String cloudName) throws IOException, InterruptedException {
-		doTest(cloudName, "travel");
+		doTest(cloudName, "travel", "travel");
 	}
 
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, enabled = true, dataProvider = "supportedCloudsWithoutByon")
 	public void testPetclinic(String cloudName) throws IOException, InterruptedException {
-		doTest(cloudName, "petclinic");
+		doTest(cloudName, "petclinic", "petclinic");
 	}
 	
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, enabled = true, dataProvider = "supportedClouds")
 	public void testPetclinicSimple(String cloudName) throws IOException, InterruptedException {
-		doTest(cloudName, "petclinic-simple");
+		doTest(cloudName, "petclinic-simple", "petclinic");
 	}
 	
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, enabled = true, dataProvider = "supportedClouds")
 	public void testPetclinicSimpleScalingRules(String cloudName) throws IOException, InterruptedException {
-		doTest(cloudName, "petclinic-simple-scalingRules");
+		doTest(cloudName, "petclinic-simple-scalingRules", "petclinic");
 	}
 	
-	private void doTest(String cloudName, String applicationName) throws IOException, InterruptedException {
-		this.appName = applicationName;
-		LogUtils.log("installing application travel on " + cloudName);
+	private void doTest(String cloudName, String applicationFolderName, String applicationName) throws IOException, InterruptedException {
+		LogUtils.log("installing application" + applicationName + " on " + cloudName);
 		setCloudToUse(cloudName);
-		String applicationPath = ScriptUtils.getBuildPath() + "/examples/" + appName;
-		installApplicationAndWait(applicationPath, appName);	
-	}
-	
-	@AfterMethod
-	public void cleanup() throws IOException, InterruptedException {
-		
-		if ((getService() != null) && (getService().getRestUrls() != null)) {
-			String command = "connect " + getService().getRestUrls()[0] + ";list-applications";
-			String output = CommandTestUtils.runCommandAndWait(command);
-			if (output.contains(appName)) {
-				uninstallApplicationAndWait(appName);			
+		String applicationPath = ScriptUtils.getBuildPath() + "/examples/" + applicationFolderName;
+		try {
+			installApplicationAndWait(applicationPath, applicationName);
+		}
+		finally {
+			if ((getService() != null) && (getService().getRestUrls() != null)) {
+				String command = "connect " + getService().getRestUrls()[0] + ";list-applications";
+				String output = CommandTestUtils.runCommandAndWait(command);
+				if (output.contains(applicationName)) {
+					uninstallApplicationAndWait(applicationName);			
+				}
 			}
 		}
 	}

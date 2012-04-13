46173125e6a663fac1f2a4381f2258a07f34e69b
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-04-13 07:58:11 +0000

    fix test
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@115841 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/webui/LicenseTest.java
+++ src/test/webui/LicenseTest.java
@@ -1,11 +1,27 @@
 package test.webui;
 
+import org.openspaces.admin.pu.DeploymentStatus;
+import org.openspaces.admin.pu.ProcessingUnit;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import framework.utils.ProcessingUnitUtils;
 
 import test.webui.objects.LoginPage;
 import test.webui.objects.dashboard.DashboardTab;
 
 public class LicenseTest extends AbstractSeleniumTest {
+	
+	@BeforeMethod
+	public void startBrowser() throws InterruptedException {	
+		ProcessingUnit webui = admin.getProcessingUnits().waitFor("webui");
+		ProcessingUnitUtils.waitForDeploymentStatus(webui, DeploymentStatus.INTACT);
+		assertTrue(webui != null);
+		assertTrue(webui.getInstances().length != 0);	
+		String url = ProcessingUnitUtils.getWebProcessingUnitURL(webui).toString();	
+		startWebBrowser(url); 
+		
+	}
 	
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT)
 	public void testCloudifyLicense() throws InterruptedException {

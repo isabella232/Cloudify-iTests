035ec35eb09262f9de577949f3014e8f152c33b9
barakme <barakme@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-04-25 12:16:57 +0000

    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@117231 eb64e737-3616-4df0-8941-5ee2ae88103d

--- apps/USM/usm/applications/travelExtended/tomcat-extend/tomcat_start.groovy
+++ apps/USM/usm/applications/travelExtended/tomcat-extend/tomcat_start.groovy
@@ -10,23 +10,25 @@ cassandraService = serviceContext.waitForService("cassandra-extend", 20, TimeUni
 cassandraInstances = cassandraService.waitForInstances(cassandraService.numberOfPlannedInstances, 60, TimeUnit.SECONDS)
 cassandraHost = cassandraInstances[0].hostAddress
 
+script = "${config.unzipFolder}/bin/catalina"
+
 println "#################### got cassandra host: ${cassandraHost}"
 
 //start tomcat
-println "executing command ${config.script}"
+println "executing command ${script}"
 new AntBuilder().sequential {
-    exec(executable:"${config.script}.sh", osfamily:"unix") {
-        env(key:"CATALINA_HOME", value: "${config.home}")
-        env(key:"CATALINA_BASE", value: "${config.home}")
-        env(key:"CATALINA_TMPDIR", value: "${config.home}/temp")
+    exec(executable:"${script}.sh", osfamily:"unix") {
+        env(key:"CATALINA_HOME", value: "${config.unzipFolder}")
+        env(key:"CATALINA_BASE", value: "${config.unzipFolder}")
+        env(key:"CATALINA_TMPDIR", value: "${config.unzipFolder}/temp")
         env(key:"CATALINA_OPTS", value:"-Dcom.sun.management.jmxremote.port=11099 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false")
         env(key:"CASSANDRA_IP", value:cassandraHost)
         arg(value:"run")
     }
-    exec(executable:"${config.script}.bat", osfamily:"windows") {
-        env(key:"CATALINA_HOME", value: "${config.home}")
-        env(key:"CATALINA_BASE", value: "${config.home}")
-        env(key:"CATALINA_TMPDIR", value: "${config.home}/temp")
+    exec(executable:"${script}.bat", osfamily:"windows") {
+        env(key:"CATALINA_HOME", value: "${config.unzipFolder}")
+        env(key:"CATALINA_BASE", value: "${config.unzipFolder}")
+        env(key:"CATALINA_TMPDIR", value: "${config.unzipFolder}/temp")
         env(key:"CATALINA_OPTS", value:"-Dcom.sun.management.jmxremote.port=11099 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false")
         env(key:"CASSANDRA_IP", value:cassandraHost)
         arg(value:"run")

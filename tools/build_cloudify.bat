63bd0a0df5e187afa2a8bdff2b717d7a2b642e58
itaif <itaif@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-05-13 07:22:53 +0000

    Fix problem of two esc.jar after batch file completes
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@118702 eb64e737-3616-4df0-8941-5ee2ae88103d

--- tools/build_cloudify.bat
+++ tools/build_cloudify.bat
@@ -8,6 +8,7 @@ copy /y %CLOUDIFYHOME%\dsl\target\dsl.jar ..\..\SGTest\tools\gigaspaces\lib\requ
 copy /y %CLOUDIFYHOME%\usm\target\usm.jar ..\..\SGTest\tools\gigaspaces\lib\platform\usm\usm.jar
 copy /y %CLOUDIFYHOME%\cli\target\cli.jar ..\..\SGTest\tools\gigaspaces\tools\cli\cli.jar
 copy /y %CLOUDIFYHOME%\restful\target\rest.war ..\..\SGTest\tools\gigaspaces\tools\rest\rest.war
+del /y ..\..\SGTest\tools\gigaspaces\lib\platform\esm\esc*.jar
 copy /y %CLOUDIFYHOME%\esc\target\esc.jar ..\..\SGTest\tools\gigaspaces\lib\platform\esm\esc.jar
 copy /y %CLOUDIFYHOME%\esc-commands\target\esc-commands.jar ..\..\SGTest\tools\gigaspaces\tools\cli\plugins\esc\esc-commands.jar
 rmdir /s /q ..\..\SGTest\tools\gigaspaces\examples\petclinic-simple

3ff040155dbbff553a852932be442053fbb7f79a
nirb <nirb@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-06-04 15:16:58 +0000

    CLOUDIFY-857 Added a test.
    The test changes the default byon cloud DSL with one that has 3 machine templates, and assigns different services to different templates. The test then assert that services are on the correct machines, and checks uninstall and teardown. 
    
    In two of the three templates, machine names are entered instead of their IPs in the byon-cloud.groovy file.
    
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@120527 eb64e737-3616-4df0-8941-5ee2ae88103d

---
+++ src/test/cli/cloudify/cloud/MultipleTemplatesWithNamesAsIPsTest.java
@@ -0,0 +1,55 @@
+package test.cli.cloudify.cloud;
+
+import java.io.IOException;
+
+import org.cloudifysource.dsl.internal.DSLException;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+
+
+/**
+ * This test installs petclinic with 3 different templates on a byon cloud. 
+ * <p>for two of the three templates the test injects to byon-cloud.groovy names of machines instead of IPs.
+ * <p>It checks that each service was assigned to the correct template, according to byon-cloud.groovy.
+ * <p>After the installation completes, the test checks the uninstall and teardown operations.
+ * 
+ * <p>Note: for two of the three templates the test injects to byon-cloud.groovy names of machines instead of IPs.
+ * 
+ * <p>Note: this test uses 5 fixed machines - 192.168.9.115,192.168.9.116,192.168.9.120,192.168.9.125,192.168.9.126.
+ */
+public class MultipleTemplatesWithNamesAsIPsTest extends MultipleMachineTemplatesTest{
+
+	@BeforeClass(enabled = true)
+	public void before() throws IOException, InterruptedException, DSLException {
+		TEMPLATE_1_IPs = "pc-lab95,pc-lab96";
+		TEMPLATE_2_IPs = "192.168.9.120,192.168.9.125";
+		TEMPLATE_3_IPs = "pc-lab106";
+		super.before();
+	}
+	
+	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, enabled = true, priority = 1)
+	public void testPetclinic() throws Exception {
+		TEMPLATE_1_IPs = "192.168.9.115,192.168.9.116";
+		TEMPLATE_3_IPs = "192.168.9.126";
+		super.testPetclinic();
+	}
+	
+	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, enabled = true, priority = 2)
+	public void testPetclinicUninstall(){
+		super.testPetclinicUninstall();
+	}
+	
+	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, enabled = true, priority = 3)
+	public void testPetclinicTeardownByon(){
+		super.testPetclinicTeardownByon();
+	}
+	
+	@AfterClass(alwaysRun = true)
+	public void teardown() throws IOException, InterruptedException {
+		super.teardown();
+	}
+}
+	

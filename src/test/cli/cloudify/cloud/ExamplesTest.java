d1141c09422971cbdabb636190304cde351f77d0
adaml <adaml@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-06-07 14:17:38 +0000

    added after method for test to completely remove installed application in case of error in installation.
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@120796 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/cloud/ExamplesTest.java
+++ src/test/cli/cloudify/cloud/ExamplesTest.java
@@ -2,6 +2,7 @@ package test.cli.cloudify.cloud;
 
 import java.io.IOException;
 
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import com.sun.jersey.api.client.Client;
@@ -13,6 +14,8 @@ import framework.utils.LogUtils;
 import framework.utils.ScriptUtils;
 
 public class ExamplesTest extends AbstractCloudTest {
+
+	private String applicationName;
 
 	public ExamplesTest() {
 		LogUtils.log("Instansiated " + ExamplesTest.class.getName());
@@ -29,12 +32,14 @@ public class ExamplesTest extends AbstractCloudTest {
 			throws IOException, InterruptedException {
 		doTest(cloudName, "petclinic", "petclinic");
 	}
+	
 
 	// petclinic-simple is covered by {@link ScalingRulesCloudTest}
 
 	protected void doTest(String cloudName, String applicationFolderName, String applicationName) throws IOException, InterruptedException {
 		LogUtils.log("installing application " + applicationName + " on " + cloudName);
 		setCloudToUse(cloudName);
+		this.applicationName = applicationName;
 		String applicationPath = ScriptUtils.getBuildPath() + "/recipes/apps/" + applicationFolderName;
 		try {
 			installApplicationAndWait(applicationPath, applicationName);
@@ -62,6 +67,26 @@ public class ExamplesTest extends AbstractCloudTest {
 				if (output.contains(applicationName)) {
 					uninstallApplicationAndWait(applicationName);			
 				}
+			}
+		}
+	}
+	
+	@AfterMethod
+	public void afterTest() {
+		if ((getService() != null) && (getService().getRestUrls() != null)) {
+			String command = "connect " + getRestUrl() + ";list-applications";
+			String output;
+			try {
+				output = CommandTestUtils.runCommandAndWait(command);
+				if (output.contains(applicationName)) {
+					uninstallApplicationAndWait(this.applicationName);			
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+				AssertFail(e.getMessage());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				AssertFail(e.getMessage());
 			}
 		}
 	}

163bcf3b817aca942e19dbb2c91427e5a5c31c65
noak <noak@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-06-13 19:08:35 +0000

    1. Updated to fit the new framework. 2. Updated to use the correct lookup groups on the admin object.
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@121195 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/cloud/MultipleMachineTemplatesTest.java
+++ src/test/cli/cloudify/cloud/MultipleMachineTemplatesTest.java
@@ -20,10 +20,10 @@ import org.testng.annotations.Test;
 import test.cli.cloudify.cloud.services.byon.ByonCloudService;
 import framework.tools.SGTestHelper;
 import framework.utils.AssertUtils;
+import framework.utils.AssertUtils.RepetitiveConditionProvider;
 import framework.utils.IOUtils;
 import framework.utils.LogUtils;
 import framework.utils.ScriptUtils;
-import framework.utils.AssertUtils.RepetitiveConditionProvider;
 
 
 /**
@@ -35,30 +35,28 @@ import framework.utils.AssertUtils.RepetitiveConditionProvider;
  */
 public class MultipleMachineTemplatesTest extends AbstractCloudTest{
 
-	private ByonCloudService service;
-	private String cloudName = "byon";
-	private static final String LOOKUPGROUP = "byon_group";
-	private File byonUploadDir = new File(ScriptUtils.getBuildPath() , "tools/cli/plugins/esc/byon/upload");
-	private File backupStartManagementFile = new File(byonUploadDir, "bootstrap-management.sh.backup");
-	private File originialBootstrapManagement = new File(byonUploadDir, "bootstrap-management.sh");
-	private File byonCloudConfDir = new File(ScriptUtils.getBuildPath() , "tools/cli/plugins/esc/byon");
-	private File backupCloudConfFile = new File(byonCloudConfDir, "byon-cloud.groovy.backup");
-	private File originialCloudConf = new File(byonCloudConfDir, "byon-cloud.groovy");
-	private File mongodbDir = new File(ScriptUtils.getBuildPath() , "recipes/services/mongodb");
-	private File tomcatDir = new File(ScriptUtils.getBuildPath() , "recipes/services");
-	private Cloud readCloud;
-	private boolean teardownFlag = false;
-	protected String TEMPLATE_1_IPs = "192.168.9.115,192.168.9.116";
-	protected String TEMPLATE_2_IPs = "192.168.9.120,192.168.9.125";
-	protected String TEMPLATE_3_IPs = "192.168.9.126";
-	private static final String DEFAULT_MACHINE_TEMPLATE = "SMALL_LINUX";
-	private static final String CLOUD_SERVICE_UNIQUE_NAME = "MultipleMachineTemplatesTest";
 	private static final int MONGOD_DEFAULT_INSTANCES_NUM = 2;
 	private static final int MONGOD_INSTANCES_NUM = 1;
 	public final static long MY_OPERATION_TIMEOUT = 1 * 60 * 1000;
+	private static final String DEFAULT_MACHINE_TEMPLATE = "SMALL_LINUX";
+	private static final String CLOUD_SERVICE_UNIQUE_NAME = "MultipleMachineTemplatesTest";
+	
+	private boolean teardownFlag = false;
+	private String cloudName = "byon";
+	private File mongodbDir = new File(ScriptUtils.getBuildPath() , "recipes/services/mongodb");
+	private File tomcatDir = new File(ScriptUtils.getBuildPath() , "recipes/services");
+	private ByonCloudService service;
+	
+	protected String TEMPLATE_1_IPs = "192.168.9.115,192.168.9.116";
+	protected String TEMPLATE_2_IPs = "192.168.9.120,192.168.9.125";
+	protected String TEMPLATE_3_IPs = "192.168.9.126";
 	
 	@BeforeClass(enabled = true)
 	public void before() throws IOException, InterruptedException, DSLException {
+		
+		// NOA: note, this next section is a hack. The test needs a special copy of byon-groovy and edits it before we create 
+		// the test's folder (byon_MultipleMachineTemplatesTest), so we must copy files and replace values here
+		// and eventually copy "byon-cloud.new" over "byon_MultipleMachineTemplatesTest\byon-cloud.groovy".
 		
 		setCloudService(cloudName, CLOUD_SERVICE_UNIQUE_NAME, false);
 		service = (ByonCloudService)getService();
@@ -66,21 +64,33 @@ public class MultipleMachineTemplatesTest extends AbstractCloudTest{
 			service.teardownCloud(); // tear down the existing byon cloud since we need a new bootstrap			
 		}
 		
-		service.setMachinePrefix(this.getClass().getName());
+		//use SGTest's special byon groovy (with templates) instead of the original one, but don't override the original file - use byon-new-cloud.groovy instead
+		File multiTemplatesGroovy = new File(SGTestHelper.getSGTestRootDir() + "/apps/cloudify/cloud/byon/byon-cloud.groovy");
+		File newCloudGroovy = new File(ScriptUtils.getBuildPath() + "/tools/cli/plugins/esc/byon/", "byon-cloud.new");
+		copyFile(multiTemplatesGroovy, newCloudGroovy);
 
-		backupAndReplaceOriginalFile(originialBootstrapManagement,SGTestHelper.getSGTestRootDir() + "/apps/cloudify/cloud/byon/bootstrap-management-multicast-and-byon-java-home.sh");
-		Map<String,String> replaceMap = new HashMap<String,String>();
+		//read the original file to get the download URL from it
+		File originialCloudGroovy = new File(ScriptUtils.getBuildPath() + "/tools/cli/plugins/esc/byon/", "byon-cloud.groovy");
+		Cloud origCloudConf = ServiceReader.readCloud(originialCloudGroovy);
+		String cloudifyUrl = origCloudConf.getProvider().getCloudifyUrl();
 		
-		readCloud = ServiceReader.readCloud(originialCloudConf);
-		String cloudifyUrl = readCloud.getProvider().getCloudifyUrl();
-		File newCloudConfFile = backupAndReplaceOriginalFile(originialCloudConf,SGTestHelper.getSGTestRootDir() + "/apps/cloudify/cloud/byon/byon-cloud.groovy");
-
+		//replace the Cloudify URL and nodes' IPs in the new groovy file
+		Map<String, String> replaceMap = new HashMap<String, String>();
 		replaceMap.put("cloudify.zip", cloudifyUrl);
 		replaceMap.put("1.1.1.1", TEMPLATE_1_IPs);
 		replaceMap.put("2.2.2.2", TEMPLATE_2_IPs);
 		replaceMap.put("3.3.3.3", TEMPLATE_3_IPs);
-
-		IOUtils.replaceTextInFile(newCloudConfFile.getAbsolutePath(), replaceMap);
+		replaceMap.put("localDirectory \"tools/cli/plugins/esc/byon/upload\"", "localDirectory \"tools/cli/plugins/esc/" + service.getServiceFolder() + "/upload\"");
+		IOUtils.replaceTextInFile(newCloudGroovy.getAbsolutePath(), replaceMap);
+		
+		// replace the default bootstrap-management and cloud groovy with the customized versions
+		File standardBootstrapManagement = new File(service.getPathToCloudFolder() + "/upload", "bootstrap-management.sh");
+		File bootstrapManagementCustomized = new File(SGTestHelper.getSGTestRootDir() + "/apps/cloudify/cloud/byon/bootstrap-management-" + service.getServiceFolder() + ".sh");
+		File fileToBeReplaced = new File(service.getPathToCloudFolder(), "byon-cloud.groovy");
+		Map<File, File> filesToReplace = new HashMap<File, File>();
+		filesToReplace.put(fileToBeReplaced, newCloudGroovy);
+		filesToReplace.put(standardBootstrapManagement, bootstrapManagementCustomized);
+		service.addFilesToReplace(filesToReplace);
 
 		//TODO : this is dangerous, need to fix
 		backupAndReplaceMachineTemplateInService("mongos", "TEMPLATE_1");
@@ -89,33 +99,18 @@ public class MultipleMachineTemplatesTest extends AbstractCloudTest{
 		backupAndReplaceMachineTemplateInService("tomcat", "TEMPLATE_2");
 		IOUtils.replaceTextInFile(mongodbDir.getAbsolutePath() + "/mongod/mongod-service.groovy", "numInstances " + MONGOD_DEFAULT_INSTANCES_NUM, "numInstances " + MONGOD_INSTANCES_NUM);
 		
+		LogUtils.log("creating admin");
+		AdminFactory factory = new AdminFactory();
+		factory.addGroup(CLOUD_SERVICE_UNIQUE_NAME);
+		admin = factory.createAdmin();
+		
+		LogUtils.log("bootstrapping byon cloud");
+		service.setMachinePrefix(this.getClass().getName());
 		service.bootstrapCloud();
 
 		if (service.getRestUrls() == null) {
 			Assert.fail("Test failed becuase the cloud was not bootstrapped properly");
 		}
-
-		LogUtils.log("creating admin");
-		AdminFactory factory = new AdminFactory();
-		factory.addGroup(LOOKUPGROUP);
-		admin = factory.createAdmin();
-
-	}
-
-	private File backupAndReplaceOriginalFile(File originalFile, String replacementFilePath) throws IOException {
-		// replace the default originalFile with a different one
-		// first make a backup of the original file
-		File backupFile = new File(originalFile.getAbsolutePath() + ".backup");
-		FileUtils.copyFile(originalFile, backupFile);
-
-		// copy replacement file to upload dir as the original file's name
-		File replacementFile = new File(replacementFilePath);
-
-		FileUtils.deleteQuietly(originalFile);
-		File newOriginalFile = new File(originalFile.getParent(), originalFile.getName());
-		FileUtils.copyFile(replacementFile, newOriginalFile);
-		
-		return newOriginalFile;
 	}
 
 	private void backupAndReplaceMachineTemplateInService(String serviceName, String newTemplate) throws IOException {
@@ -142,6 +137,7 @@ public class MultipleMachineTemplatesTest extends AbstractCloudTest{
 		String template2IPsArray[] = TEMPLATE_2_IPs.split(",");
 		String template3IPsArray[] = TEMPLATE_3_IPs.split(",");
 		
+		//TODO : edit this, so if it fails it won't be on NPE!
 		String hostAddressToCompare = admin.getProcessingUnits().getProcessingUnit("petclinic.mongod")
 				.getInstances()[0].getMachine().getHostAddress();
 		Assert.assertTrue(Arrays.asList(template2IPsArray).contains(hostAddressToCompare));
@@ -295,29 +291,29 @@ public class MultipleMachineTemplatesTest extends AbstractCloudTest{
 	@AfterClass(alwaysRun = true)
 	public void teardown() throws IOException, InterruptedException {
 		
-		/*setService(new ByonCloudService(CLOUD_SERVICE_UNIQUE_NAME));
-		restoreOriginalBootstrapManagementFile();
-		restoreOriginalByonCloudFile();*/
+		if (admin != null) {
+			admin.close();
+			admin = null;
+		}
+		
 		restoreOriginalServiceFile("mongos");
 		restoreOriginalServiceFile("mongod");
 		restoreOriginalServiceFile("tomcat");
 		restoreOriginalServiceFile("mongoConfig");
 		
+		LogUtils.log("tearing down byon cloud");
 		if(!teardownFlag)
 			service.teardownCloud();
 	}
+	
+	private void copyFile(File originalFile, File targetFile) throws IOException {
+		if (targetFile.exists()) {
+			targetFile.delete();		
+		}
 
-/*	private void restoreOriginalBootstrapManagementFile() throws IOException {
-		originialBootstrapManagement.delete();
-		FileUtils.moveFile(backupStartManagementFile, originialBootstrapManagement);
-
+		// copy replacement file with the original file's name
+		FileUtils.copyFile(originalFile, targetFile);
 	}
-
-	private void restoreOriginalByonCloudFile() throws IOException {
-		originialCloudConf.delete();
-		FileUtils.moveFile(backupCloudConfFile, originialCloudConf);
-
-	}*/
 	
 	private void restoreOriginalServiceFile(String serviceName) throws IOException {
 		

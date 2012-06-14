3feab331e91ffe3925ccea8a6a4439ba910923ad
barakme <barakme@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-06-14 09:06:28 +0000

    Fixed image/hardware ID details test
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@121231 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/cloud/ExamplesTest.java
+++ src/test/cli/cloudify/cloud/ExamplesTest.java
@@ -2,7 +2,10 @@ package test.cli.cloudify.cloud;
 
 import java.io.IOException;
 
+import org.cloudifysource.dsl.internal.CloudifyConstants;
+
 import org.testng.annotations.AfterMethod;
+
 import org.testng.annotations.Test;
 
 import com.sun.jersey.api.client.Client;
@@ -18,7 +21,6 @@ public class ExamplesTest extends AbstractCloudTest {
 	private static final String CLOUD_SERVICE_UNIQUE_NAME = "ExamplesTest";
 	private String applicationName;
 
-	
 	public ExamplesTest() {
 		LogUtils.log("Instansiated " + ExamplesTest.class.getName());
 	}
@@ -34,45 +36,49 @@ public class ExamplesTest extends AbstractCloudTest {
 			throws IOException, InterruptedException {
 		doTest(cloudName, "petclinic", "petclinic");
 	}
-	
 
 	// petclinic-simple is covered by {@link ScalingRulesCloudTest}
 
-	protected void doTest(String cloudName, String applicationFolderName, String applicationName) throws IOException, InterruptedException {
+	protected void doTest(String cloudName, String applicationFolderName, String applicationName)
+			throws IOException, InterruptedException {
 		LogUtils.log("installing application " + applicationName + " on " + cloudName);
 		setCloudService(cloudName, CLOUD_SERVICE_UNIQUE_NAME, true);
 		this.applicationName = applicationName;
 		String applicationPath = ScriptUtils.getBuildPath() + "/recipes/apps/" + applicationFolderName;
 		try {
 			installApplicationAndWait(applicationPath, applicationName);
-			if(applicationName.equals("travel") && cloudName.equals("ec2")) {
+			if (applicationName.equals("travel") && cloudName.equals("ec2")) {
 				// verify image and hardware ID only for travel on EC2
 				Client client = Client.create(new DefaultClientConfig());
 				final WebResource service = client.resource(this.getRestUrl());
-				String hardwareIDResult= service.path("/admin/ProcessingUnits/Names/travel.tomcat/Instances/0/Statistics/Monitors/USM/Details/Attributes/CLOUDIFY_CLOUD_HARDWARE_ID").get(String.class);
+				String hardwareIDResult =
+						service.path(
+								"/admin/ProcessingUnits/Names/travel.tomcat/Instances/0/Statistics/Monitors/USM/Details/Attributes/"
+										+ CloudifyConstants.USM_DETAILS_HARDWARE_ID).get(String.class);
 				assertTrue("error reading hardware id", !hardwareIDResult.contains("error"));
-				assertTrue("Could not find expected hardware ID: m1.small in response", hardwareIDResult.contains("m1.small"));
-				
-				String imageIDResult= service.path("/admin/ProcessingUnits/Names/travel.tomcat/Instances/0/Statistics/Monitors/USM/Details/Attributes/CLOUDIFY_CLOUD_IMAGE_ID").get(String.class);
+				assertTrue("Could not find expected hardware ID: m1.small in response",
+						hardwareIDResult.contains("m1.small"));
+
+				String imageIDResult =
+						service.path(
+								"/admin/ProcessingUnits/Names/travel.tomcat/Instances/0/Statistics/Monitors/USM/Details/Attributes/" + CloudifyConstants.USM_DETAILS_IMAGE_ID)
+								.get(String.class);
 				assertTrue("error reading image id", !imageIDResult.toLowerCase().contains("error"));
-				
-				
 
 			}
 			// http://localhost:8100
-			
-		}
-		finally {
+
+		} finally {
 			if ((getService() != null) && (getService().getRestUrls() != null)) {
 				String command = "connect " + getRestUrl() + ";list-applications";
 				String output = CommandTestUtils.runCommandAndWait(command);
 				if (output.contains(applicationName)) {
-					uninstallApplicationAndWait(applicationName);			
+					uninstallApplicationAndWait(applicationName);
 				}
 			}
 		}
 	}
-	
+
 	@AfterMethod
 	public void afterTest() {
 		if ((getService() != null) && (getService().getRestUrls() != null)) {
@@ -81,7 +87,7 @@ public class ExamplesTest extends AbstractCloudTest {
 			try {
 				output = CommandTestUtils.runCommandAndWait(command);
 				if (output.contains(applicationName)) {
-					uninstallApplicationAndWait(this.applicationName);			
+					uninstallApplicationAndWait(this.applicationName);
 				}
 			} catch (IOException e) {
 				e.printStackTrace();

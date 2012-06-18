a796e318c8f74dbdc172f79279c293ed2f7e1d01
adaml <adaml@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-06-18 16:05:57 +0000

    added a test for second service failover. this test is excpected to fail due to the gsm/usm class loader issue.
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@122268 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/InternalUSMPuServiceDownTest.java
+++ src/test/cli/cloudify/InternalUSMPuServiceDownTest.java
@@ -6,13 +6,14 @@ import java.io.File;
 import java.io.IOException;
 import java.net.ConnectException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.UnknownHostException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.http.HttpStatus;
-import org.cloudifysource.dsl.internal.packaging.PackagingException;
 import org.cloudifysource.dsl.utils.ServiceUtils;
 import org.cloudifysource.usm.USMException;
 import org.cloudifysource.usm.shutdown.DefaultProcessKiller;
@@ -41,11 +42,11 @@ import groovy.util.ConfigSlurper;
 
 public class InternalUSMPuServiceDownTest extends AbstractLocalCloudTest {
 	
-	ProcessingUnit tomcat;
-	Long tomcatPId;
-	Machine machineA;
+
 	WebClient client;
 	private final String TOMCAT_URL = "http://127.0.0.1:8080";
+	private String serviceDir = ScriptUtils.getBuildPath() + "/recipes/services/tomcat";
+	private ProcessingUnitInstanceLifecycleEventListener eventListener;
 
 	@Override
 	@BeforeMethod
@@ -55,38 +56,88 @@ public class InternalUSMPuServiceDownTest extends AbstractLocalCloudTest {
 	}
 	
 	@SuppressWarnings("deprecation")
-	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, groups = "1", enabled = true)
-	public void tomcatServiceDownAndCorruptedTest() throws IOException, InterruptedException, PackagingException {
+	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, groups = "1", enabled = false)
+	public void tomcatServiceDownAndCorruptedTest() throws IOException, InterruptedException {
 		
-		String serviceDir = ScriptUtils.getBuildPath() + "/recipes/services/tomcat";
-		String command = "connect " + this.restUrl + ";" + "install-service " + "--verbose -timeout 10 " + serviceDir;
-		try {
-			LogUtils.log("installing tomcat service using Cli");
-			CommandTestUtils.runCommandAndWait(command);
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-		LogUtils.log("Retrieving tomcat process pid from admin");
-		tomcat = admin.getProcessingUnits().waitFor(ServiceUtils.getAbsolutePUName("default", "tomcat"), 10, TimeUnit.SECONDS);
-		assertNotNull(tomcat);
-		assertTrue("USM Service state is not RUNNING", USMTestUtils.waitForPuRunningState("default.tomcat", 100, TimeUnit.SECONDS, admin));
-		ProcessingUnitUtils.waitForDeploymentStatus(tomcat, DeploymentStatus.INTACT);
-		assertTrue(tomcat.getStatus().equals(DeploymentStatus.INTACT));
-		
-		ProcessingUnitInstance tomcatInstance = tomcat.getInstances()[0];	
-		assertTrue("Tomcat instance is not in RUNNING State", USMTestUtils.waitForPuRunningState("default.tomcat", 60, TimeUnit.SECONDS, admin));
-		CustomServiceMonitors customServiceDetails = (CustomServiceMonitors) tomcatInstance.getStatistics().getMonitors().get("USM");
-		GridServiceContainer container = tomcatInstance.getGridServiceContainer();
-		machineA = container.getMachine();
-		tomcatPId = (Long) customServiceDetails.getMonitors().get("USM_Actual Process ID");
-		
+		installTomcat();
 		
 		final CountDownLatch removed = new CountDownLatch(1);
 		final CountDownLatch added = new CountDownLatch(2);
 		
+		addLifecycleListnersToTomcatPu(removed, added);
+		
+		deleteCatalinaExec(serviceDir);
+		
+		Long tomcatPId = getTomcatPId();
+		
+		killTomcatProcess(tomcatPId);
+		
+		waitForServiceRecovery(removed, added);
+	}
+
+	@SuppressWarnings("deprecation")
+	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, groups = "1", enabled = true)
+	public void tomcatServiceDownAndCorruptedTwiceTest() throws IOException, InterruptedException {
+		
+		installTomcat();
+		
+		CountDownLatch removed;
+		CountDownLatch added;
+		
+		for (int i = 0; i < 2; i++) {
+			LogUtils.log("Starting the " + (i + 1) + " consequtive hard failover.");
+			removed = new CountDownLatch(1);
+			added = new CountDownLatch(2);
+			
+			addLifecycleListnersToTomcatPu(removed, added);
+			
+			deleteCatalinaExec(serviceDir);
+			
+			Long tomcatPId = getTomcatPId();
+			
+			killTomcatProcess(tomcatPId);
+			
+			waitForServiceRecovery(removed, added);
+			
+			removeLifecycleListnersFromTomcatPu();
+		}
+		
+	}
+	private void removeLifecycleListnersFromTomcatPu() {
+		ProcessingUnit tomcatPu = getTomcatPu();
+		tomcatPu.removeLifecycleListener(this.eventListener);
+	}
+
+	private ProcessingUnitInstance getTomcatInstance(){
+		ProcessingUnit tomcatPu = getTomcatPu();
+		ProcessingUnitInstance tomcatInstance = tomcatPu.getInstances()[0];	
+		return tomcatInstance;
+	}
+	
+	private Long getTomcatPId() {
+		ProcessingUnitInstance tomcatInstance = getTomcatInstance();
+		CustomServiceMonitors customServiceDetails = (CustomServiceMonitors) tomcatInstance.getStatistics().getMonitors().get("USM");
+		Long tomcatPId = (Long) customServiceDetails.getMonitors().get("USM_Actual Process ID");
+		return tomcatPId;
+	}
+	
+
+	private void waitForServiceRecovery(final CountDownLatch removed,
+			final CountDownLatch added) throws InterruptedException {
+		LogUtils.log("waiting for tomcat pu instances to decrease");
+		assertTrue("Tomcat PU instance was not decresed", removed.await(240, TimeUnit.SECONDS));
+		LogUtils.log("waiting for tomcat pu instances to increase");
+		added.await(60 * 6, TimeUnit.SECONDS);
+		assertTrue("ProcessingUnitInstanceAdded event has not been fired", added.getCount() == 0);	
+		LogUtils.log("verifiying tomcat service in running");
+		assertTomcatPageExists(client);	
+		LogUtils.log("all's well that ends well :)");
+		
+
+	}
+
+	private void addLifecycleListnersToTomcatPu(final CountDownLatch removed,
+			final CountDownLatch added) {
 		LogUtils.log("adding a lifecycle listener to tomcat pu");
 		ProcessingUnitInstanceLifecycleEventListener eventListener = new ProcessingUnitInstanceLifecycleEventListener() {
 			
@@ -104,8 +155,13 @@ public class InternalUSMPuServiceDownTest extends AbstractLocalCloudTest {
 				added.countDown();	
 			}
 		};
+		ProcessingUnit tomcat = getTomcatPu();
 		tomcat.addLifecycleListener(eventListener);
-		
+		this.eventListener = eventListener;
+	}
+
+	private void deleteCatalinaExec(String serviceDir)
+			throws MalformedURLException {
 		String pathToTomcat;
 		
 		LogUtils.log("deleting catalina.sh/bat from pu folder");
@@ -123,7 +179,9 @@ public class InternalUSMPuServiceDownTest extends AbstractLocalCloudTest {
 		File tomcatRun = new File(pathToTomcat);
 		
 		assertTrue("failed while deleting file: " + tomcatRun, tomcatRun.delete());
-		
+	}
+
+	private void killTomcatProcess(long tomcatPId) throws IOException {
 		LogUtils.log("killing tomcat process : " + tomcatPId);
 		DefaultProcessKiller dpk = new DefaultProcessKiller();
 		try {
@@ -134,26 +192,44 @@ public class InternalUSMPuServiceDownTest extends AbstractLocalCloudTest {
 		
 		int responseCode = getResponseCode(TOMCAT_URL);
 		assertTrue("Tomcat service is still running. Request returned response code: " + responseCode, HttpStatus.SC_NOT_FOUND == responseCode);
-		
-		LogUtils.log("waiting for tomcat pu instances to decrease");
-		assertTrue("Tomcat PU instance was not decresed", removed.await(240, TimeUnit.SECONDS));
-		LogUtils.log("waiting for tomcat pu instances to increase");
-		added.await(60 * 6, TimeUnit.SECONDS);
-		assertTrue("ProcessingUnitInstanceAdded event has not been fired", added.getCount() == 0);	
-		LogUtils.log("verifiying tomcat service in running");
-		assertTomcatPageExists(client);	
-		LogUtils.log("all's well that ends well :)");
+	}
+
+	private void installTomcat() throws UnknownHostException {
+		String command = "connect " + this.restUrl + ";" + "install-service " + "--verbose -timeout 10 " + this.serviceDir;
+		try {
+			LogUtils.log("installing tomcat service using Cli");
+			CommandTestUtils.runCommandAndWait(command);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		LogUtils.log("Retrieving tomcat process pid from admin");
+		ProcessingUnit tomcat = getTomcatPu();
+		assertTrue("USM Service state is not RUNNING", USMTestUtils.waitForPuRunningState("default.tomcat", 100, TimeUnit.SECONDS, admin));
+		ProcessingUnitUtils.waitForDeploymentStatus(tomcat, DeploymentStatus.INTACT);
+		assertTrue(tomcat.getStatus().equals(DeploymentStatus.INTACT));
+		assertTrue("Tomcat instance is not in RUNNING State", USMTestUtils.waitForPuRunningState("default.tomcat", 60, TimeUnit.SECONDS, admin));
 	}
 	
+	private ProcessingUnit getTomcatPu() {
+		ProcessingUnit tomcat = admin.getProcessingUnits().waitFor(ServiceUtils.getAbsolutePUName("default", "tomcat"), 10, TimeUnit.SECONDS);
+		assertNotNull("Tomcat instace is null", tomcat);
+		return tomcat;
+	}
+
 	private boolean isWindows() {
 		return (System.getenv("windir") != null);
 	}
 	
 	private void assertTomcatPageExists(WebClient client) {
+		ProcessingUnitInstance tomcatInstance = getTomcatInstance();
+		GridServiceContainer container = tomcatInstance.getGridServiceContainer();		
+		Machine tomcatMachine = container.getMachine();
 		
         HtmlPage page = null;
         try {
-            page = client.getPage("http://" + machineA.getHostAddress() + ":8080");
+            page = client.getPage("http://" + tomcatMachine.getHostAddress() + ":8080");
         } catch (IOException e) {
             fail(e.getMessage());
         }

019c57d81fc213de65537765513d27eeaea5fa66
barakme <barakme@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-06-28 07:10:56 +0000

    Test for USM monitors caching
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@122737 eb64e737-3616-4df0-8941-5ee2ae88103d

---
+++ apps/USM/usm/simpleGroovy/groovy-service.groovy
@@ -0,0 +1,39 @@
+import java.util.concurrent.TimeUnit
+
+
+service {
+	name "groovy"
+	icon "icon.png"
+	type "WEB_SERVER"
+	elastic true
+	numInstances 1
+	maxAllowedInstances 1
+	lifecycle { 
+	
+		init { println "This is the init event" }
+		preInstall {println "This is the preInstall event" }
+		postInstall {println "This is the postInstall event"}
+		preStart {println "This is the preStart event" }
+		
+		start "run.groovy" 
+		
+		postStart {println "This is the postStart event" }
+		preStop {println "This is the preStop event" }
+		postStop {println "This is the postStop event" }
+		shutdown {println "This is the shutdown event" }
+		
+		startDetection {
+			new File(context.serviceDirectory + "/marker.txt").exists()
+		}
+		
+		monitors {
+			def time = System.currentTimeMillis()
+			println "Monitors executed. Time is: " + time
+			return [
+				"time" : time]
+		}
+		
+		
+	}
+
+}

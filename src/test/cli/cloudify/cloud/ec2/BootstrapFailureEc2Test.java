fae8807d9c93a2794b2c9773659a584fa0474c5b
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-07-25 15:00:28 +0000

    fix test to extend from NewAbstractCloudTest
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@124687 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/cloud/ec2/BootstrapFailureEc2Test.java
+++ src/test/cli/cloudify/cloud/ec2/BootstrapFailureEc2Test.java
@@ -9,18 +9,20 @@ import java.util.Set;
 import org.jclouds.compute.domain.NodeMetadata;
 import org.jclouds.compute.domain.NodeState;
 import org.testng.Assert;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import test.cli.cloudify.CloudTestUtils;
-import test.cli.cloudify.cloud.AbstractCloudTest;
 import test.cli.cloudify.cloud.JcloudsUtils;
+import test.cli.cloudify.cloud.NewAbstractCloudTest;
 import test.cli.cloudify.cloud.services.ec2.Ec2CloudService;
 import framework.tools.SGTestHelper;
 import framework.utils.AssertUtils;
 import framework.utils.AssertUtils.RepetitiveConditionProvider;
-import framework.utils.LogUtils;
 
 /**
  * This test makes a bootstrap on ec2 fail by changing the JAVA_HOME path to a bad one in the bootstrap-management.sh file.
@@ -29,9 +31,8 @@ import framework.utils.LogUtils;
  * @author nirb
  *
  */
-public class BootstrapFailureEc2Test extends AbstractCloudTest{
+public class BootstrapFailureEc2Test extends NewAbstractCloudTest {
 
-	private static final String CLOUD_SERVICE_UNIQUE_NAME = "BootstrapFailureEc2Test";
 	private Ec2CloudService service;
 	private NodeMetadata managementMachine;
 	private static final long TIME_TO_TERMINATE_IN_MILLS = 60000;
@@ -40,35 +41,31 @@ public class BootstrapFailureEc2Test extends AbstractCloudTest{
 
 	@BeforeMethod
 	public void init() throws IOException, InterruptedException {	
-		
-		setCloudService("EC2", CLOUD_SERVICE_UNIQUE_NAME, false);
-		service = (Ec2CloudService)getService();
+		service = new Ec2CloudService(this.getClass().getName());
 		service.setMachinePrefix(machineName);	
 	}
-	
-	
-	
+
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, enabled = true)
-	public void installTest() throws IOException, InterruptedException {
-		
+	public void failedBootstrapTest() throws IOException, InterruptedException {
+
 		//replace the bootstrap-management with a bad version, to fail the bootstrap.
 		File standardBootstrapManagement = new File(service.getPathToCloudFolder() + "/upload", "bootstrap-management.sh");
 		File badBootstrapManagement = new File(SGTestHelper.getSGTestRootDir() + "/apps/cloudify/cloud/ec2/bad-bootstrap-management.sh");
 		Map<File, File> filesToReplace = new HashMap<File, File>();
 		filesToReplace.put(standardBootstrapManagement, badBootstrapManagement);
 		service.addFilesToReplace(filesToReplace);
-		
+
 		try {
 			service.bootstrapCloud();
 		} catch (AssertionError ae) {
 			System.out.println(ae.getMessage());
 		}
-	
+
 		JcloudsUtils.createContext(service);
 		Set<? extends NodeMetadata> machines = JcloudsUtils.getServersByName(machineName);
 		Assert.assertTrue(machines != null);
 		managementMachine = machines.iterator().next();
-				
+
 		RepetitiveConditionProvider condition = new RepetitiveConditionProvider() {
 			@Override
 			public boolean getCondition() {
@@ -80,23 +77,32 @@ public class BootstrapFailureEc2Test extends AbstractCloudTest{
 				return managementMachineTerminated;
 			}
 		};
-		
+
 		AssertUtils.repetitiveAssertTrue("management machine was not terminated", condition, TIME_TO_TERMINATE_IN_MILLS);
 	}
 
-	@AfterMethod(alwaysRun = true)
+	@AfterClass
 	public void teardown() {
-		JcloudsUtils.closeContext();
-		
-		try {
-			if (!managementMachineTerminated) {
-				service.teardownCloud();				
-			}
-		}
-		catch (Throwable e) {
-			LogUtils.log("caught an exception while tearing down ec2", e);
-			sendTeardownCloudFailedMail("ec2", e);
-		}
+		JcloudsUtils.closeContext();		
+		if (!managementMachineTerminated) {
+			super.teardown();			
+		}		
 	}
-	
+
+	@Override
+	protected String getCloudName() {
+		return "ec2";
+	}
+
+	@Override
+	protected boolean isReusableCloud() {
+		return false;
+	}
+
+	@Override
+	protected void customizeCloud() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
 }

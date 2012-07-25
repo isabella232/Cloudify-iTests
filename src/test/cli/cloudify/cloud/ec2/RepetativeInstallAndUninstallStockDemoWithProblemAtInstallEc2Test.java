570853024b5aa211079da0a4d7d7c3c77a9be355
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-07-25 16:29:32 +0000

    fix test to use NewAbstractCloudService instead of the old AbstractCloudService
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@124702 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/cloud/ec2/RepetativeInstallAndUninstallStockDemoWithProblemAtInstallEc2Test.java
+++ src/test/cli/cloudify/cloud/ec2/RepetativeInstallAndUninstallStockDemoWithProblemAtInstallEc2Test.java
@@ -2,18 +2,21 @@ package test.cli.cloudify.cloud.ec2;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import junit.framework.Assert;
 
 import org.apache.commons.io.FileUtils;
-import org.testng.annotations.AfterMethod;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import test.cli.cloudify.CloudTestUtils;
 import test.cli.cloudify.CommandTestUtils;
-import test.cli.cloudify.cloud.AbstractCloudTest;
+import test.cli.cloudify.cloud.NewAbstractCloudTest;
 import test.cli.cloudify.cloud.services.ec2.Ec2CloudService;
 import framework.tools.SGTestHelper;
 import framework.utils.LogUtils;
@@ -25,98 +28,79 @@ import framework.utils.WebUtils;
 	1. try to install stockdemo on and fail<p>
 	2. uninstall<p>
 	3. try to install again <p>
-	
+
 	Details: the failour at step 1 is achieved by renaming cassandra's post start script.
 	before step 3 is done the script is renamed back to the original name, this is why step 3 
 	is asserted.
  * @author gal
  *
  */
-public class RepetativeInstallAndUninstallStockDemoWithProblemAtInstallEc2Test extends AbstractCloudTest {
+public class RepetativeInstallAndUninstallStockDemoWithProblemAtInstallEc2Test extends NewAbstractCloudTest {
 
-	private static final String STOCKDEMO_APP_NAME = "stockdemo";
-	private static final String CLOUD_SERVICE_UNIQUE_NAME = "RepetativeInstallAndUninstallStockDemoWithProblemAtInstallEc2Test";
 	private final int repetitions = 1;
 	private String cassandraPostStartScriptPath = null;
 	private String newPostStartScriptPath = null;
 	private Ec2CloudService service;
-	private URL stockdemoUrl;
 	private String restUrl;
 	private final String stockdemoAppPath = CommandTestUtils.getPath("apps/USM/usm/applications/stockdemo");
-	
+
 	private File cloudPluginDir = new File(ScriptUtils.getBuildPath() + "/tools/cli/plugins/esc/ec2");
-	private File bootstapManagementInSGTest = new File(SGTestHelper.getSGTestRootDir() + "/apps/cloudify/cloud/ec2/bootstrap-management.sh");
-	private File bootstapManagementInBuild = new File(cloudPluginDir.getAbsolutePath() + "/upload/bootstrap-management.sh");
-	private File bootstrapManagementBackup = new File(cloudPluginDir.getAbsolutePath() + "/upload/bootstrap-management.backup");
+	private URL stockdemoUrl;
 
 
-	
-	@BeforeMethod
-	public void bootstrap() throws IOException, InterruptedException {	
-		
-		LogUtils.log("replacing original bootstrap-management file with a one that installs open jdk");
-		FileUtils.copyFile(bootstapManagementInBuild, bootstrapManagementBackup);
-		FileUtils.copyFile(bootstapManagementInSGTest, bootstapManagementInBuild);
-		
+	@BeforeClass
+	public void bootstrap(ITestContext iTestContext) {	
+
 		LogUtils.log("put cloudify-xap license in upload dir, needed to run stockdemo app");
 		File xapLicense = new File(SGTestHelper.getSGTestRootDir() + "/apps/cloudify/cloud/gslicense.xml");
 		File cloudifyOverrides = new File(cloudPluginDir.getAbsolutePath() + "/upload/cloudify-overrides");
 		if (!cloudifyOverrides.exists()) {
 			cloudifyOverrides.mkdir();
 		}
-		FileUtils.copyFileToDirectory(xapLicense, cloudifyOverrides);
-		
-		setCloudService("EC2", CLOUD_SERVICE_UNIQUE_NAME, false);
-		service = (Ec2CloudService)getService();
-		service.setMachinePrefix(this.getClass().getName() + CloudTestUtils.SGTEST_MACHINE_PREFIX);
-		service.bootstrapCloud();
-		restUrl = super.getRestUrl();
-		//String hostIp = restUrl.substring(0, restUrl.lastIndexOf(':'));
-		//stockdemoUrl = new URL(hostIp + ":8080/stockdemo.StockDemo/");
-	}
-	
-	
-	@AfterMethod(alwaysRun = true)
-	public void teardown() throws IOException {
 		try {
-			String command = "connect " + super.getRestUrl() + ";list-applications";
-			String output = CommandTestUtils.runCommandAndWait(command);
-			if (output.contains(STOCKDEMO_APP_NAME)) {
-				uninstallApplicationAndWait(STOCKDEMO_APP_NAME);			
-			}
-			service.teardownCloud();
+			FileUtils.copyFileToDirectory(xapLicense, cloudifyOverrides);
 		}
-		catch (Throwable e) {
-			LogUtils.log("caught an exception while tearing down ec2", e);
-			sendTeardownCloudFailedMail("ec2", e);
+		catch (IOException e) {
+			AssertFail(e.getMessage());
 		}
-		LogUtils.log("restoring original bootstrap-management file");
-		FileUtils.copyFile(bootstrapManagementBackup, bootstapManagementInBuild);
-		FileUtils.deleteQuietly(bootstrapManagementBackup);
-		
+
+		service = new Ec2CloudService(this.getClass().getName());
+		service.setMachinePrefix(this.getClass().getName() + CloudTestUtils.SGTEST_MACHINE_PREFIX);
+
+		super.bootstrap(iTestContext, service);
+
+		restUrl = super.getRestUrl();
+
+		cloudPluginDir = new File(service.getPathToCloudFolder());
+		String hostIp = restUrl.substring(0, restUrl.lastIndexOf(':'));
+		try {
+			stockdemoUrl = new URL(hostIp + ":8080/stockdemo.StockDemo/");
+		} catch (MalformedURLException e) {
+			AssertFail(e.getMessage() , e);
+		}
 	}
-	
+
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 6, groups = "1", enabled = true)
 	public void installAndUninstallTest() throws Exception {
-		
+
 		cassandraPostStartScriptPath = stockdemoAppPath + "/cassandra/cassandra_poststart.groovy";	
 		newPostStartScriptPath = stockdemoAppPath + "/cassandra/cassandra_poststart123.groovy";
 		int secondInstallationSuccessCounter = 0;
 		int secondInstallationFailCounter = 0;
 		int firstInstallSuccessCounter = 0;
-		
+
 		for(int i=0 ; i < repetitions ; i++){
 			LogUtils.log("starting iteration " + i);
 			switch(installUninstallInstall(stockdemoAppPath, cassandraPostStartScriptPath ,newPostStartScriptPath)){
 			case 1: {firstInstallSuccessCounter++;
-					break;
-					}
+			break;
+			}
 			case 2: {secondInstallationSuccessCounter++;
-					break;
-					}
+			break;
+			}
 			case 3: {secondInstallationFailCounter++;
-					break;
-					}				
+			break;
+			}				
 			}
 			LogUtils.log("uninstalling stockdemo after iteration " + i);
 			uninstallApplicationAndWait("stockdemo");
@@ -132,10 +116,10 @@ public class RepetativeInstallAndUninstallStockDemoWithProblemAtInstallEc2Test e
 	private int installUninstallInstall(String stockdemoAppPath, String cassandraPostStartScriptPath ,String  newPostStartScriptPath) throws Exception {
 		LogUtils.log("corrupting cassandra service");
 		corruptCassandraService(cassandraPostStartScriptPath ,newPostStartScriptPath);
-		try{
+		try {
 			LogUtils.log("first installation of stockdemo - this should fail");
 			installApplication(stockdemoAppPath, "stockdemo", 5, true, true);		
-		}catch(AssertionError e){
+		} catch(AssertionError e){
 			return 1;
 		}
 		LogUtils.log("fixing cassandra service");
@@ -144,44 +128,33 @@ public class RepetativeInstallAndUninstallStockDemoWithProblemAtInstallEc2Test e
 		uninstallApplicationAndWait("stockdemo");
 		LogUtils.log("asserting all services are down");
 		assertUninstallWasSuccessful();
-		try{
+		try {
 			LogUtils.log("second installation of stockdemo - this should succeed");
 			installApplication(stockdemoAppPath, "stockdemo", 45, true, false);
-//			LogUtils.log("checking second installation's result");
-//			Assert.assertTrue("The applications home page isn't available, counts as not installed properly" ,
-//					WebUtils.isURLAvailable(stockdemoUrl));
+			LogUtils.log("checking second installation's result");
+			Assert.assertTrue("The applications home page isn't available, counts as not installed properly" , WebUtils.isURLAvailable(stockdemoUrl));
 			return 2;
-		}catch(AssertionError e){
+		} catch(AssertionError e){
 			LogUtils.log(e.getMessage());
 			return 3;
 		}
 
-	}
-	
-	@Override
-	@AfterMethod
-	public void afterTest(){
-		try {
-			fixCassandraService(cassandraPostStartScriptPath , newPostStartScriptPath);
-		} catch (IOException e) {
-			LogUtils.log("FAILED FIXING CASSANDRA SERVICE AFTER TEST");
-		}
 	}
 
 	private void corruptCassandraService(String cassandraPostStartScriptPath , String newPostStartScriptPath) throws IOException {
 		File cassandraPostStartScript = new File(cassandraPostStartScriptPath);
 		FileUtils.moveFile(cassandraPostStartScript, new File(newPostStartScriptPath));
 	}
-	
+
 	private void fixCassandraService(String cassandraPostStartScriptPath , String newPostStartScriptPath) throws IOException {
 		File cassandraPostStartScript = new File(newPostStartScriptPath);
 		boolean success = cassandraPostStartScript.renameTo(new File(cassandraPostStartScriptPath));
 		if(!success)
 			throw new IOException("Test error: failed renaming " +  newPostStartScriptPath + " to " + cassandraPostStartScriptPath);
 	}
-	
+
 	private void assertUninstallWasSuccessful() throws Exception{
-		
+
 		URL cassandraPuAdminUrl = new URL(restUrl + "/admin/ProcessingUnits/Names/stockdemo.cassandra");
 		URL stockAnalyticsMirrorPuAdminUrl = new URL(restUrl + "/admin/ProcessingUnits/Names/stockdemo.stockAnalyticsMirror");
 		URL stockAnalyticsSpacePuAdminUrl = new URL(restUrl + "/admin/ProcessingUnits/Names/stockdemo.stockAnalyticsSpace");
@@ -189,7 +162,7 @@ public class RepetativeInstallAndUninstallStockDemoWithProblemAtInstallEc2Test e
 		URL StockDemoPuAdminUrl = new URL(restUrl + "/admin/ProcessingUnits/Names/stockdemo.StockDemo");
 		URL stockAnalyticsPuAdminUrl = new URL(restUrl + "/admin/ProcessingUnits/Names/stockdemo.stockAnalytics");
 		URL stockAnalyticsFeederPuAdminUrl = new URL(restUrl + "/admin/ProcessingUnits/Names/stockdemo.stockAnalyticsFeeder");
-		
+
 		assertTrue(!WebUtils.isURLAvailable(cassandraPuAdminUrl));
 		assertTrue(!WebUtils.isURLAvailable(stockAnalyticsMirrorPuAdminUrl));
 		assertTrue(!WebUtils.isURLAvailable(stockAnalyticsSpacePuAdminUrl));
@@ -199,8 +172,29 @@ public class RepetativeInstallAndUninstallStockDemoWithProblemAtInstallEc2Test e
 		assertTrue(!WebUtils.isURLAvailable(stockAnalyticsFeederPuAdminUrl));
 	}
 	
+	@AfterClass
+	public void teardown() {
+		super.teardown();
+		super.scanNodesLeak();
+	}
 
-	
 
-	
+	@Override
+	protected String getCloudName() {
+		return "ec2";
+	}
+
+
+	@Override
+	protected boolean isReusableCloud() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	protected void customizeCloud() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
 }

f18ee74ecbd79d1046f661baf42dcbc621496bd0
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-07-26 15:12:31 +0000

    fix xcopy command
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@124776 eb64e737-3616-4df0-8941-5ee2ae88103d

--- deploy/bin/windows/download_processing_units.bat
+++ deploy/bin/windows/download_processing_units.bat
@@ -5,7 +5,7 @@
 if exist %LOCAL_SGPATH%\apps rmdir %LOCAL_SGPATH%\apps /s /q
 
 @echo copying new apps to local sgtest folder...
-xcopy %SGTEST_CHECKOUT_FOLDER%\apps %LOCAL_SGPATH%\apps /e /i
+xcopy %SGTEST_CHECKOUT_FOLDER%\apps %LOCAL_SGPATH%\apps /s /i /y
 
 @echo deleting current selenium jar from local sgtest folder...
 del %LOCAL_SGPATH%\lib\selenium\selenium-java-*.jar

041f0e72ed67d1d74494a42e14b0f65aa5dad06c
barakme <barakme@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-07-30 09:21:59 +0000

    Updated test to work with async install in USM
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@124903 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/HttpStartDetectionTest.java
+++ src/test/cli/cloudify/HttpStartDetectionTest.java
@@ -3,70 +3,73 @@ package test.cli.cloudify;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.cloudifysource.dsl.utils.ServiceUtils;
 import org.openspaces.admin.machine.Machine;
-import org.openspaces.admin.pu.DeploymentStatus;
 import org.openspaces.admin.pu.ProcessingUnit;
 import org.testng.annotations.Test;
 
 import test.usm.USMTestUtils;
-
-import org.cloudifysource.dsl.utils.ServiceUtils;
-
 import framework.utils.LogUtils;
 
 /**
  * 
  * @author adaml
- *
+ * 
  */
 public class HttpStartDetectionTest extends AbstractLocalCloudTest {
-	
+
 	private Machine machineA;
 	public static final String USM_APPLICATION_NAME = "default";
 	public static final String USM_SERVICE_NAME = "tomcat";
-	
+
 	/**
-	 * deploy tomcat on port 8081(VIA Recipe) and try to assert
-	 * deployment of tomcat on port 8080 using HttpStartDetection.
-	 * Look at recipe tomcatHttpStartDetection for more details.
+	 * deploy tomcat on port 8081(VIA Recipe) and try to assert deployment of tomcat on port 8080 using
+	 * HttpStartDetection. Look at recipe tomcatHttpStartDetection for more details.
 	 * 
 	 * @throws IOException
 	 * @throws InterruptedException
 	 */
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, groups = "1")
-	public void httpStartDetectionTest() throws IOException, InterruptedException {
-		String serviceDir = CommandTestUtils.getPath("/apps/USM/usm/tomcatHttpStartDetection");
+	public void httpStartDetectionTest()
+			throws IOException, InterruptedException {
+		final String serviceDir = CommandTestUtils.getPath("/apps/USM/usm/tomcatHttpStartDetection");
 		doTest(serviceDir);
 	}
-	
+
 	/**
-	 * deploy tomcat on port 8081(VIA Recipe) and try to assert
-	 * deployment of tomcat on port 8080 using HttpLivenessDetector Plugin.
-	 * Look at recipe tomcatHttpLivenessDetectorPlugin for more details.
+	 * deploy tomcat on port 8081(VIA Recipe) and try to assert deployment of tomcat on port 8080 using
+	 * HttpLivenessDetector Plugin. Look at recipe tomcatHttpLivenessDetectorPlugin for more details.
 	 * 
 	 * @throws IOException
 	 * @throws InterruptedException
 	 */
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, groups = "1")
-	public void urlLivenessDetectorTest() throws IOException, InterruptedException {
-		String serviceDir =CommandTestUtils.getPath("/apps/USM/usm/tomcatHttpLivenessDetectorPlugin");
+	public void urlLivenessDetectorTest()
+			throws IOException, InterruptedException {
+		final String serviceDir = CommandTestUtils.getPath("/apps/USM/usm/tomcatHttpLivenessDetectorPlugin");
 		doTest(serviceDir);
 	}
-	
-	private void doTest(String serviceDir) throws IOException, InterruptedException {
-        assertNotNull("Failed to find rest pu", admin.getProcessingUnits().waitFor("rest", 30 , TimeUnit.SECONDS));
+
+	private void doTest(final String serviceDir)
+			throws IOException, InterruptedException {
+		assertNotNull("Failed to find rest pu", admin.getProcessingUnits().waitFor("rest", 30, TimeUnit.SECONDS));
 		machineA = admin.getProcessingUnits().getProcessingUnit("rest").getInstances()[0].getMachine();
-		String command = "connect " + restUrl + ";install-service --verbose " + serviceDir + ";exit";
-			
-			LogUtils.log("Installing tomcat on port 8081");
-			CommandTestUtils.runCommandExpectedFail(command);
-			String tomcatAbsolutePuName = ServiceUtils.getAbsolutePUName(USM_APPLICATION_NAME, USM_SERVICE_NAME);
-			assertTrue("tomcat service processing unit does not exist", admin.getProcessingUnits().getProcessingUnit(tomcatAbsolutePuName) != null);
-			assertTrue("tomcat service should not be installed.", !admin.getProcessingUnits().getProcessingUnit(tomcatAbsolutePuName).getStatus().equals(DeploymentStatus.INTACT));
-			ProcessingUnit tomcatPu = admin.getProcessingUnits().waitFor(tomcatAbsolutePuName, Constants.PROCESSINGUNIT_TIMEOUT_SEC, TimeUnit.SECONDS);
-			assertTrue("Expected processing unit instances of tomcat service is not 0", !USMTestUtils.isUSMServiceRunning(tomcatAbsolutePuName, admin));
-			assertTrue("Service " + tomcatAbsolutePuName + " State is RUNNING.", 
-					!USMTestUtils.waitForPuRunningState(tomcatAbsolutePuName, 60, TimeUnit.SECONDS, admin));
-		
+		final String command = "connect " + restUrl + ";install-service --verbose " + serviceDir + ";exit";
+
+		LogUtils.log("Installing tomcat on port 8081");
+		CommandTestUtils.runCommandExpectedFail(command);
+		final String tomcatAbsolutePuName = ServiceUtils.getAbsolutePUName(USM_APPLICATION_NAME, USM_SERVICE_NAME);
+		final ProcessingUnit processingUnit =
+				admin.getProcessingUnits().waitFor(tomcatAbsolutePuName, Constants.PROCESSINGUNIT_TIMEOUT_SEC,
+						TimeUnit.SECONDS);
+		assertTrue("tomcat service processing unit does not exist", processingUnit != null);
+
+		// This is not a good test - there could be a temporary moment where the instance is considered 'OK'
+		// assertTrue("tomcat service should not be installed.",
+		// !admin.getProcessingUnits().getProcessingUnit(tomcatAbsolutePuName).getStatus().equals(DeploymentStatus.INTACT));
+
+		assertTrue("Expected USM processing unit instance to be not running",
+				!USMTestUtils.isUSMServiceRunning(tomcatAbsolutePuName, admin));
+
 	}
 }

030c72ca5ede410a63381725fee06e7d2e9b1c91
adaml <adaml@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-09-11 13:50:05 +0000

    Added override properties
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@128758 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/cloud/ec2/Ec2PropertiesFileTest.java
+++ src/test/cli/cloudify/cloud/ec2/Ec2PropertiesFileTest.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.io.FileUtils;
 import org.testng.ITestContext;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -12,10 +13,13 @@ import test.cli.cloudify.CloudTestUtils;
 import test.cli.cloudify.cloud.NewAbstractCloudTest;
 import test.cli.cloudify.cloud.services.ec2.Ec2CloudService;
 import framework.tools.SGTestHelper;
+import framework.utils.LogUtils;
 import framework.utils.ScriptUtils;
 
 public class Ec2PropertiesFileTest extends NewAbstractCloudTest{
-	final String tomcatServicePath = ScriptUtils.getBuildPath() + "/recipes/services/tomcat" ;
+	private static final String MANAGEMENT_MACHINE_TEMPLATE_MY_HARDWARE_ID = "managementMachineTemplate myHardwareId";
+	final String serviceName = "tomcat";
+	final String tomcatServicePath = ScriptUtils.getBuildPath() + "/recipes/services/" + serviceName;
 
 	@Override
 	protected String getCloudName() {
@@ -28,15 +32,23 @@ public class Ec2PropertiesFileTest extends NewAbstractCloudTest{
 	}
 	
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 4, enabled = true)
-	public void testInstallationWithPropsFile() throws Exception {		
-		installServiceAndWait(tomcatServicePath, "tomcat");
+	public void testInstallationWithPropsFile() throws Exception {
+		String pathToCloudFolder = ((Ec2CloudService)cloud).getPathToCloudFolder();
+		File cloudConfig = new File(pathToCloudFolder, "ec2-cloud.groovy");
+		String configFileAsString = FileUtils.readFileToString(cloudConfig);
+		LogUtils.log("asserting cloud was bootstrapped with the correct properties (properties were overridden)");
+		assertTrue("Management machine did not start using the properties defined in the properties file",
+				configFileAsString.contains(MANAGEMENT_MACHINE_TEMPLATE_MY_HARDWARE_ID));
+
+		LogUtils.log("Testing service installation");
+		installServiceAndWait(tomcatServicePath, serviceName);
+		uninstallServiceAndWait(serviceName);
 	}
 
 	@Override
 	protected void customizeCloud() throws Exception {
-		//create the properties file.
+		//Copy the props file into the cloud folder path
 		Map<File, File> filesToReplace = new HashMap<File, File>();
-		
 		String pathToCloudFolder = ((Ec2CloudService)cloud).getPathToCloudFolder();
 		String sgTestRootDir = SGTestHelper.getSGTestRootDir() ;
 		File propertiesFile = new File(sgTestRootDir + "/apps/cloudify/cloud/ec2/", "ec2-cloud.properties");
@@ -44,6 +56,12 @@ public class Ec2PropertiesFileTest extends NewAbstractCloudTest{
 		filesToReplace.put(mockFile, propertiesFile);
 		((Ec2CloudService)cloud).addFilesToReplace(filesToReplace);
 		
+		//Set the management machine template option to be taken from the cloud props file.
+		((Ec2CloudService)cloud).setAdditionalPropsToReplace(new HashMap<String, String>());
+		((Ec2CloudService)cloud).getAdditionalPropsToReplace().put("managementMachineTemplate \"SMALL_LINUX\"",
+				MANAGEMENT_MACHINE_TEMPLATE_MY_HARDWARE_ID);
+		
+		//set the machine name
 		cloud.setMachinePrefix(this.getClass().getName() + CloudTestUtils.SGTEST_MACHINE_PREFIX);
 		
 	}

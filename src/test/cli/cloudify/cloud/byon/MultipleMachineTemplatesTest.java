38acf2ce3ad92adf5f2bde2fd53a8065579a7732
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-09-15 10:46:34 +0000

    disable test.
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@129001 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/cloud/byon/MultipleMachineTemplatesTest.java
+++ src/test/cli/cloudify/cloud/byon/MultipleMachineTemplatesTest.java
@@ -61,7 +61,7 @@ public class MultipleMachineTemplatesTest extends AbstractByonCloudTest {
 	 * 
 	 * @throws Exception
 	 */
-	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, enabled = true, priority = 1)
+	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, enabled = false, priority = 1)
 	public void testPetclinic() throws Exception {
 
 		LogUtils.log("installing application petclinic on " + cloudName);

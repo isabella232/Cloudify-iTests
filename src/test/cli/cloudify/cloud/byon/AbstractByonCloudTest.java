f5b101a82ae87db502e0e1b930884c2ee676f568
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-09-23 16:18:28 +0000

    CLOUDIFY-1069 close admin before teardown
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@129489 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/cloud/byon/AbstractByonCloudTest.java
+++ src/test/cli/cloudify/cloud/byon/AbstractByonCloudTest.java
@@ -1,36 +1,30 @@
 package test.cli.cloudify.cloud.byon;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.commons.io.FileUtils;
 import org.cloudifysource.dsl.internal.CloudifyConstants;
 import org.openspaces.admin.Admin;
 import org.openspaces.admin.AdminFactory;
 import org.testng.ITestContext;
 
-import test.AbstractTest;
 import test.cli.cloudify.cloud.NewAbstractCloudTest;
 import test.cli.cloudify.cloud.services.byon.ByonCloudService;
 import framework.tools.SGTestHelper;
 import framework.utils.LogUtils;
-import framework.utils.SSHUtils;
 
 public class AbstractByonCloudTest extends NewAbstractCloudTest {
 	
-	protected Admin admin;
+	@Override
+	protected void beforeTeardown() throws Exception {
+		closeAdmin();
+	}
 
+	protected Admin admin;
+	
 	public ByonCloudService getService() {
 		return (ByonCloudService) super.getService();
-	}
-	
-	@Override
-	protected void beforeBootstrap() throws Exception {
-		super.beforeBootstrap();
-		cleanMachines();
-		printBootstrapManagementFile();
 	}
 
 
@@ -45,12 +39,6 @@ public class AbstractByonCloudTest extends NewAbstractCloudTest {
 		super.afterBootstrap();
 		createAdmin();
 	}
-	
-	@Override
-	protected void afterTeardown() throws Exception {
-		super.afterTeardown();
-		cleanMachines();
-	}
 
 
 	private void createAdmin() {
@@ -58,11 +46,18 @@ public class AbstractByonCloudTest extends NewAbstractCloudTest {
 		AdminFactory factory = new AdminFactory();
 		for (String host : managementHosts) {
 			LogUtils.log("creating admin");
-			String utlNoHttp = host.substring(7); // remove "http://"
+			String utlNoHttp = host.substring(7); /* remove "http://" */
 			String ip = utlNoHttp.split(":")[0];
 			factory.addLocators(ip + ":" + CloudifyConstants.DEFAULT_LUS_PORT);
 		}
 		admin = factory.createAdmin();
+	}
+	
+	private void closeAdmin() {
+		if (admin != null) {
+			admin.close();
+			admin = null;
+		}
 	}
 	
 	@Override
@@ -73,7 +68,6 @@ public class AbstractByonCloudTest extends NewAbstractCloudTest {
 		Map<File, File> filesToReplace = new HashMap<File, File>();
 		filesToReplace.put(standardBootstrapManagement, customBootstrapManagement);
 		getService().addFilesToReplace(filesToReplace);
-
 	}
 
 	@Override
@@ -84,47 +78,5 @@ public class AbstractByonCloudTest extends NewAbstractCloudTest {
 	@Override
 	protected boolean isReusableCloud() {
 		return false;
-	}
-	
-	private void cleanMachines() {
-		killAllJavaOnAllHosts();
-		cleanGSFilesOnAllHosts();
-	}
-	
-	private void printBootstrapManagementFile() throws IOException {
-		String pathToBootstrap = getService().getPathToCloudFolder() + "/upload/bootstrap-management.sh";
-		File bootstrapFile = new File(pathToBootstrap);
-		if (!bootstrapFile.exists()) {
-			LogUtils.log("Failed to print the cloud configuration file content");
-			return;
-		}
-		String cloudConfigFileAsString = FileUtils.readFileToString(bootstrapFile);
-		LogUtils.log("Bootstrap-management file: " + bootstrapFile.getAbsolutePath());
-		LogUtils.log(cloudConfigFileAsString);
-		
-	}
-
-	private void cleanGSFilesOnAllHosts() {
-		String command = "rm -rf /tmp/gs-files";
-		String[] hosts = getService().getMachines();
-		for (String host : hosts) {
-			try {
-				LogUtils.log(SSHUtils.runCommand(host, AbstractTest.OPERATION_TIMEOUT, command, "tgrid", "tgrid"));
-			} catch (AssertionError e) {
-				LogUtils.log("Failed to clean gs-files on host " + host + " .Reason --> " + e.getMessage());
-			}
-		}	
-	}
-	
-	private void killAllJavaOnAllHosts() {
-		String command = "killall -9 java";
-		String[] hosts = getService().getMachines();
-		for (String host : hosts) {
-			try {
-				LogUtils.log(SSHUtils.runCommand(host, AbstractTest.OPERATION_TIMEOUT, command, "tgrid", "tgrid"));
-			} catch (AssertionError e) {
-				LogUtils.log("Failed to clean gs-files on host " + host + " .Reason --> " + e.getMessage());
-			}
-		}
 	}
 }

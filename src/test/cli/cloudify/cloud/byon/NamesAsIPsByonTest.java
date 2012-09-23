572bf20f46067e73194a30220ade390265428f8d
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-09-23 16:20:36 +0000

    CLOUDIFY-1069 improve assertions. avoid NPE
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@129493 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/cloud/byon/NamesAsIPsByonTest.java
+++ src/test/cli/cloudify/cloud/byon/NamesAsIPsByonTest.java
@@ -1,20 +1,19 @@
 package test.cli.cloudify.cloud.byon;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import org.testng.ITestContext;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import test.cli.cloudify.cloud.services.byon.ByonCloudService;
-import framework.utils.AssertUtils;
 import framework.utils.ScriptUtils;
 
 
 /**
- * This test installs petclinic-simple on byon after changing the byon-cloud.groovy file to contain machine names instead of IPs.
- * <p>It checks whether the bootstrap and instal have succeeded.
+ * This test installs petclinic-simple on BYON after changing the byon-cloud.groovy file to contain machine names instead of IPs.
+ * <p>It checks whether the bootstrap and install have succeeded.
  * <p>Note: this test uses 3 fixed machines - 192.168.9.115, 192.168.9.116, 192.168.9.120.
  */
 public class NamesAsIPsByonTest extends AbstractByonCloudTest {
@@ -30,10 +29,9 @@ public class NamesAsIPsByonTest extends AbstractByonCloudTest {
 	public void testPetclinic() throws IOException, InterruptedException{
 
 		installApplicationAndWait(ScriptUtils.getBuildPath() + "/recipes/apps/petclinic-simple", "petclinic");
-
-		//TODO : edit this, so if it fails it won't be on NPE!
-		AssertUtils.assertTrue("petclinic.mongod is not up - install failed", admin.getProcessingUnits().getProcessingUnit("petclinic.mongod") != null);		
-		AssertUtils.assertTrue("petclinic.tomcat is not up - install failed", admin.getProcessingUnits().getProcessingUnit("petclinic.tomcat") != null);
+		assertTrue("petclinic.mongod is not up - install failed", admin.getProcessingUnits().waitFor("petclinic.mongod", OPERATION_TIMEOUT, TimeUnit.MILLISECONDS) != null);
+		assertTrue("petclinic.tomcat is not up - install failed", admin.getProcessingUnits().waitFor("petclinic.tomcat", OPERATION_TIMEOUT, TimeUnit.MILLISECONDS) != null);
+		uninstallApplicationAndWait("petclinic");
 
 	}
 
@@ -45,21 +43,6 @@ public class NamesAsIPsByonTest extends AbstractByonCloudTest {
 	@Override
 	protected void customizeCloud() throws Exception {
 		super.customizeCloud();
-		getService().setIpList(System.getProperty(ByonCloudService.IP_LIST_PROPERTY, namesList));
-	}
-
-	@Override
-	protected void beforeTeardown() throws Exception {
-		// clean
-		if (admin != null) {
-			admin.close();
-			admin = null;
-		}
-
-		try{
-			uninstallApplicationAndWait("petclinic");
-		} finally {
-			System.clearProperty(ByonCloudService.IP_LIST_PROPERTY);
-		}
+		getService().setIpList(namesList);
 	}
 }

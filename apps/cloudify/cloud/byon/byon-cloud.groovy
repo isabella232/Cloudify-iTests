8199629a30938f962ae91315cd003b3e7fa749e7
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-09-27 15:43:43 +0000

    CLOUDIFY-1069 fixed byon-cloud.groovy used by multiple machine templates test
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@129782 eb64e737-3616-4df0-8941-5ee2ae88103d

--- apps/cloudify/cloud/byon/byon-cloud.groovy
+++ apps/cloudify/cloud/byon/byon-cloud.groovy
@@ -5,11 +5,11 @@
  * @author noak
  *
  */
- 
+
 cloud {
 	// Mandatory. The name of the cloud, as it will appear in the Cloudify UI.
 	name = "byon"
-	
+
 	/********
 	 * General configuration information about the cloud driver implementation.
 	 */
@@ -17,7 +17,7 @@ cloud {
 		// The cloud-driver implementation class.
 		className "org.cloudifysource.esc.driver.provisioning.byon.ByonProvisioningDriver"
 		// Optional. The template name for the management machines. Defaults to the first template in the templates section below.
-		managementMachineTemplate "TEMPLATE_3"
+		managementMachineTemplate "SMALL_LINUX"
 		// Optional. Indicates whether internal cluster communications should use the machine private IP. Defaults to true.
 		connectToPrivateIp true
 		//Indicates whether communications with the management servers should use the machine private IP.
@@ -30,113 +30,181 @@ cloud {
 	provider {
 		// Mandatory. The name of the provider.
 		provider "byon"
-		
-		// Mandatory. The HTTP/S URL where cloudify can be downloaded from by newly started machines.
-		cloudifyUrl "cloudify.zip"
-		// Mandatory. The prefix for new machines started for services.
+
+
+		// Optional. The HTTP/S URL where cloudify can be downloaded from by newly started machines. Defaults to downloading the
+		// cloudify version matching that of the client from the cloudify CDN.
+		// Change this if your compute nodes do not have access to an internet connection, or if you prefer to use a
+		// different HTTP server instead.
+		// cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.2.0/gigaspaces-cloudify-2.2.0-m4-b2493-70.zip"
+
+		// Mandatory. The prefix for new machines started for servies.
 		machineNamePrefix "cloudify_agent_"
 		// Optional. Defaults to true. Specifies whether cloudify should try to deploy services on the management machine.
 		// Do not change this unless you know EXACTLY what you are doing.
-		
+
 		managementOnlyFiles ([])
-		
-		// Optional. Logging level for the internal cloud provider logger. Defaults to INFO.
+
+		// Optional. Logging level for the intenal cloud provider logger. Defaults to INFO.
 		sshLoggingLevel "INFO"
-		// Mandatory. Name of the new machine/s started as cloudify management machines. 
+		// Mandatory. Name of the new machine/s started as cloudify management machines.
 		managementGroup "cloudify_manager"
 		// Mandatory. Number of management machines to start on bootstrap-cloud. In production, should be 2. Can be 1 for dev.
 		numberOfManagementMachines 1
-		
+
 		reservedMemoryCapacityPerMachineInMB 1024
 	}
-	
+
 	/*************
 	 * Cloud authentication information
 	 */
 	user {
 
 	}
-	
+
 	/***********
 	 * Cloud machine templates available with this cloud. 
 	 */
 	templates ([
 				// Mandatory. Template Name.
+				SMALL_LINUX : template{
+					// Mandatory. Amount of RAM available to machine.
+					machineMemoryMB 1600
+					// Mandatory. Files from the local directory will be copied to this directory on the remote machine.
+					remoteDirectory "/tmp/gs-files"
+					// Optional. template-generic credentials. Can be overridden by specific credentials on each node, in the nodesList section.
+					username "ENTER_USER"
+					password "ENTER_PASSWORD"
+
+					// Mandatory. All files from this LOCAL directory will be copied to the remote machine directory.
+					localDirectory "upload"
+
+					// Mandatory for BYON.
+					custom ([
+								// Mandatory for BYON. The nodesList custom property lists the nodes that compose this cloud-like environment.
+								// For each node required:
+								// An alias for this node. can be static or use a template with an dynamic-index.
+								// The server's private (local) IP. can be a single IP, a list, a range or CIDR.
+								//Optional - user and password for the node. can be taken from general cloud configuration.
+								"nodesList" : ([
+									([
+										"id" : "byon-pc-lab{0}",
+										"host-list" : "SMALL_LINUX_HOSTS"
+									])
+								])
+							])
+
+					// enable sudo.
+					privileged true
+
+				},
+				// Mandatory. Template Name.
 				TEMPLATE_1 : template{
-				// Mandatory. Amount of RAM available to machine.
-				machineMemoryMB 1600
-				// Mandatory. Files from the local directory will be copied to this directory on the remote machine.
-				remoteDirectory "/tmp/gs-files"
-				// Optional. template-generic credentials. Can be overridden by specific credentials on each node, in the nodesList section.
-				username "tgrid"
-				password "tgrid"
-				
-				// Mandatory. All files from this LOCAL directory will be copied to the remote machine directory.
-				localDirectory "upload"
-				
-				// Mandatory for BYON.
+					// Mandatory. Amount of RAM available to machine.
+					machineMemoryMB 1600
+					// Mandatory. Files from the local directory will be copied to this directory on the remote machine.
+					remoteDirectory "/tmp/gs-files"
+					// Optional. template-generic credentials. Can be overridden by specific credentials on each node, in the nodesList section.
+					username "ENTER_USER"
+					password "ENTER_PASSWORD"
+
+					// Mandatory. All files from this LOCAL directory will be copied to the remote machine directory.
+					localDirectory "upload"
+
+					// Mandatory for BYON.
 					custom ([
-						// Mandatory for BYON. The nodesList custom property lists the nodes that compose this cloud-like environment.
-						// For each node required:
-						// An alias for this node. can be static or use a template with an dynamic-index.
-						// The server's private (local) IP. can be a single IP, a list, a range or CIDR.
-						//Optional - user and password for the node. can be taken from general cloud configuration.
-						"nodesList" : ([
-										([
-											"id" : "byon-mgmt{0}",
-											"host-list" : "1.1.1.1"
-										])
-						])
-					])
+								// Mandatory for BYON. The nodesList custom property lists the nodes that compose this cloud-like environment.
+								// For each node required:
+								// An alias for this node. can be static or use a template with an dynamic-index.
+								// The server's private (local) IP. can be a single IP, a list, a range or CIDR.
+								//Optional - user and password for the node. can be taken from general cloud configuration.
+								"nodesList" : ([
+									([
+										"id" : "byon-pc-lab{0}",
+										"host-list" : "TEMPLATE_1_HOSTS"
+									])
+								])
+							])
+
+					// enable sudo.
+					privileged true
+
 				},
+				// Mandatory. Template Name.
 				TEMPLATE_2 : template{
-				machineMemoryMB 1600
-				remoteDirectory "/tmp/gs-files"
-				username "tgrid"
-				password "tgrid"
-				// Mandatory. All files from this LOCAL directory will be copied to the remote machine directory.
-				localDirectory "upload"
-			
-				// Mandatory for BYON.
+					// Mandatory. Amount of RAM available to machine.
+					machineMemoryMB 1600
+					// Mandatory. Files from the local directory will be copied to this directory on the remote machine.
+					remoteDirectory "/tmp/gs-files"
+					// Optional. template-generic credentials. Can be overridden by specific credentials on each node, in the nodesList section.
+					username "ENTER_USER"
+					password "ENTER_PASSWORD"
+
+					// Mandatory. All files from this LOCAL directory will be copied to the remote machine directory.
+					localDirectory "upload"
+
+					// Mandatory for BYON.
 					custom ([
-						"nodesList" : ([
-										([
-											"id" : "byon-small{0}",
-											"host-list" : "2.2.2.2"
-										])
-						])
-					])
+								// Mandatory for BYON. The nodesList custom property lists the nodes that compose this cloud-like environment.
+								// For each node required:
+								// An alias for this node. can be static or use a template with an dynamic-index.
+								// The server's private (local) IP. can be a single IP, a list, a range or CIDR.
+								//Optional - user and password for the node. can be taken from general cloud configuration.
+								"nodesList" : ([
+									([
+										"id" : "byon-pc-lab{0}",
+										"host-list" : "TEMPLATE_2_HOSTS"
+									])
+								])
+							])
+
+					// enable sudo.
+					privileged true
+
 				},
+				// Mandatory. Template Name.
 				TEMPLATE_3 : template{
-				machineMemoryMB 1600
-				remoteDirectory "/tmp/gs-files"
-				username "tgrid"
-				password "tgrid"
-				// Mandatory. All files from this LOCAL directory will be copied to the remote machine directory.
-				localDirectory "upload"
-			
-				// Mandatory for BYON.
+					// Mandatory. Amount of RAM available to machine.
+					machineMemoryMB 1600
+					// Mandatory. Files from the local directory will be copied to this directory on the remote machine.
+					remoteDirectory "/tmp/gs-files"
+					// Optional. template-generic credentials. Can be overridden by specific credentials on each node, in the nodesList section.
+					username "ENTER_USER"
+					password "ENTER_PASSWORD"
+
+					// Mandatory. All files from this LOCAL directory will be copied to the remote machine directory.
+					localDirectory "upload"
+
+					// Mandatory for BYON.
 					custom ([
-						"nodesList" : ([
-										([
-											"id" : "byon-big{0}",
-											"host-list" : "3.3.3.3"
-										])
-						])
-					])
+								// Mandatory for BYON. The nodesList custom property lists the nodes that compose this cloud-like environment.
+								// For each node required:
+								// An alias for this node. can be static or use a template with an dynamic-index.
+								// The server's private (local) IP. can be a single IP, a list, a range or CIDR.
+								//Optional - user and password for the node. can be taken from general cloud configuration.
+								"nodesList" : ([
+									([
+										"id" : "byon-pc-lab{0}",
+										"host-list" : "TEMPLATE_3_HOSTS"
+									])
+								])
+							])
+
+					// enable sudo.
+					privileged true
+
 				}
-	])
-	
+			])
+
 	/*****************
 	 * Optional. Custom properties used to extend existing drivers or create new ones. 
 	 */
-	 // Optional. Sets whether to delete the remoteDirectory created by the cloud driver, when shutting down.
+	// Optional. Sets whether to delete the remoteDirectory created by the cloud driver, when shutting down.
 	custom ([
-		"cleanGsFilesOnShutdown": "true",
-		"itemsToClean": ([
-			"/tmp/gs-files/upload/gigaspaces/work",
-			"/tmp/gs-files/upload/gigaspaces.zip"
-		])
-	])
+				"cleanGsFilesOnShutdown": "true",
+				"itemsToClean": ([
+					"/tmp/gs-files/gigaspaces.tar.gz"
+				])
+			])
 
 }

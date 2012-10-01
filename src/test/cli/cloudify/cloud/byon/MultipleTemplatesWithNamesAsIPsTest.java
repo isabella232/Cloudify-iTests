d77604d1a714853df4951ccb38176965f993ba94
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-10-01 11:12:56 +0000

    fix test
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@129901 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/cloud/byon/MultipleTemplatesWithNamesAsIPsTest.java
+++ src/test/cli/cloudify/cloud/byon/MultipleTemplatesWithNamesAsIPsTest.java
@@ -13,22 +13,17 @@ import com.gigaspaces.webuitf.util.LogUtils;
 
 
 /**
- * This test installs petclinic with 3 different templates on a byon cloud. 
- * <p>for two of the three templates the test injects to byon-cloud.groovy names of machines instead of IPs.
- * <p>It checks that each service was assigned to the correct template, according to byon-cloud.groovy.
- * <p>After the installation completes, the test checks the uninstall and teardown operations.
- * 
- * <p>Note: for two of the three templates the test injects to byon-cloud.groovy names of machines instead of IPs.
- * 
- * <p>Note: this test uses 5 fixed machines - 192.168.9.115,192.168.9.116,192.168.9.120,192.168.9.125,192.168.9.126.
+ * this test is exactly like the super test, except that the hosts are specified as host names instead of IP's.
+ * @author elip
+ *
  */
 public class MultipleTemplatesWithNamesAsIPsTest extends MultipleMachineTemplatesTest {
-	
+
 	@BeforeClass(alwaysRun = true)
 	protected void bootstrap(final ITestContext testContext) {
 		super.bootstrap(testContext);
 	}
-	
+
 	@Override
 	public void customizeCloud() throws Exception {	
 		String[] machines = getService().getMachines();
@@ -36,39 +31,36 @@ public class MultipleTemplatesWithNamesAsIPsTest extends MultipleMachineTemplate
 		LogUtils.log("converting every other address to host name");
 		for (int i = 0 ; i < machines.length ; i++) {
 			String host = machines[i];
-			if ((i % 2) == 0) {
-				
-				String[] chars = host.split("\\.");
-				
-				byte[] add = new byte[chars.length];
-				for (int j = 0 ; j < chars.length ; j++) {
-					add[j] =(byte) ((int) Integer.valueOf(chars[j]));
-				}
-				
-				try {
-					InetAddress byAddress = InetAddress.getByAddress(add);
-					String hostName = byAddress.getHostName();
-					LogUtils.log("IP address " + host + " was resolved to " + hostName);
-					machinesHostNames[i] = hostName;
-				} catch (UnknownHostException e) {
-					throw new IllegalStateException("could not resolve host name of ip " + host);
-				}
-			} else {
-				machinesHostNames[i] = host;
+
+			String[] chars = host.split("\\.");
+
+			byte[] add = new byte[chars.length];
+			for (int j = 0 ; j < chars.length ; j++) {
+				add[j] =(byte) ((int) Integer.valueOf(chars[j]));
 			}
+
+			try {
+				InetAddress byAddress = InetAddress.getByAddress(add);
+				String hostName = byAddress.getHostName();
+				LogUtils.log("IP address " + host + " was resolved to " + hostName);
+				machinesHostNames[i] = hostName;
+			} catch (UnknownHostException e) {
+				throw new IllegalStateException("could not resolve host name of ip " + host);
+			}
+
 		}
 		getService().setMachines(machinesHostNames);
 		super.customizeCloud();
 	}
 
-	
+
 	@Override
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, enabled = true, priority = 1)
 	public void testPetclinic() throws Exception {
 		super.testPetclinic();
 	}
-	
-	
+
+
 	/**
 	 * Gets the address of the machine on which the given processing unit is deployed. 
 	 * @param puName The name of the processing unit to look for
@@ -80,6 +72,5 @@ public class MultipleTemplatesWithNamesAsIPsTest extends MultipleMachineTemplate
 		Assert.assertNotNull(pu.getInstances()[0], puName + " processing unit is not found");
 		return pu.getInstances()[0].getMachine().getHostName();	
 	}
-	
+
 }
-	

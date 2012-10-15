206ce293a3c75041112cc521947bbc11be7716c7
nirb <nirb@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-10-15 13:40:37 +0000

    CLOUDIFY-1140 improved the test
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@130717 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/cloud/ec2/TravelChefTest.java
+++ src/test/cli/cloudify/cloud/ec2/TravelChefTest.java
@@ -1,18 +1,33 @@
 package test.cli.cloudify.cloud.ec2;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Map;
 
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.type.TypeFactory;
+import org.codehaus.jackson.type.JavaType;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+
+import test.cli.cloudify.CommandTestUtils;
 import test.cli.cloudify.cloud.NewAbstractCloudTest;
+import framework.utils.AssertUtils;
+import framework.utils.LogUtils;
+import framework.utils.ScriptUtils;
+import framework.utils.WebUtils;
 
 public class TravelChefTest extends NewAbstractCloudTest{
 
-	private String appName = "travel-chef";
+	private static final String apacheUrlRestPath = "/admin/ProcessingUnits/Names/travel.apacheLB/ProcessingUnitInstances/0/ServiceDetailsByServiceId/USM/Attributes/Cloud%20Public%20IP";
+	private String appName = "travel";
+	private String appFolderName = "travel-chef";
 	
 	@BeforeClass(alwaysRun = true)
 	protected void bootstrap(final ITestContext testContext) {
@@ -21,17 +36,41 @@ public class TravelChefTest extends NewAbstractCloudTest{
 	
 	@AfterClass(alwaysRun = true)
 	protected void teardown() {
-		super.teardown();
-	}
-	
-	@AfterMethod(alwaysRun = true)
-	public void cleanUp() throws IOException, InterruptedException {
-		super.uninstallApplicationAndWait(appName);
+//		super.teardown();
 	}
 	
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 4, enabled = true)
-	public void testStockDemo() throws IOException, InterruptedException {
-		doSanityTest(appName, appName);
+	public void testTravelChef() throws Exception {
+
+		String[] services = {"chef-server", "apacheLB", "mysql"};
+		String restUrl = getRestUrl();
+		String apachePort = "8090";
+		
+		installApplicationAndWait(ScriptUtils.getBuildPath() + "/recipes/apps/" + appFolderName, appName, 0);
+		
+		LogUtils.log("verifing successful installation");
+
+		Client client = Client.create(new DefaultClientConfig());
+		final WebResource service = client.resource(this.getRestUrl());
+		
+		String command = "connect " + restUrl + ";use-application " + appName + ";list-services";
+		String output = CommandTestUtils.runCommandAndWait(command);
+		
+		for(String singleService : services){
+			AssertUtils.assertTrue("the service " + singleService + " is not running", output.contains(singleService));					
+		}
+		
+		String restApacheService = service.path(apacheUrlRestPath).get(String.class);
+		Map<String, Object> apacheUrlMap = jsonToMap(restApacheService);
+		String apacheServiceHostURL = (String) apacheUrlMap.get(restApacheService);
+		
+		assertPageExists("http://" + apacheServiceHostURL + ":" + apachePort + "/");
+		
+		super.uninstallApplicationAndWait(appName);
+
+		LogUtils.log("verifing successful uninstallation");
+		
+		AssertUtils.assertTrue("the application is running",!WebUtils.isURLAvailable(new URL(getWebuiUrl() + "/travel")));
 	}
 	
 	@Override
@@ -47,6 +86,21 @@ public class TravelChefTest extends NewAbstractCloudTest{
 	@Override
 	protected boolean isReusableCloud() {
 		return false;
+	}
+	
+	private void assertPageExists(String url) {
+
+		try {
+			WebUtils.isURLAvailable(new URL(url));
+		} catch (Exception e) {
+			AssertUtils.AssertFail(e.getMessage());
+		}
+	}
+	
+	private static Map<String, Object> jsonToMap(final String response) throws IOException {
+		final JavaType javaType = TypeFactory.type(Map.class);
+		ObjectMapper om = new ObjectMapper();
+		return om.readValue(response, javaType);
 	}
 
 }

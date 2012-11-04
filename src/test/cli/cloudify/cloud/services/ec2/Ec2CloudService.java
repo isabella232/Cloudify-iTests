441c6c24baab71d33d7125f0bb6e0be31f336a9e
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-11-04 12:29:36 +0000

    use us east region as default
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@132102 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/test/cli/cloudify/cloud/services/ec2/Ec2CloudService.java
+++ src/test/cli/cloudify/cloud/services/ec2/Ec2CloudService.java
@@ -279,7 +279,7 @@ public class Ec2CloudService extends AbstractCloudService {
 	}
 	
 	public String getRegion() {
-		return System.getProperty("ec2.region" , "eu");
+		return System.getProperty("ec2.region" , "us-east-1");
 	}
 
 	public void setPemFileName(final String pemFileName) {

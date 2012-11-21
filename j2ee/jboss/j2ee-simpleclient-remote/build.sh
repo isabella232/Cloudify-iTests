8b0b47fdcf1f703c3720caec0a80437b7a524982
sagib <sagib@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-11-21 11:02:42 +0000

    CLOUDIFY-1216 modified project layout
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@133371 eb64e737-3616-4df0-8941-5ee2ae88103d

--- j2ee/jboss/j2ee-simpleclient-remote/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-LOOKUPGROUPS=sgtest
-.  ${BUILD_DIR}/bin/setenv.sh
-
-echo "${ANT_JARS}":"${JAVA_HOME}"/lib/tools.jar
-
-java "${LOOKUP_GROUPS_PROP}" -classpath "${ANT_JARS}":"${JAVA_HOME}"/lib/tools.jar org.apache.tools.ant.Main $1
--- j2ee/jboss/j2ee-simpleclient/ejb/src/org/openspaces/example/data/ejb/SimpleSession.java
+++
@@ -1,9 +0,0 @@
-package org.openspaces.example.data.ejb;
-
-import test.data.Data;
-
-public interface SimpleSession {
-	public Data read(String id);
-	public void write(String id);
-	public int count();
-}

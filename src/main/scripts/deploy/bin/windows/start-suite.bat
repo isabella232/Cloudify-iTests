f90468aae5b6d733ea2ebf3e9402faa1d1a88f4e
sagib <sagib@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-11-22 16:15:22 +0000

    CLOUDIFY-1216 updated script
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@133506 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/main/scripts/deploy/bin/windows/start-suite.bat
+++ src/main/scripts/deploy/bin/windows/start-suite.bat
@@ -42,5 +42,4 @@ call mvn test -e -X -U ^
 
 popd
 
-REM -DBUILD_NUMBER=%BUILD_NUMBER%  -DMAJOR_VERSION=%VERSION% -DMINOR_VERSION=%MILESTONE%
-call %LOCAL_SGPATH%\deploy\bin\windows\generate-report.bat %BUILD_NUMBER% %SUITE_NAME% %VERSION% %MILESTONE% %BUILD_LOG_URL%
+call %SGTEST_HOME%\src\main\scripts\deploy\bin\windows\generate-report.bat %BUILD_NUMBER% %SUITE_NAME% %VERSION% %MILESTONE% %BUILD_LOG_URL%

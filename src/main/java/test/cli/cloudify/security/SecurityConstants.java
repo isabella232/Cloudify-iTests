cedf82bddcbcabfe546aad66f73b1279a4d11b1f
nirb <nirb@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-11-25 08:33:37 +0000

    Added and changed Users
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@133584 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/main/java/test/cli/cloudify/security/SecurityConstants.java
+++ src/main/java/test/cli/cloudify/security/SecurityConstants.java
@@ -3,7 +3,10 @@ package test.cli.cloudify.security;
 public class SecurityConstants {
 	
 	public static final String CLOUD_ADMIN_USER_PWD = "Amanda";
-	public static final String APP_MANAGER_USER_PWD = "Dana";
+	public static final String CLOUD_ADMIN_AND_APP_MANAGER_USER_PWD = "Dana";
+	public static final String APP_MANAGER_USER_PWD = "Dan";
+	public static final String APP_MANAGER_AND_VIEWER_USER_PWD = "Don";
 	public static final String VIEWER_USER_PWD = "John";
+	public static final String NO_ROLE_USER_PWD = "Jane";
 
 }

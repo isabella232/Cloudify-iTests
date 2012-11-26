f093702a0ca1c614b7d8fedb3e2fba63f039f988
kobi <kobi@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-11-26 10:22:26 +0000

    fixed script
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@133922 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/main/scripts/deploy/bin/windows/set-build-env.bat
+++ src/main/scripts/deploy/bin/windows/set-build-env.bat
@@ -3,4 +3,4 @@ set BUILD_FOLDER=gigaspaces-cloudify-%VERSION%-%MILESTONE%
 set LOCAL_SGPATH=%USER_HOME%\sgtest-cloudify3.0
 set BUILD_LOCATION=%LOCAL_SGPATH%\deploy\local-builds\%BUILD_NUMBER%\%BUILD_FOLDER%
 set REMOTE_BUILD_DIR=\\tarzan\builds\cloudify\%VERSION%\%BUILD_NUMBER%
-set BUILD_FOLDER=gigaspaces-cloudify-%VERSION%-%MILESTONE%
+set BUILD_TEST_DIR=%LOCAL_SGPATH%\deploy\local-builds\%BUILD_NUMBER%

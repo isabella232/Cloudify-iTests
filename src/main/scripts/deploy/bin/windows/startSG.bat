9f1f5293c2f58e37fa6a977c161a44f8e8e7a68c
kobi <kobi@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-11-26 14:55:12 +0000

    fixed script
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@133974 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/main/scripts/deploy/bin/windows/startSG.bat
+++ src/main/scripts/deploy/bin/windows/startSG.bat
@@ -22,6 +22,9 @@ set BRANCH_NAME=%9
 shift
 set SVN_BRANCH_DIRECTORY=%9
 
+shift
+set EC2_REGION=%9
+
 @echo setting up enviroment variables
 call set-build-env.bat
 
@@ -56,7 +59,7 @@ svn export --force %SVN_SGTEST_REPOSITORY% %SGTEST_HOME%
 call %SGTEST_HOME%\src\main\scripts\deploy\bin\windows\deploy_webuitf.bat
 
 @echo Running Suite %SUITE_NAME%: 
-call %SGTEST_HOME%\src\main\scripts\deploy\bin\windows\start-suite.bat %SUITE_NAME% %INCLUDE% %EXCLUDE% %BUILD_TEST_DIR%
+call %SGTEST_HOME%\src\main\scripts\deploy\bin\windows\start-suite.bat %SUITE_NAME% %INCLUDE% %EXCLUDE% %EC2_REGION% %BUILD_TEST_DIR%
 
 @echo generating report... 
 call %SGTEST_HOME%\src\main\scripts\deploy\bin\windows\generate-report.bat %BUILD_NUMBER% %SUITE_NAME% %VERSION% %MILESTONE% %BUILD_LOG_URL%

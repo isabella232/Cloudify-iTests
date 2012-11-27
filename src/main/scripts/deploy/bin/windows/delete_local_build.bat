ff31c69ee434197952c3fa9c0b962d266342150f
kobi <kobi@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-11-27 12:46:11 +0000

    added debugs
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@134140 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/main/scripts/deploy/bin/windows/delete_local_build.bat
+++ src/main/scripts/deploy/bin/windows/delete_local_build.bat
@@ -2,4 +2,6 @@
 @set MILESTONE=%2
 @set USER_HOME=%3
 
+echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% %USER_HOME%
+
 @if exist %USER_HOME%\gigaspaces-cloudify-%VERSION%-%MILESTONE% rmdir %USER_HOME%\gigaspaces-cloudify-%VERSION%-%MILESTONE% /s /q

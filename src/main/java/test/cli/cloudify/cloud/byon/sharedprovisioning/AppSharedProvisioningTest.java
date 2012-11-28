9c8a5484e8aa7a87eb04e18c1f77ad1aba0e19d3
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-11-28 12:56:51 +0000

    CLOUDIFY-1206 added more tests
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@134313 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/main/java/test/cli/cloudify/cloud/byon/sharedprovisioning/AppSharedProvisioningTest.java
+++ src/main/java/test/cli/cloudify/cloud/byon/sharedprovisioning/AppSharedProvisioningTest.java
@@ -3,8 +3,10 @@ package test.cli.cloudify.cloud.byon.sharedprovisioning;
 import java.io.IOException;
 import java.util.Set;
 
+import org.cloudifysource.dsl.utils.ServiceUtils;
 import org.openspaces.admin.machine.Machine;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -16,13 +18,16 @@ public class AppSharedProvisioningTest extends AbstractSharedProvisioningByonClo
 	private static final String APPLICATION_ONE = "application1";
 	private static final String APPLICATION_TWO = "application2";
 	
+	private static final String SERVICE_ONE = "service1";
+	private static final String SERVICE_TWO = "service2";
+	
 	@BeforeClass(alwaysRun = true)
 	protected void bootstrap() throws Exception {
-		//super.bootstrap();
+		super.bootstrap();
 	}
 	
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, enabled = true)
-	public void test() throws IOException, InterruptedException {
+	public void testTwoApplications() throws IOException, InterruptedException {
 		super.installManualAppSharedProvisioningApplicationAndWait(APPLICATION_ONE);
 		super.installManualAppSharedProvisioningApplicationAndWait(APPLICATION_TWO);
 		
@@ -33,7 +38,30 @@ public class AppSharedProvisioningTest extends AbstractSharedProvisioningByonClo
 		AssertUtils.assertTrue("applications have ovelapping machines even though the isolation is app based", 
 				!applicationOneMachines.removeAll(applicationTwoMachines));
 		
+		super.uninstallApplicationAndWait(APPLICATION_ONE);
+		super.uninstallApplicationAndWait(APPLICATION_TWO);
 		
+		
+	}
+	
+	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, enabled = true)
+	public void testTwoServicesOnOneApplication() throws IOException, InterruptedException {
+		super.installManualAppSharedProvisioningServiceAndWait(SERVICE_ONE, APPLICATION_ONE);
+		super.installManualAppSharedProvisioningServiceAndWait(SERVICE_TWO, APPLICATION_ONE);
+		
+		Set<Machine> serviceOneMachines = ProcessingUnitUtils.getMachinesOfService(admin, ServiceUtils.getAbsolutePUName(APPLICATION_ONE, SERVICE_ONE));
+		Set<Machine> serviceTwoMachines = ProcessingUnitUtils.getMachinesOfService(admin, ServiceUtils.getAbsolutePUName(APPLICATION_ONE, SERVICE_TWO));
+		
+		AssertUtils.assertTrue("services should be deployed on the same machine since they belong to the same application", 
+				serviceOneMachines.equals(serviceTwoMachines));
+		
+		super.uninstallApplicationAndWait(APPLICATION_ONE);	
+	}
+	
+	@AfterMethod
+	public void cleanup() throws IOException, InterruptedException {
+		super.uninstallApplicationIfFound(APPLICATION_ONE);
+		super.uninstallApplicationIfFound(APPLICATION_TWO);
 	}
 	
 	@AfterClass(alwaysRun = true)
--- src/main/java/test/cli/cloudify/cloud/byon/sharedprovisioning/TenantSharedProvisioningTest.java
+++ src/main/java/test/cli/cloudify/cloud/byon/sharedprovisioning/TenantSharedProvisioningTest.java
@@ -3,8 +3,10 @@ package test.cli.cloudify.cloud.byon.sharedprovisioning;
 import java.io.IOException;
 import java.util.Set;
 
+import org.cloudifysource.dsl.utils.ServiceUtils;
 import org.openspaces.admin.machine.Machine;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -16,13 +18,17 @@ public class TenantSharedProvisioningTest extends AbstractSharedProvisioningByon
 	private static final String APPLICATION_ONE = "application1";
 	private static final String APPLICATION_TWO = "application2";
 	
+	private static final String SERVICE_ONE = "service1";
+	private static final String SERVICE_TWO = "service2";
+
+	
 	@BeforeClass(alwaysRun = true)
 	protected void bootstrap() throws Exception {
 		super.bootstrap();
 	}
 	
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, enabled = true)
-	public void test() throws IOException, InterruptedException {
+	public void testTwoTenant() throws IOException, InterruptedException {
 
 		super.installManualTenantSharedProvisioningApplicationAndWait("ROLE_CLOUDADMINS", APPLICATION_ONE);
 		super.installManualTenantSharedProvisioningApplicationAndWait("ROLE_APPMANAGERS", APPLICATION_TWO);
@@ -34,6 +40,35 @@ public class TenantSharedProvisioningTest extends AbstractSharedProvisioningByon
 		AssertUtils.assertTrue("applications have ovelapping machines even though the isolation is app based", 
 				!applicationOneMachines.removeAll(applicationTwoMachines));
 		
+		super.uninstallApplicationAndWait(APPLICATION_ONE);
+		super.uninstallApplicationAndWait(APPLICATION_TWO);
+		
+	}
+	
+	
+	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, enabled = true)
+	public void testTwoServicesOnOneTenant() throws IOException, InterruptedException {
+		super.installManualTenantSharedProvisioningServiceAndWait("ROLE_CLOUDADMINS", SERVICE_ONE);
+		super.installManualTenantSharedProvisioningServiceAndWait("ROLE_CLOUDADMINS", SERVICE_TWO);
+		
+		Set<Machine> serviceOneMachines = ProcessingUnitUtils.getMachinesOfService(admin, ServiceUtils.getAbsolutePUName(APPLICATION_ONE, SERVICE_ONE));
+		Set<Machine> serviceTwoMachines = ProcessingUnitUtils.getMachinesOfService(admin, ServiceUtils.getAbsolutePUName(APPLICATION_ONE, SERVICE_TWO));
+		
+		AssertUtils.assertTrue("services should be deployed on the same machine since they belong to the same tenant", 
+				serviceOneMachines.equals(serviceTwoMachines));
+		
+		super.uninstallServiceAndWait(SERVICE_ONE);
+		super.uninstallServiceAndWait(SERVICE_TWO);
+		
+		
+	}
+	
+	@AfterMethod
+	public void cleanup() throws IOException, InterruptedException {
+		super.uninstallApplicationIfFound(APPLICATION_ONE);
+		super.uninstallApplicationIfFound(APPLICATION_TWO);
+		super.uninstallApplicationIfFound(SERVICE_ONE);
+		super.uninstallApplicationIfFound(SERVICE_TWO);
 	}
 	
 	

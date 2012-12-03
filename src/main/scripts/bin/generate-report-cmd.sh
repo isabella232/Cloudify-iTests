c2be5ebd146462db4249452d8e54fa26be47a41d
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-12-03 17:10:14 +0000

    fix script
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@134743 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/main/scripts/bin/generate-report-cmd.sh
+++ src/main/scripts/bin/generate-report-cmd.sh
@@ -14,7 +14,7 @@ TYPE=cloudify
 
 if [ ! -z `echo ${SUITE_NAME} | grep BigData` ] ; then TYPE=xap-premium; fi
 
-CLOUDIFY_HOME=/export/tgrid/sgtest3.0-cloudify/deploy/local-builds/${BUILD_NUMBER}/${TYPE}-${MAJOR_VERSION}-${MINOR_VERSION}
+CLOUDIFY_HOME=/export/tgrid/sgtest3.0-cloudify/deploy/local-builds/${BUILD_NUMBER}/gigaspaces-${TYPE}-${MAJOR_VERSION}-${MINOR_VERSION}
 
 mvn exec:java -Dexec.mainClass="framework.testng.report.TestsReportMerger" -Dexec.args="${SUITE_TYPE} ${BUILD_NUMBER} ${SUITE_NAME} ${MAJOR_VERSION} ${MINOR_VERSION}" -Dcloudify.home=${CLOUDIFY_HOME}
 

529d1f21a1d734656c285530de9af5d0aba4c767
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-12-04 13:17:03 +0000

    CLOUDIFY-1298 added test
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@134826 eb64e737-3616-4df0-8941-5ee2ae88103d

---
+++ src/main/java/test/cli/cloudify/DefaultSecurityFileLocationTest.java
@@ -0,0 +1,33 @@
+package test.cli.cloudify;
+
+import java.io.IOException;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import framework.tools.SGTestHelper;
+import framework.utils.LocalCloudBootstrapper;
+
+/**
+ * CLOUDIFY-1298
+ * @author elip
+ *
+ */
+public class DefaultSecurityFileLocationTest extends AbstractSecuredLocalCloudTest {
+	
+	@Override
+	@BeforeClass
+	public void bootstrap() throws IOException {
+		
+		// dont bootstrap, it is part of the test
+	}
+	
+	@Test(timeOut = DEFAULT_TEST_TIMEOUT, groups = "1", enabled = true)
+	public void testDefaultSecurityFileLocation() throws IOException {
+		LocalCloudBootstrapper bootstrapper = new LocalCloudBootstrapper();
+		bootstrapper.secured(true).securityFilePath(SGTestHelper.getBuildDir() + "/config/security/spring-security.xml");
+		bootstrapper.keystoreFilePath(getDefaultKeystoreFilePath()).keystorePassword(getDefaultKeystorePassword());
+		super.bootstrap(bootstrapper);
+	}
+
+}

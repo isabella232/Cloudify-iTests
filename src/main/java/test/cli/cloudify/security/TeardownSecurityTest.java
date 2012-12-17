5e813715b7ff91be182b14a91cd08387bc660f7b
nirb <nirb@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-12-17 16:32:44 +0000

    CLOUDIFY-1124 added local cloud teardown tests.
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@135683 eb64e737-3616-4df0-8941-5ee2ae88103d

---
+++ src/main/java/test/cli/cloudify/security/TeardownSecurityTest.java
@@ -0,0 +1,63 @@
+package test.cli.cloudify.security;
+
+import java.io.IOException;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import test.cli.cloudify.AbstractSecuredLocalCloudTest;
+import framework.utils.AssertUtils;
+import framework.utils.LocalCloudBootstrapper;
+
+public class TeardownSecurityTest extends AbstractSecuredLocalCloudTest{
+	
+	private LocalCloudBootstrapper bootstrapper;
+	
+	@BeforeClass
+	public void bootstrap() throws Exception {
+		bootstrapper = new LocalCloudBootstrapper();
+		bootstrapper.secured(true).securityFilePath(SecurityConstants.BUILD_SECURITY_FILE_PATH);
+		bootstrapper.keystoreFilePath(SecurityConstants.DEFAULT_KEYSTORE_FILE_PATH).keystorePassword(SecurityConstants.DEFAULT_KEYSTORE_PASSWORD);
+		super.bootstrap(bootstrapper);	
+	}
+	
+	@AfterClass(alwaysRun = true)
+	public void teardown() throws IOException, InterruptedException {
+		super.teardown();
+	}
+	
+	@Test(timeOut = DEFAULT_TEST_TIMEOUT, enabled = true)
+	public void testTeardown() throws Exception{
+				
+		teardownAndVerify(SecurityConstants.CLOUD_ADMIN_USER_PWD, SecurityConstants.CLOUD_ADMIN_USER_PWD, SecurityConstants.CLOUD_ADMIN_DESCRIPTIN, false);
+		
+		super.bootstrap(bootstrapper);		
+		teardownAndVerify(SecurityConstants.CLOUD_ADMIN_AND_APP_MANAGER_USER_PWD, SecurityConstants.CLOUD_ADMIN_AND_APP_MANAGER_USER_PWD, SecurityConstants.CLOUD_ADMIN_AND_APP_MANAGER_DESCRIPTION, false);
+		
+		super.bootstrap(bootstrapper);
+		teardownAndVerify(SecurityConstants.APP_MANAGER_USER_PWD, SecurityConstants.APP_MANAGER_USER_PWD, SecurityConstants.APP_MANAGER_DESCRIPTIN, true);
+
+		teardownAndVerify(SecurityConstants.APP_MANAGER_AND_VIEWER_USER_PWD, SecurityConstants.APP_MANAGER_AND_VIEWER_USER_PWD, SecurityConstants.APP_MANAGER_AND_VIEWER_DESCRIPTIN, true);
+		
+		teardownAndVerify(SecurityConstants.VIEWER_USER_PWD, SecurityConstants.VIEWER_USER_PWD, SecurityConstants.VIEWER_DESCRIPTIN, true);
+		
+		teardownAndVerify(SecurityConstants.NO_ROLE_USER_PWD, SecurityConstants.NO_ROLE_USER_PWD, SecurityConstants.NO_ROLE_DESCRIPTIN, true);
+
+	}
+
+	private void teardownAndVerify(String user, String password, String userDescription ,boolean isExpectedToFail) throws Exception{
+		bootstrapper.user(user).password(password);
+		String output = super.teardown(bootstrapper);
+		
+		if(isExpectedToFail){			
+			AssertUtils.assertTrue(userDescription + " succeeded to teardown", output.contains(TEARDOWN_ACCESS_DENIED_MESSAGE));
+		}
+		else{			
+			AssertUtils.assertTrue(userDescription + " failed to teardown", output.contains(TEARDOWN_SUCCESSFULL_MESSAGE));
+		}
+	}
+	
+	
+
+}

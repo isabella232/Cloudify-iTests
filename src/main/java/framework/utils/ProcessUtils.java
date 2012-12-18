6550ca01a0252a3d2af1513c72def72657de2f09
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-12-18 09:54:02 +0000

    CLOUDIFY-1124 ground work for possible new abstract class for local cloud tests
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@135709 eb64e737-3616-4df0-8941-5ee2ae88103d

---
+++ src/main/java/framework/utils/ProcessUtils.java
@@ -0,0 +1,113 @@
+package framework.utils;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.hyperic.sigar.ProcExe;
+import org.hyperic.sigar.ProcState;
+import org.hyperic.sigar.Sigar;
+import org.hyperic.sigar.SigarException;
+import org.hyperic.sigar.SigarPermissionDeniedException;
+
+import com.gigaspaces.internal.sigar.SigarHolder;
+
+public class ProcessUtils {
+	
+	public static boolean killProcessesByBaseName(final Set<String> baseNames) throws SigarException {
+		
+		final Map<Long, ProcessDetails> processTable = createProcessTable();
+		boolean failed = false;
+		final Set<Entry<Long, ProcessDetails>> entries = processTable.entrySet();
+		for (final Entry<Long, ProcessDetails> entry : entries) {
+			final long pid = entry.getKey();
+			final ProcessDetails procDetails = entry.getValue();
+			if (baseNames.contains(procDetails.baseName)) {
+				LogUtils.log("Found a leaking process: " + procDetails);
+				failed = true;
+				SetupUtils.killProcessesByIDs(new HashSet<String>(Arrays.asList("" + pid)));
+			}
+		}
+		return failed;
+	}
+	
+	public static boolean killJavaProcessesByArgs(final Set<String> processesArgs) throws SigarException {
+		
+		final Map<Long, ProcessDetails> processTable = createProcessTable();
+		boolean failed = false;
+		final Set<Entry<Long, ProcessDetails>> entries = processTable.entrySet();
+		for (final Entry<Long, ProcessDetails> entry : entries) {
+			final long pid = entry.getKey();
+			final ProcessDetails procDetails = entry.getValue();
+			if (procDetails.baseName.contains("java")) {
+				final String[] args = procDetails.args;
+				for (final String arg : args) {
+					if (processesArgs.contains(arg)) {
+						LogUtils.log("Found a leaking java process (" + arg + "): " + procDetails);
+						failed = true;
+						SetupUtils.killProcessesByIDs(new HashSet<String>(Arrays.asList("" + pid)));
+					}
+				}
+			}
+		}
+		return failed;
+
+	}
+	
+	public static Map<Long, ProcessDetails> createProcessTable()
+			throws SigarException {
+		final Sigar sigar = SigarHolder.getSigar();
+		final long[] allpids = sigar.getProcList();
+
+		final Map<Long, ProcessDetails> processDetailsByPid = new HashMap<Long, ProcessDetails>();
+
+		for (final long pid : allpids) {
+			try {
+				final ProcessDetails details = new ProcessDetails();
+				final ProcState state = sigar.getProcState(pid);
+				details.baseName = state.getName();
+				details.parentPid = state.getPpid();
+
+				final ProcExe exe = sigar.getProcExe(pid);
+				details.fullName = exe.getName();
+				details.args = sigar.getProcArgs(pid);
+
+				details.pid = pid;
+				processDetailsByPid.put(pid, details);
+
+			} catch (final SigarPermissionDeniedException e) {
+				// ignore
+			} catch (final SigarException e) {
+				// this often happens for security reasons, as procs from other
+				// users will fail on this.
+				LogUtils.log("Failed to read process details for pid: " + pid
+						+ ". Error was: " + e.getMessage());
+			}
+		}
+
+		return processDetailsByPid;
+	}
+	
+	
+	private static class ProcessDetails {
+
+		private long pid;
+		private String baseName;
+		private String fullName;
+		private String[] args;
+		private long parentPid;
+
+		@Override
+		public String toString() {
+			return "ProcessDetails [pid=" + pid + ", baseName=" + baseName
+					+ ", fullName=" + fullName + ", args="
+					+ Arrays.toString(args) + ", parentPid=" + parentPid + "]";
+		}
+
+	}
+	
+
+}
---
+++ src/main/java/test/cli/cloudify/NewAbstractLocalCloudTest.java
@@ -0,0 +1,117 @@
+package test.cli.cloudify;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.cloudifysource.dsl.internal.CloudifyConstants;
+import org.hyperic.sigar.SigarException;
+import org.openspaces.admin.Admin;
+import org.openspaces.admin.AdminFactory;
+import org.openspaces.admin.application.Application;
+import org.openspaces.admin.pu.ProcessingUnit;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import test.AbstractTestSupport;
+import test.cli.cloudify.CommandTestUtils.ProcessResult;
+import framework.utils.ApplicationInstaller;
+import framework.utils.AssertUtils;
+import framework.utils.LocalCloudBootstrapper;
+import framework.utils.ProcessUtils;
+import framework.utils.ServiceInstaller;
+
+public class NewAbstractLocalCloudTest extends AbstractTestSupport {
+	
+	private static final Set<String> suspectProcessNames = new HashSet<String>(
+			Arrays.asList("mongo", "mongod", "mongos", "nc"));
+
+	private static final Set<String> suspectJavaProcessNames = new HashSet<String>(
+			Arrays.asList(
+					"org.codehaus.groovy.tools.GroovyStarter",
+					"simplejavaprocess.jar",
+					"org.apache.catalina.startup.Bootstrap",
+					"org.apache.cassandra.thrift.CassandraDaemon"));
+	
+	private Admin admin;
+	
+	@BeforeSuite
+	public void bootstrap() throws Exception {
+		LocalCloudBootstrapper bootstrapper = new LocalCloudBootstrapper();
+		bootstrap(bootstrapper);		
+		admin = super.createAdminAndWaitForManagement();
+	}
+	
+    @AfterMethod(alwaysRun = true)
+    public void cleanup() throws SigarException, IOException, InterruptedException {
+    	
+    	uninstallApplications();
+    	uninstallService();
+    	
+    	// scan for leaked processes.
+    	boolean processesByBaseNameFound = ProcessUtils.killProcessesByBaseName(suspectJavaProcessNames);
+    	boolean javaProcessesByArgsFound = ProcessUtils.killJavaProcessesByArgs(suspectProcessNames);
+    	
+    	// Fail if leaked processes were found after uninstallation.
+    	AssertUtils.assertTrue("Leaked processes found!", !javaProcessesByArgsFound & !processesByBaseNameFound);
+    	
+    }
+	
+    private void uninstallApplications() throws IOException, InterruptedException {
+		
+    	ApplicationInstaller installer;
+    	for (Application app : admin.getApplications() ) {
+    		final String applicationName = app.getName();
+    		if (applicationName.equals(CloudifyConstants.MANAGEMENT_APPLICATION_NAME)) {
+    			return; // dont uninstall the management.
+    		}
+    		installer = new ApplicationInstaller(getRestUrl(), applicationName);
+    		installer.uninstall();
+    	}
+		
+	}
+
+	private void uninstallService() throws IOException, InterruptedException {
+
+		ServiceInstaller installer;
+    	for (ProcessingUnit pu : admin.getProcessingUnits() ) {
+    		final String serviceName = pu.getName();
+    		if (serviceName.equals("rest") || serviceName.equals("webui") || serviceName.equals("cloudifyManagementSpace")) {
+    			return; // dont uninstall management services.
+    		}
+    		installer = new ServiceInstaller(getRestUrl(), serviceName);
+    		installer.uninstall();
+    	}
+	}
+
+	@AfterSuite
+    public void teardown() throws IOException, InterruptedException {
+    	LocalCloudBootstrapper bootstrapper = new LocalCloudBootstrapper();
+    	if (admin != null) {
+    		admin.close();
+    	}
+    	bootstrapper.teardown();
+    }
+
+	protected ProcessResult bootstrap(LocalCloudBootstrapper bootstrapper) throws Exception {
+		ProcessResult bootstrapResult = bootstrapper.bootstrap();
+		if (bootstrapper.isBootstraped()) {
+			// only create admin if bootstrap was successful
+			admin = super.createAdminAndWaitForManagement();
+		}
+		return bootstrapResult;
+	}
+    
+    @Override
+    protected AdminFactory createAdminFactory() {
+		AdminFactory factory = new AdminFactory();
+		factory.addLocator("127.0.0.1:" + CloudifyConstants.DEFAULT_LOCALCLOUD_LUS_PORT);
+		return factory;
+    }
+    
+    protected String getRestUrl() {
+    	return "http://127.0.0.1:8100";
+    }
+}

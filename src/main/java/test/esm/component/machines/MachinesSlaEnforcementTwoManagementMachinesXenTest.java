6ad64975e536293b9435f29e232b3ec04969c916
boris <boris@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-12-26 15:24:12 +0000

    CLOUDIFY-1229 added test case to test.esm.component.machines from XEN to BYON
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@136146 eb64e737-3616-4df0-8941-5ee2ae88103d

---
+++ src/main/java/test/esm/component/machines/MachinesSlaEnforcementTwoManagementMachinesXenTest.java
@@ -0,0 +1,61 @@
+package test.gsm.component.machines.xen;
+
+import java.util.Collection;
+import java.util.concurrent.TimeUnit;
+
+import org.openspaces.admin.gsa.GridServiceAgent;
+import org.openspaces.admin.gsa.GridServiceManagerOptions;
+import org.openspaces.grid.gsm.machines.CapacityMachinesSlaPolicy;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import test.gsm.component.SlaEnforcementTestUtils;
+
+public class MachinesSlaEnforcementTwoManagementMachinesXenTest extends AbstractMachinesSlaEnforcementTest {
+    
+    @Test(timeOut = DEFAULT_TEST_TIMEOUT)
+    public void oneMachineTest() throws InterruptedException  {
+        
+        // the first GSAs is already started in BeginTest
+        Assert.assertEquals(admin.getGridServiceAgents().getSize(),1);
+        repetitiveAssertNumberOfGSAsAdded(1, OPERATION_TIMEOUT);
+        repetitiveAssertNumberOfGSAsRemoved(0, OPERATION_TIMEOUT);
+        
+        // Start a seconds machine and put a LUS on it
+        GridServiceAgent gsa2 = super.startNewVM(OPERATION_TIMEOUT, TimeUnit.MILLISECONDS);
+        gsa2.startGridService(new GridServiceManagerOptions());
+        Assert.assertEquals(admin.getGridServiceAgents().getSize(),2);
+        Assert.assertTrue(admin.getGridServiceManagers().waitFor(2,OPERATION_TIMEOUT,TimeUnit.MILLISECONDS));
+        
+        
+        // enforce numberOfMachines SLA
+        endpoint = createEndpoint(pu, machinesSlaEnforcement);
+        CapacityMachinesSlaPolicy sla = createSla(1);
+        
+        String firstApprovedAgentUid = null; 
+        for (int i = 0 ; i < 3 ; i++) {
+
+        	SlaEnforcementTestUtils.enforceSlaAndWait(admin, endpoint, sla, machineProvisioning);
+        	Collection<String> agentUids = endpoint.getAllocatedCapacity(sla).getAgentUids();
+            assertEquals(1, agentUids.size());
+        	String allocatedAgentUid = agentUids.iterator().next();
+        	if (firstApprovedAgentUid == null) {
+        		firstApprovedAgentUid = allocatedAgentUid;
+        	}
+        	else {
+        		Assert.assertEquals(
+        				firstApprovedAgentUid,
+        				allocatedAgentUid,
+        				"MachinesSla keeps changing its mind");
+        	}
+	        
+        	
+        }
+        
+        // make sure no extra machine was started nor terminated
+    	// even after SLA has reached steady state
+        repetitiveAssertNumberOfGSAsAdded(2, OPERATION_TIMEOUT);
+        repetitiveAssertNumberOfGSAsRemoved(0, OPERATION_TIMEOUT);
+        
+    }
+ }

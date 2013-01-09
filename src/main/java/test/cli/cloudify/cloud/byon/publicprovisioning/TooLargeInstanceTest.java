82851b1f4bcc444e9d2d9a4cad7a2df8b9ec2fa6
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2013-01-09 06:51:40 +0000

    CLOUDIFY-1414 added test that installs application, not just service
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@136870 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/main/java/test/cli/cloudify/cloud/byon/publicprovisioning/TooLargeInstanceTest.java
+++ src/main/java/test/cli/cloudify/cloud/byon/publicprovisioning/TooLargeInstanceTest.java
@@ -2,9 +2,12 @@ package test.cli.cloudify.cloud.byon.publicprovisioning;
 
 import java.io.IOException;
 
+import org.cloudifysource.dsl.internal.CloudifyErrorMessages;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import framework.utils.AssertUtils;
 
 /**
  * CLOUDIFY-1414
@@ -34,7 +37,25 @@ public class TooLargeInstanceTest extends AbstractPublicProvisioningByonCloudTes
 		int memoryForServicesOnManagementMachine = machineMemoryMB - reservedMemoryCapacityPerManagementMachineInMB;
 				
 		// this installation should fail because there is not machine that can accommodate the instance (memory wise)
-		installManualPublicProvisioningServiceAndWait(GROOVY_ONE, 1, memoryForServicesOnManagementMachine * 2, 0, DEFAULT_TEMPLATE_NAME, true, true);
+		String output = installManualPublicProvisioningServiceAndWait(GROOVY_ONE, 1, memoryForServicesOnManagementMachine * 2, 0, DEFAULT_TEMPLATE_NAME, true, true);
+		String errorKey = CloudifyErrorMessages.INSUFFICIENT_MEMORY.getName();
+		AssertUtils.assertTrue("Installation output does not contains the error " + errorKey, output.contains(errorKey));
+		
+		super.scanForLeakedAgentNodes();
+	}
+	
+	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, enabled = true)
+	public void testFaultyApplication() throws IOException, InterruptedException {
+			
+		int reservedMemoryCapacityPerManagementMachineInMB = getService().getCloud().getProvider().getReservedMemoryCapacityPerManagementMachineInMB();
+		int machineMemoryMB = getService().getCloud().getTemplates().get(DEFAULT_TEMPLATE_NAME).getMachineMemoryMB();
+		
+		int memoryForServicesOnManagementMachine = machineMemoryMB - reservedMemoryCapacityPerManagementMachineInMB;
+				
+		// this installation should fail because there is not machine that can accommodate the instance (memory wise)
+		String output = installManualPublicProvisioningApplicationAndWait(GROOVY_ONE, 1, memoryForServicesOnManagementMachine * 2, 0, DEFAULT_TEMPLATE_NAME, true, true);
+		String errorKey = CloudifyErrorMessages.INSUFFICIENT_MEMORY.getName();
+		AssertUtils.assertTrue("Installation output does not contains the error " + errorKey, output.contains(errorKey));
 		
 		super.scanForLeakedAgentNodes();
 	}

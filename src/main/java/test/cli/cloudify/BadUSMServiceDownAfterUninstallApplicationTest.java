86c09a08a23192859aff11727bc056f1fd6c06d2
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2013-01-20 10:00:05 +0000

    CLOUDIFY-214 fix test
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@137412 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/main/java/test/cli/cloudify/BadUSMServiceDownAfterUninstallApplicationTest.java
+++ src/main/java/test/cli/cloudify/BadUSMServiceDownAfterUninstallApplicationTest.java
@@ -2,37 +2,24 @@ package test.cli.cloudify;
 
 import java.io.IOException;
 
-import org.openspaces.admin.machine.Machine;
 import org.testng.annotations.Test;
 
-import org.cloudifysource.dsl.utils.ServiceUtils;
-
-import framework.utils.AssertUtils;
-import framework.utils.AssertUtils.RepetitiveConditionProvider;
+import framework.utils.ApplicationInstaller;
 
 public class BadUSMServiceDownAfterUninstallApplicationTest extends AbstractLocalCloudTest {
 
-    @SuppressWarnings("unused")
-    private Machine machineA;
+	private static final String APPLICATION_PATH = CommandTestUtils.getPath("src/main/resources/apps/USM/badUsmServices/simpleApplication");
 
-    @Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, groups = "1")
+	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 2, groups = "1")
     public void badUsmServiceDownTest() throws IOException, InterruptedException {
-        final String serviceDir = CommandTestUtils.getPath("src/main/resources/apps/USM/badUsmServices/simpleApplication");
-        String command = "connect " + restUrl + ";install-application --verbose " + serviceDir + ";exit";
-
-        CommandTestUtils.runCommandExpectedFail(command);
-        machineA = admin.getMachines().getMachines()[0];
-
-        command = "connect " + restUrl + ";" + "uninstall-application simple;exit";
-        runCommand(command);
-
-        RepetitiveConditionProvider condition = new RepetitiveConditionProvider() {
-            @Override
-            public boolean getCondition() {
-                return admin.getProcessingUnits().getProcessingUnit(ServiceUtils.getAbsolutePUName("simple", "simple")) == null;
-            }
-        };
-        AssertUtils.repetitiveAssertTrue(null, condition, 10000);
+        
+        ApplicationInstaller installer = new ApplicationInstaller(restUrl, "simple");
+        installer.recipePath(APPLICATION_PATH);
+        installer.expectToFail(true).timeoutInMinutes(2);
+        installer.install();
+        
+        installer.expectToFail(false);
+        installer.uninstall();
     }
 
 }

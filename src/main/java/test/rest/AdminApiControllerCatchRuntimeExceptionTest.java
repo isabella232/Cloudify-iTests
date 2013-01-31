24124e5d7d7f950eb8ca7397aa7caf523c516823
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2013-01-31 09:48:23 +0000

    fix test
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@137918 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/main/java/test/rest/AdminApiControllerCatchRuntimeExceptionTest.java
+++ src/main/java/test/rest/AdminApiControllerCatchRuntimeExceptionTest.java
@@ -1,40 +1,30 @@
 package test.rest;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.URLConnection;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 
+import org.apache.http.client.HttpResponseException;
 import org.testng.annotations.Test;
 
 import test.cli.cloudify.AbstractLocalCloudTest;
+import framework.utils.AssertUtils;
+import framework.utils.WebUtils;
 
 public class AdminApiControllerCatchRuntimeExceptionTest extends AbstractLocalCloudTest {
 
 	
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT * 5, groups = "1", enabled = true)
-	public void arrayIlegalIndexTest() throws IOException{		
+	public void arrayIlegalIndexTest() throws IOException, KeyManagementException, NoSuchAlgorithmException, URISyntaxException{		
 		String machinesUrl = restUrl + "/admin/Machines/Machines/10";
-		try{
 			// try to access a none existing url
-			getHtmlFromURL(machinesUrl);
-		}catch(Exception e){
-			assertTrue("cought exception should contain 404 error" , e.getMessage().contains("404"));
+		try {
+			WebUtils.getURLContent(new URL(machinesUrl));
+			AssertUtils.assertFail("Should be able to run commands on a non existing url = " + machinesUrl);
+		} catch (HttpResponseException e) {
+			AssertUtils.assertTrue("Http exception should be HttpNotFound", e.getMessage().contains("Not Found"));
 		}
 	}
-
-	private String getHtmlFromURL(String urlStr) throws IOException{
-		URL url = new URL(urlStr);
-		URLConnection conn = url.openConnection ();
-
-		// Get the response
-		BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		StringBuffer sb = new StringBuffer();
-		String line;
-		while ((line = rd.readLine()) != null)
-			sb.append(line);
-		
-		return sb.toString();
-		}
 }

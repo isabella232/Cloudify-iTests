f475026f19996eec8c02e3bcb57000cfeb153bb3
kobi <kobi@eb64e737-3616-4df0-8941-5ee2ae88103d>
2013-02-03 14:32:07 +0000

    updated windows ami
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@138035 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/main/java/test/cli/cloudify/cloud/services/ec2/Ec2WinCloudService.java
+++ src/main/java/test/cli/cloudify/cloud/services/ec2/Ec2WinCloudService.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Ec2WinCloudService extends Ec2CloudService {
 	
 	private static final String DEFAULT_EU_WEST_MEDIUM_WIN_AMI = "eu-west-1/ami-d2929fa6";
-	private static final String DEFAULT_US_EAST_MEDIUM_WIN_AMI = "us-east-1/ami-2542c04c";
+	private static final String DEFAULT_US_EAST_MEDIUM_WIN_AMI = "us-east-1/ami-d6929fa2";
 	
 	public Ec2WinCloudService() {
 		super("ec2-win");

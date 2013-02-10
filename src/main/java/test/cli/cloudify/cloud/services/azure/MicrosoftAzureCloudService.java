826f87924c3e0fd06dc54a9a70175c73f70277f4
kobi <kobi@eb64e737-3616-4df0-8941-5ee2ae88103d>
2013-02-10 14:47:37 +0000

    fixed pfx path
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/quality/frameworks/SGTest@138445 eb64e737-3616-4df0-8941-5ee2ae88103d

--- src/main/java/test/cli/cloudify/cloud/services/azure/MicrosoftAzureCloudService.java
+++ src/main/java/test/cli/cloudify/cloud/services/azure/MicrosoftAzureCloudService.java
@@ -29,7 +29,7 @@ public class MicrosoftAzureCloudService extends AbstractCloudService {
 
 	private final MicrosoftAzureRestClient azureClient;
 	private final String AZURE_SUBSCRIPTION_ID = certProperties.getProperty("AZURE_SUBSCRIPTION_ID");
-	private static final String PATH_TO_PFX = SGTestHelper.getSGTestRootDir() + "/src/main/resources/apps/cloudify/cloud/azure/azure-cert.pfx";
+	private static final String PATH_TO_PFX = SGTestHelper.getSGTestRootDir() + "/src/main/resources/credentials/cloud/azure/azure-cert.pfx";
 	private final String PFX_PASSWORD = certProperties.getProperty("PFX_PASSWORD");
 
 	private final String ADDRESS_SPACE = certProperties.getProperty("ADDRESS_SPACE");
@@ -263,7 +263,7 @@ public class MicrosoftAzureCloudService extends AbstractCloudService {
 	}
 
 	private void copyPrivateKeyToUploadFolder() throws IOException {
-		File pfxFilePath = new File(SGTestHelper.getSGTestRootDir() + "/src/main/resources/apps/cloudify/cloud/azure/azure-cert.pfx"); 	
+		File pfxFilePath = new File(SGTestHelper.getSGTestRootDir() + "/src/main/resources/credentials/cloud/azure/azure-cert.pfx");
 		File uploadDir = new File(getPathToCloudFolder() + "/upload");
 		FileUtils.copyFileToDirectory(pfxFilePath, uploadDir);
 	}

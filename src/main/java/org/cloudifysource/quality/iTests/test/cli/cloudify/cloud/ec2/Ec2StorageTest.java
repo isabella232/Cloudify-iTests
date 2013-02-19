36bfba372dfabefa618fc6c69c325a3ee0306dfe
adamlavie <adaml@adaml-PC.gspaces.com>
2013-02-19 16:05:44 +0200

    extracted storage test logics to an abstract class so that it could be reused by other cloud storage tests

--- src/main/java/org/cloudifysource/quality/iTests/test/cli/cloudify/cloud/ec2/Ec2StorageTest.java
+++ src/main/java/org/cloudifysource/quality/iTests/test/cli/cloudify/cloud/ec2/Ec2StorageTest.java
@@ -2,11 +2,9 @@ package org.cloudifysource.quality.iTests.test.cli.cloudify.cloud.ec2;
 
 import java.io.IOException;
 
-import org.cloudifysource.quality.iTests.framework.utils.LogUtils;
 import org.cloudifysource.quality.iTests.test.AbstractTestSupport;
 import org.cloudifysource.quality.iTests.test.cli.cloudify.CommandTestUtils;
-import org.cloudifysource.quality.iTests.test.cli.cloudify.CommandTestUtils.ProcessResult;
-import org.cloudifysource.quality.iTests.test.cli.cloudify.cloud.NewAbstractCloudTest;
+import org.cloudifysource.quality.iTests.test.cli.cloudify.cloud.AbstractStorageTest;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -19,14 +17,11 @@ import org.testng.annotations.Test;
  * @author adaml
  *
  */
-public class Ec2StorageTest extends NewAbstractCloudTest{
+public class Ec2StorageTest extends AbstractStorageTest{
 
 	private static final String SERVICE_NAME = "simpleStorage";
 	private static String SERVICE_PATH = CommandTestUtils.getPath("src/main/resources/apps/USM/usm/" + SERVICE_NAME);
-	
-	final static String WRITE_FILE_COMMAND_NAME = "writeToStorage";
-	final static String LIST_FILES_COMMAND_NAME = "listFilesInStorage";
-	final static String LIST_MOUNTED_DEVICES_COMMAND_NAME = "listMount";
+	private static String EXPECTED_LIST_MOUNT_OUTPUT = "/dev/xvdc on /home/ec2-user/storage type ext4 (rw)";
 	
 	@Override
 	protected String getCloudName() {
@@ -41,26 +36,12 @@ public class Ec2StorageTest extends NewAbstractCloudTest{
 	
 	@Test(timeOut = AbstractTestSupport.DEFAULT_TEST_TIMEOUT * 4, enabled = true)
 	public void testWriteToStorage() throws IOException, InterruptedException {
-		LogUtils.log("creating a new file called foo.txt in the storage volume. " 
-						+ "running 'touch ~/storage/foo.txt' command on remote machine.");
-		ProcessResult invokeCommandResult = invokeCommand(SERVICE_NAME, WRITE_FILE_COMMAND_NAME); 
-		assertTrue("create file command exited with an abnormal exit code. Output was " + invokeCommandResult.getOutput(),
-				invokeCommandResult.getExitcode() == 0);
-		LogUtils.log("listing all files inside mounted storage folder. running 'ls ~/storage/' command");
-		ProcessResult listFilesResult = invokeCommand(SERVICE_NAME, LIST_FILES_COMMAND_NAME); 
-		assertTrue("failed listing files. Output was " + listFilesResult.getOutput(), listFilesResult.getExitcode() == 0);
-		assertTrue("File was not created in storage volume. Output was " + listFilesResult.getOutput(),
-				listFilesResult.getOutput().contains("foo.txt"));
+		super.testWriteToStorage();
 	}
 	
 	@Test(timeOut = AbstractTestSupport.DEFAULT_TEST_TIMEOUT * 4, enabled = true)
 	protected void testStorageVolumeMounted() throws IOException, InterruptedException {
-		LogUtils.log("Listing all mounted devices. running command 'mount -l' on remote machine");
-		ProcessResult listMountedResult = invokeCommand(SERVICE_NAME, LIST_MOUNTED_DEVICES_COMMAND_NAME);
-		assertTrue("failed listing all mounted devices. Output was " + listMountedResult.getOutput(),
-				listMountedResult.getExitcode() == 0);
-		assertTrue("device is not in the mounted devices list: " + listMountedResult.getOutput(),
-				listMountedResult.getOutput().contains("/dev/xvdc on /home/ec2-user/storage type ext4 (rw)"));
+		super.testStorageVolumeMounted(EXPECTED_LIST_MOUNT_OUTPUT);
 	}
 	
 	@AfterClass(alwaysRun = true)

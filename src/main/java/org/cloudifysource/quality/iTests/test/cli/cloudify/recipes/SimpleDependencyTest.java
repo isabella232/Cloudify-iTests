05047003c827e6b9a5c32fd1e88d7a640961bd69
barakm <barakme@gmail.com>
2013-02-21 14:57:40 +0200

    Added logging to test due to sporadic error.

--- src/main/java/org/cloudifysource/quality/iTests/test/cli/cloudify/recipes/SimpleDependencyTest.java
+++ src/main/java/org/cloudifysource/quality/iTests/test/cli/cloudify/recipes/SimpleDependencyTest.java
@@ -4,21 +4,21 @@ import java.io.IOException;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.cloudifysource.quality.iTests.framework.utils.AssertUtils.RepetitiveConditionProvider;
+import org.cloudifysource.quality.iTests.framework.utils.LogUtils;
+import org.cloudifysource.quality.iTests.test.cli.cloudify.AbstractLocalCloudTest;
+import org.cloudifysource.quality.iTests.test.cli.cloudify.CommandTestUtils;
 import org.openspaces.admin.pu.ProcessingUnitInstance;
 import org.openspaces.admin.pu.events.ProcessingUnitInstanceAddedEventListener;
 import org.testng.annotations.Test;
 
-import org.cloudifysource.quality.iTests.test.cli.cloudify.AbstractLocalCloudTest;
-import org.cloudifysource.quality.iTests.test.cli.cloudify.CommandTestUtils;
-import org.cloudifysource.quality.iTests.framework.utils.AssertUtils.RepetitiveConditionProvider;
-
 /**
  * This test checks the application dependency mechanism.
  * It asserts "second" service starts after "first" service first instance.
- * 
+ *
  *  The first service takes 30 seconds to install and the second takes 10 seconds to install,
  *  so it makes it more likely to find a bug in dependency order.
- *   
+ *
  * @author itaif
  *
  */
@@ -39,9 +39,11 @@ public class SimpleDependencyTest extends AbstractLocalCloudTest{
 					ProcessingUnitInstance processingUnitInstance) {
 				String name = processingUnitInstance.getProcessingUnit().getName();
 				if (name.equals("simpleDependency.first")) {
+					LogUtils.log("simpleDependency.first instance added at: " + System.currentTimeMillis());
 					firstStarted.incrementAndGet();
 				}
 				else if (name.equals("simpleDependency.second")) {
+					LogUtils.log("simpleDependency.second instance added at: " + System.currentTimeMillis());
 					// This is where we check dependency order
 					if (firstStarted.get() == 0) {
 						testFailed.set(true);
@@ -55,7 +57,7 @@ public class SimpleDependencyTest extends AbstractLocalCloudTest{
 			// install application and wait for both services to be discovered
 			CommandTestUtils.runCommandExpectedFail("connect " + this.restUrl + ";install-application " + path + ";exit");
 			RepetitiveConditionProvider condition = new RepetitiveConditionProvider() {
-				
+
 				@Override
 				public boolean getCondition() {
 					return firstStarted.get() > 0 && secondStarted.get() > 0;
@@ -66,7 +68,7 @@ public class SimpleDependencyTest extends AbstractLocalCloudTest{
 		finally {
 			admin.getProcessingUnits().getProcessingUnitInstanceAdded().remove(eventListener);
 		}
-		
+
 		assertTrue("second service cannot be started before first service since they are dependant", !testFailed.get());
 	}
 }

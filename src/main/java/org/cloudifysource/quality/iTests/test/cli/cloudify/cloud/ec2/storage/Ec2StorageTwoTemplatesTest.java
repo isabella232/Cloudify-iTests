d3e5dd7ef9b6543f16f5c78166223b8b40b00e95
iliapolo <Eli.polonsky@gmail.com>
2013-03-14 09:21:42 +0200

    Fixed test usage of custom cloud config file. we should never delete the original grooyv file from the clouds folder

--- src/main/java/org/cloudifysource/quality/iTests/test/cli/cloudify/cloud/ec2/storage/Ec2StorageTwoTemplatesTest.java
+++ src/main/java/org/cloudifysource/quality/iTests/test/cli/cloudify/cloud/ec2/storage/Ec2StorageTwoTemplatesTest.java
@@ -4,6 +4,7 @@ import org.apache.commons.io.FileUtils;
 import org.cloudifysource.quality.iTests.framework.tools.SGTestHelper;
 import org.cloudifysource.quality.iTests.framework.utils.IOUtils;
 import org.cloudifysource.quality.iTests.test.cli.cloudify.cloud.AbstractStorageTest;
+import org.cloudifysource.quality.iTests.test.cli.cloudify.cloud.services.ec2.Ec2CloudService;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -16,25 +17,16 @@ import java.io.File;
  */
 public class Ec2StorageTwoTemplatesTest extends AbstractStorageTest {
 
-    private static final String ORIGINAL_EC2_DSL_FILE_PATH = SGTestHelper.getBuildDir() + "/clouds/ec2/ec2-cloud.groovy";
-    private static final String CUSTOM_EC2_DSL_FILE_PATH = SGTestHelper.getSGTestRootDir() + "/src/main/resources/apps/USM/usm/customStorageTemplateService/ec2-cloud.groovy";
-    private static String BACKUP_EC2_DSL_FILE_PATH = null;
+    private static final String CUSTOM_EC2_DSL_FILE_PATH = SGTestHelper.getCustomCloudConfigDir("ec2") + "/storage-two-templates/ec2-cloud.groovy";
 
     @BeforeMethod(alwaysRun = true)
     public void bootstrapAndInit() throws Exception{
-
-        BACKUP_EC2_DSL_FILE_PATH = IOUtils.backupFile(ORIGINAL_EC2_DSL_FILE_PATH);
-        IOUtils.replaceFile(ORIGINAL_EC2_DSL_FILE_PATH, CUSTOM_EC2_DSL_FILE_PATH);
-
         super.bootstrapAndInit();
     }
 
     @AfterMethod(alwaysRun = true)
     public void cleanup() throws Exception{
         super.cleanup();
-
-        FileUtils.deleteQuietly(new File(ORIGINAL_EC2_DSL_FILE_PATH));
-        IOUtils.replaceFileWithMove(new File(ORIGINAL_EC2_DSL_FILE_PATH), new File(BACKUP_EC2_DSL_FILE_PATH));
     }
 
     @Test(timeOut = DEFAULT_TEST_TIMEOUT * 4, enabled = true)
@@ -50,5 +42,12 @@ public class Ec2StorageTwoTemplatesTest extends AbstractStorageTest {
     @Override
     protected boolean isReusableCloud() {
         return false;
+    }
+
+    @Override
+    protected void customizeCloud() throws Exception {
+        super.customizeCloud();
+        File customCloudFile = new File(CUSTOM_EC2_DSL_FILE_PATH);
+        ((Ec2CloudService)getService()).setCloudGroovy(customCloudFile);
     }
 }

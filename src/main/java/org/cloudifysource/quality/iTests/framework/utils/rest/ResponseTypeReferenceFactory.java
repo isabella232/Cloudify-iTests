47bd9ba998a440632362c142be9e675cfcb105d3
Ahmad Mahagna <ahmad@gigaspaces.com>
2013-03-18 17:55:01 +0200

    CLOUDIFY-1599 add implementation for attributs methods

--- src/main/java/org/cloudifysource/quality/iTests/framework/utils/rest/ResponseTypeReferenceFactory.java
+++ src/main/java/org/cloudifysource/quality/iTests/framework/utils/rest/ResponseTypeReferenceFactory.java
@@ -1,9 +1,7 @@
 package org.cloudifysource.quality.iTests.framework.utils.rest;
 
-import org.cloudifysource.dsl.rest.response.DeleteServiceInstanceAttributeResponse;
 import org.cloudifysource.dsl.rest.response.Response;
 import org.cloudifysource.dsl.rest.response.ServiceDetails;
-import org.cloudifysource.dsl.rest.response.ServiceInstanceDetails;
 import org.codehaus.jackson.type.TypeReference;
 
 public class ResponseTypeReferenceFactory {
@@ -16,12 +14,5 @@ public class ResponseTypeReferenceFactory {
 		return new TypeReference<Response<Void>>() {};
 	}
 	
-	public static TypeReference<Response<DeleteServiceInstanceAttributeResponse>> newDeleteServiceInstanceAttributeResponse() {
-		return new TypeReference<Response<DeleteServiceInstanceAttributeResponse>>() {};
-	}
-	
-	public static TypeReference<Response<ServiceInstanceDetails>> newServiceInstanceDetailsResponse() {
-		return new TypeReference<Response<ServiceInstanceDetails>>() {};
-	}
 
 }

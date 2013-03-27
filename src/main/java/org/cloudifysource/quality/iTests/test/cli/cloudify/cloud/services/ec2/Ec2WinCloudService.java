21db248dde2e4f47871085ded9c1a84da2bcd01f
iliapolo <elip@gigaspaces.com>
2013-03-27 15:45:05 +0200

    CLOUDIFY-1617 use Windows Server R2 Images, R1 does not work

--- src/main/java/org/cloudifysource/quality/iTests/test/cli/cloudify/cloud/services/ec2/Ec2WinCloudService.java
+++ src/main/java/org/cloudifysource/quality/iTests/test/cli/cloudify/cloud/services/ec2/Ec2WinCloudService.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 public class Ec2WinCloudService extends Ec2CloudService {
 
-    private static final String DEFAULT_EU_WEST_MEDIUM_WIN_AMI = "eu-west-1/ami-843b31f0";
-    private static final String DEFAULT_US_EAST_MEDIUM_WIN_AMI = "us-east-1/ami-acb22ac5";
+    private static final String DEFAULT_EU_WEST_MEDIUM_WIN_AMI = "eu-west-1/ami-403a3034";
+    private static final String DEFAULT_US_EAST_MEDIUM_WIN_AMI = "us-east-1/ami-ceb129a7";
 
 	public Ec2WinCloudService() {
 		super("ec2-win");
@@ -25,7 +25,7 @@ public class Ec2WinCloudService extends Ec2CloudService {
 		if (getRegion().contains("eu")) {
 			getProperties().put("imageId", DEFAULT_EU_WEST_MEDIUM_WIN_AMI);
 		} else {
-			getProperties().put("locationId", "us-east-1");
+			getProperties().put("locationId", "us-east-1c");
 			getProperties().put("imageId", DEFAULT_US_EAST_MEDIUM_WIN_AMI);
 		}
 	}

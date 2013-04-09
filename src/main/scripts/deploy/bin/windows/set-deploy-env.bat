e8b2ac9ac8b16f5132010f1240136945e045d848
nirb <nirb@gigaspaces.com>
2013-04-09 11:57:06 +0300

    prevent cyclic copy

--- src/main/scripts/deploy/bin/windows/set-deploy-env.bat
+++ src/main/scripts/deploy/bin/windows/set-deploy-env.bat
@@ -2,7 +2,3 @@ set USER_HOME=C:\Users\ca
 set apache.port=8000
 set apache.home=%USER_HOME%\Apache Software Foundation\Apache2.2
 set RUNTIME_BUILD_LOCATION=%USER_HOME%\%BUILD_FOLDER%
-
-@echo copying build to user home dir, this is the build to be used in runtime
-xcopy %BUILD_LOCATION% %RUNTIME_BUILD_LOCATION% /e /i
-xcopy %SGTEST_HOME%\src\main\config\hsqldb.xml %RUNTIME_BUILD_LOCATION%\config\gsa /i /y

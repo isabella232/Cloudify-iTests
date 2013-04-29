820620923330a10c4b00abfcfbdbb3884acb8e45
barakm <barakme@gigaspaces.com>
2013-04-29 09:03:43 +0300

    CLOUDIFY-1695 test to check behavior of invoke custom command when no command is present.

---
+++ src/main/java/org/cloudifysource/quality/iTests/test/cli/cloudify/CustomCommandMissingTest.java
@@ -0,0 +1,42 @@
+package org.cloudifysource.quality.iTests.test.cli.cloudify;
+
+import iTests.framework.tools.SGTestHelper;
+
+import java.io.IOException;
+
+import org.cloudifysource.quality.iTests.framework.utils.LogUtils;
+import org.testng.annotations.Test;
+
+/**
+ * GS-1230
+ *
+ * @author nirb
+ *
+ */
+public class CustomCommandMissingTest extends AbstractLocalCloudTest {
+
+	private static final String SGTEST_ROOT_DIR = SGTestHelper.getSGTestRootDir().replace('\\', '/');
+
+	private static final String SERVICE_NAME = "groovy";
+	private static final String SERVICE_PATH = SGTEST_ROOT_DIR + "/src/main/resources/apps/USM/usm/simpleGroovy";
+
+	@Test(timeOut = DEFAULT_TEST_TIMEOUT, enabled = true)
+	public void install() throws IOException, InterruptedException {
+
+		installServiceAndWait(SERVICE_PATH, SERVICE_NAME, false);
+		try {
+			final String invokeResult =
+					CommandTestUtils.runCommandExpectedFail("connect " + restUrl + ";invoke " + SERVICE_NAME
+							+ " missingCommand");
+			org.testng.Assert.assertTrue(invokeResult.contains("No such Custom Command"));
+		} finally {
+			try {
+				this.uninstallService(SERVICE_NAME);
+			} catch (final Exception e) {
+				LogUtils.log("Failed to uninstall service: " + SERVICE_NAME, e);
+			}
+
+		}
+
+	}
+}

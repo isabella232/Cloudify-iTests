29979f55a1009efbdcc833d2de40391e2cb702c3
kobi <kobi@gigaspaces.com>
2013-05-20 16:32:39 +0300

    each suite/sub suite will now using a different webuitf.jar and iTests-framework.jar

--- src/main/scripts/bin/generate-report-cmd.sh
+++ src/main/scripts/bin/generate-report-cmd.sh
@@ -16,9 +16,9 @@ if [ ! -z `echo ${SUITE_NAME} | grep BigData` ] ; then TYPE=xap-premium; fi
 
 CLOUDIFY_HOME=/export/tgrid/sgtest3.0-cloudify/deploy/local-builds/${BUILD_NUMBER}/gigaspaces-${TYPE}-${MAJOR_VERSION}-${MINOR_VERSION}
 
-mvn exec:java -Dexec.mainClass="iTests.framework.testng.report.TestsReportMerger" -Dexec.args="${SUITE_NAME} ../${SUITE_NAME} ../${SUITE_NAME}" -Dcloudify.home=${CLOUDIFY_HOME}
+mvn exec:java -Dexec.mainClass="iTests.framework.testng.report.TestsReportMerger" -Dexec.args="${SUITE_NAME} ../${SUITE_NAME} ../${SUITE_NAME}" -Dcloudify.home=${CLOUDIFY_HOME} -DiTests.suiteName=${SUITE_NAME} -DiTests.suiteId=0
 
-mvn exec:java -Dexec.mainClass="iTests.framework.testng.report.wiki.WikiReporter" -Dexec.args="../${SUITE_NAME} ${SUITE_TYPE} ${BUILD_NUMBER} ${MAJOR_VERSION} ${MINOR_VERSION} ${BUILD_LOG_URL}" -Dcloudify.home=${CLOUDIFY_HOME} -Dmysql.host="192.168.9.44" -Dmysql.user="sa" -Dmysql.pass=""
+mvn exec:java -Dexec.mainClass="iTests.framework.testng.report.wiki.WikiReporter" -Dexec.args="../${SUITE_NAME} ${SUITE_TYPE} ${BUILD_NUMBER} ${MAJOR_VERSION} ${MINOR_VERSION} ${BUILD_LOG_URL}" -Dcloudify.home=${CLOUDIFY_HOME} -Dmysql.host="192.168.9.44" -Dmysql.user="sa" -Dmysql.pass="" -DiTests.suiteName=${SUITE_NAME} -DiTests.suiteId=0
 
 popd
 

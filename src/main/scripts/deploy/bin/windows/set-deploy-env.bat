693e51444312be66510c25721752b9f098e516b1
nirb <Qqry3090>
2013-06-04 14:17:36 +0300

    fix build location

--- src/main/scripts/deploy/bin/windows/set-deploy-env.bat
+++ src/main/scripts/deploy/bin/windows/set-deploy-env.bat
@@ -1,4 +1,4 @@
 set USER_HOME=C:\Users\ca
 set apache.port=8000
 set apache.home=%USER_HOME%\Apache Software Foundation\Apache2.2
-set RUNTIME_BUILD_LOCATION=%USER_HOME%\%BUILD_FOLDER%
+set RUNTIME_BUILD_LOCATION=%BUILD_LOCATION%

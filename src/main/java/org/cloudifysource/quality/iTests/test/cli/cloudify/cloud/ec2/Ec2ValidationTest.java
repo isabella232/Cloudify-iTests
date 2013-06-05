b217202d6233adb9b30e52f0c2387f38e19f308c
noak <noak@gigaspaces.com>
2013-06-05 14:20:40 +0300

    Fixed OpenstackValidationTest.java and created an abstract Test to be extended by OpenstackValidationTest.java and Ec2ValidationTest.java. Renamed the resources folder.

--- src/main/java/org/cloudifysource/quality/iTests/test/cli/cloudify/cloud/ec2/Ec2ValidationTest.java
+++ src/main/java/org/cloudifysource/quality/iTests/test/cli/cloudify/cloud/ec2/Ec2ValidationTest.java
@@ -1,126 +1,48 @@
 package org.cloudifysource.quality.iTests.test.cli.cloudify.cloud.ec2;
 
-import iTests.framework.tools.SGTestHelper;
-import iTests.framework.utils.IOUtils;
-
-import java.io.File;
-import java.io.IOException;
-
-import org.cloudifysource.quality.iTests.framework.utils.CloudBootstrapper;
-import org.cloudifysource.quality.iTests.test.cli.cloudify.cloud.NewAbstractCloudTest;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.cloudifysource.quality.iTests.test.cli.cloudify.cloud.DefaultBootstrapValidationTest;
 import org.testng.annotations.Test;
 
-public class Ec2ValidationTest  extends NewAbstractCloudTest {
+public class Ec2ValidationTest extends DefaultBootstrapValidationTest {
 
-	private String groovyFileName;
-	CloudBootstrapper bootstrapper;
-
-	
-	@BeforeClass
-	public void init() throws Exception {
-		bootstrapper = new CloudBootstrapper();
-		bootstrapper.scanForLeakedNodes(true);
-		bootstrapper.setBootstrapExpectedToFail(true);
-	}
-	
-	
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT, enabled = true)
 	public void wrongCredentialsTest() throws Exception {
-		try {
-			bootstrapper.scanForLeakedNodes(false);
-			groovyFileName = "wrongcredentials-ec2-cloud.groovy";
-			super.bootstrap(bootstrapper);
-			String bootstrapOutput = bootstrapper.getLastActionOutput();
-			assertTrue("The credentials are wrong but the wrong error was thrown. Reported error: " + bootstrapOutput,
-					bootstrapOutput.contains("HTTP/1.1 401 Unauthorized"));
-		} catch (Exception e) {
-			throw e;
-		} finally {
-			bootstrapper.scanForLeakedNodes(true);
-		}
+		super.wrongCredentialsTest("wrongcredentials-ec2-cloud.groovy");
 	}
 
 	
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT, enabled = true)
 	public void wrongImageTest() throws Exception {
-		groovyFileName = "wrongimage-ec2-cloud.groovy";
-		super.bootstrap(bootstrapper);
-		String bootstrapOutput = bootstrapper.getLastActionOutput();
-		assertTrue("The imageId is invalid but the wrong error was thrown. Reported error: " + bootstrapOutput,
-				bootstrapOutput.contains("Image") && bootstrapOutput.contains("is not valid for location"));
+		super.wrongImageTest("wrongimage-ec2-cloud.groovy");
 	}
 	
 	
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT, enabled = true)
 	public void wrongHardwareTest() throws Exception {
-		groovyFileName = "wronghardware-ec2-cloud.groovy";
-		super.bootstrap(bootstrapper);
-		String bootstrapOutput = bootstrapper.getLastActionOutput();
-		assertTrue("The hardwareId is invalid but the wrong error was thrown. Reported error: " + bootstrapOutput,
-				bootstrapOutput.contains("hardware") && bootstrapOutput.contains("is not valid for location"));
+		super.wrongHardwareTest("wronghardware-ec2-cloud.groovy");
 	}
 	
 	
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT, enabled = true)
 	public void wrongSecurityGroupTest() throws Exception {
-		groovyFileName = "wrongsecuritygroup-ec2-cloud.groovy";
-		super.bootstrap(bootstrapper);
-		String bootstrapOutput = bootstrapper.getLastActionOutput();
-		assertTrue("The security group name is wrong but the wrong error was thrown. "
-				+ "Reported error: " + bootstrapOutput,
-				bootstrapOutput.contains("Security group") && bootstrapOutput.contains("does not exist"));
+		super.wrongSecurityGroupTest("wrongsecuritygroup-ec2-cloud.groovy");
 	}
 	
 	
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT, enabled = true)
 	public void wrongKeyPairTest() throws Exception {
-		groovyFileName = "wrongkeypair-ec2-cloud.groovy";
-		super.bootstrap(bootstrapper);
-		String bootstrapOutput = bootstrapper.getLastActionOutput();
-		assertTrue("The key-pair name is wrong but the wrong error was thrown. Reported error: " + bootstrapOutput,
-				bootstrapOutput.contains("is invalid or in the wrong availability zone"));
+		super.wrongKeyPairTest("wrongkeypair-ec2-cloud.groovy");
 	}
 	
 	
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT, enabled = true)
 	public void wrongCloudifyUrlTest() throws Exception {
-		groovyFileName = "wrongcloudifyurl-ec2-cloud.groovy";
-		super.bootstrap(bootstrapper);
-		String bootstrapOutput = bootstrapper.getLastActionOutput();
-		assertTrue("The cloudify URL is wrong but the wrong error was thrown. Reported error: " + bootstrapOutput,
-				bootstrapOutput.contains("Invalid cloudify URL"));
+		super.wrongCloudifyUrlTest("wrongcloudifyurl-ec2-cloud.groovy");
 	}
-	
-
-	@AfterClass
-	public void teardown() throws Exception {
-		super.teardown();
-	}
-	
 
 	@Override
 	protected String getCloudName() {
 		return "ec2";
-	}
-	
-
-	@Override
-	protected boolean isReusableCloud() {
-		return false;
-	}
-	
-
-	protected void customizeCloud() throws IOException {
-		//replace the ec2-cloud.groovy with a wrong version, to fail the validation.
-		File standardGroovyFile = new File(getService().getPathToCloudFolder(), "ec2-cloud.groovy");
-		File wrongGroovyFile = new File(SGTestHelper.getSGTestRootDir() + "/src/main/resources/apps/cloudify/cloud/ec2/" + groovyFileName);
-		IOUtils.replaceFile(standardGroovyFile, wrongGroovyFile);
-		File newFile = new File(getService().getPathToCloudFolder(), groovyFileName);
-		if (newFile.exists()) {
-			newFile.renameTo(standardGroovyFile);
-		}
 	}
 
 }
--- src/main/java/org/cloudifysource/quality/iTests/test/cli/cloudify/cloud/openstack/OpenstackValidationTest.java
+++ src/main/java/org/cloudifysource/quality/iTests/test/cli/cloudify/cloud/openstack/OpenstackValidationTest.java
@@ -1,114 +1,39 @@
 package org.cloudifysource.quality.iTests.test.cli.cloudify.cloud.openstack;
 
-import java.io.File;
-import java.io.IOException;
-
-import iTests.framework.tools.SGTestHelper;
-import org.cloudifysource.quality.iTests.framework.utils.CloudBootstrapper;
-import iTests.framework.utils.IOUtils;
-import org.cloudifysource.quality.iTests.test.cli.cloudify.cloud.NewAbstractCloudTest;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.cloudifysource.quality.iTests.test.cli.cloudify.cloud.DefaultBootstrapValidationTest;
 import org.testng.annotations.Test;
 
-public class OpenstackValidationTest  extends NewAbstractCloudTest {
+public class OpenstackValidationTest extends DefaultBootstrapValidationTest {
 
-	private String groovyFileName;
-	CloudBootstrapper bootstrapper;
-
-	
-	@BeforeClass
-	public void init() throws Exception {
-		bootstrapper = new CloudBootstrapper();
-		bootstrapper.scanForLeakedNodes(false);
-	}
 
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT, enabled = true)
-	public void wrongImageTest() throws IOException, InterruptedException {
-		try {
-			groovyFileName = "wrongimage-openstack-cloud.groovy";
-			super.bootstrap(bootstrapper);
-			assertTrue("The imageId is invalid yet no error was thrown", false);
-		} catch (Throwable ae) {
-			assertTrue("The imageId is invalid but the wrong error was thrown. Reported error: " + ae.getMessage(),
-					ae.getMessage().contains("Invalid template configuration: imageId"));
-		}
+	public void wrongImageTest() throws Exception {
+		super.wrongImageTest("wrongimage-openstack-cloud.groovy");
 	}
 	
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT, enabled = true)
-	public void wrongHardwareTest() throws IOException, InterruptedException {
-		try {
-			groovyFileName = "wronghardware-openstack-cloud.groovy";
-			super.bootstrap(bootstrapper);
-			assertTrue("The hardwareId is invalid yet no error was thrown", false);
-		} catch (Throwable ae) {
-			assertTrue("The hardwareId is invalid but the wrong error was thrown. Reported error: " + ae.getMessage(),
-					ae.getMessage().contains("Invalid template configuration: hardwareId"));
-		}
+	public void wrongHardwareTest() throws Exception {
+		super.wrongHardwareTest("wronghardware-openstack-cloud.groovy");
 	}
 	
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT, enabled = true)
-	public void wrongSecurityGroupTest() throws IOException, InterruptedException {
-		try {
-			groovyFileName = "wrongsecuritygroup-openstack-cloud.groovy";
-			super.bootstrap(bootstrapper);
-			assertTrue("The security group name is wrong yet no error was thrown", false);
-		} catch (Throwable ae) {
-			assertTrue("The security group name is wrong but the wrong error was thrown. "
-					+ "Reported error: "	+ ae.getMessage(), 
-					ae.getMessage().contains("Invalid security groups configuration: Invalid security group name"));
-		}
+	public void wrongSecurityGroupTest() throws Exception {
+		super.wrongSecurityGroupTest("wrongsecuritygroup-openstack-cloud.groovy");
 	}
 	
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT, enabled = true)
-	public void wrongKeyPairTest() throws IOException, InterruptedException {
-		try {
-			groovyFileName = "wrongkeypair-openstack-cloud.groovy";
-			super.bootstrap(bootstrapper);
-			assertTrue("The key-pair name is wrong yet no error was thrown", false);
-		} catch (Throwable ae) {
-			assertTrue("The key-pair name is wrong but the wrong error was thrown. Reported error: " + ae.getMessage(),
-					ae.getMessage().contains("is invalid or in the wrong availability zone"));
-		}
+	public void wrongKeyPairTest() throws Exception {
+		super.wrongKeyPairTest("wrongkeypair-openstack-cloud.groovy");
 	}
 	
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT, enabled = true)
-	public void wrongCloudifyUrlTest() throws IOException, InterruptedException {
-		try {
-			groovyFileName = "wrongcloudifyurl-openstack-cloud.groovy";
-			super.bootstrap(bootstrapper);
-			assertTrue("The cloudify URL is wrong yet no error was thrown", false);
-		} catch (Throwable ae) {
-			assertTrue("The cloudify URL is wrong but the wrong error was thrown. Reported error: " + ae.getMessage(),
-					ae.getMessage().contains("Invalid cloudify URL"));
-		}
-	}
-
-	@AfterClass
-	public void teardown() throws Exception {
-		//JCloudsUtils.closeContext();		
-		//super.teardown();			
+	public void wrongCloudifyUrlTest() throws Exception {
+		super.wrongCloudifyUrlTest("wrongcloudifyurl-openstack-cloud.groovy");
 	}
 
 	@Override
 	protected String getCloudName() {
 		return "hp";
-	}
-
-	@Override
-	protected boolean isReusableCloud() {
-		return false;
-	}
-
-	protected void customizeCloud() throws IOException {
-		//replace the openstack-cloud.groovy with a wrong version, to fail the validation.
-		File standardGroovyFile = new File(getService().getPathToCloudFolder(), "hp-cloud.groovy");
-		File wrongproviderGroovyFile = new File(SGTestHelper.getSGTestRootDir() + "/src/main/resources/apps/cloudify/cloud/openstack/" + groovyFileName);
-		IOUtils.replaceFile(standardGroovyFile, wrongproviderGroovyFile);
-		File newFile = new File(getService().getPathToCloudFolder(), groovyFileName);
-		if (newFile.exists()) {
-			newFile.renameTo(standardGroovyFile);
-		}
 	}
 
 }

43d03566da6df472318517f78215d0070fc6e448
nirb <sdf>
2013-06-12 16:05:15 +0300

    temporarily not sending report

--- src/main/scripts/deploy/bin/windows/startSG.bat
+++ src/main/scripts/deploy/bin/windows/startSG.bat
@@ -58,21 +58,17 @@ call %SGTEST_HOME%\src\main\scripts\deploy\bin\windows\deploy_framework.bat
 @echo Running Suite %SUITE_NAME%: 
 call %SGTEST_HOME%\src\main\scripts\deploy\bin\windows\start-suite.bat %SUITE_NAME% %INCLUDE% %EXCLUDE% %EC2_REGION% %BUILD_TEST_DIR% %REVERSE_PROXY% %SUITE_TYPE% %MAVEN_PROJECTS_VERSION_XAP% %MAVEN_PROJECTS_VERSION_CLOUDIFY%
 
-@echo generating report... 
-call %SGTEST_HOME%\src\main\scripts\deploy\bin\windows\generate-report.bat %BUILD_NUMBER% %SUITE_NAME% %VERSION% %MILESTONE% %BUILD_LOG_URL% %SUITE_TYPE% %MAVEN_PROJECTS_VERSION_XAP% %MAVEN_PROJECTS_VERSION_CLOUDIFY%
+rem @echo generating report...
+rem call %SGTEST_HOME%\src\main\scripts\deploy\bin\windows\generate-report.bat %BUILD_NUMBER% %SUITE_NAME% %VERSION% %MILESTONE% %BUILD_LOG_URL% %SUITE_TYPE% %MAVEN_PROJECTS_VERSION_XAP% %MAVEN_PROJECTS_VERSION_CLOUDIFY%
 
-rem if %selenium.browser% == Chrome (
-rem 	taskkill /im chromedriver.exe /F
-rem 	taskkill /im chrome.exe /F
-rem )
-rem
-rem if %selenium.browser% == Firefox (
-rem 	taskkill /im firefox.exe /F
-rem )
+if %selenium.browser% == Chrome (
+	taskkill /im chromedriver.exe /F
+	taskkill /im chrome.exe /F
+)
+if %selenium.browser% == Firefox (
+	taskkill /im firefox.exe /F
+)
 
 @echo transferring reports to tgrid from %BUILD_TEST_DIR%
 xcopy %BUILD_LOCATION% W:\%BUILD_NUMBER%\%BUILD_FOLDER% /s /i /y
 xcopy %BUILD_TEST_DIR%\%SUITE_NAME% W:\%BUILD_NUMBER%\%SUITE_NAME% /s /i /y
-
-rem @echo cleaning local build folder - %BUILD_TEST_DIR%
-rem if exist %BUILD_TEST_DIR% rmdir %BUILD_TEST_DIR% /s /q

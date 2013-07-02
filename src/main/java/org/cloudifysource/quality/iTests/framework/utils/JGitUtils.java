9cb198de39c944ae04d2a60864dd267a1a6f3bd6
nirb <uy>
2013-07-02 10:29:27 +0300

    added ability to clone branch

--- src/main/java/org/cloudifysource/quality/iTests/framework/utils/JGitUtils.java
+++ src/main/java/org/cloudifysource/quality/iTests/framework/utils/JGitUtils.java
@@ -1,23 +1,40 @@
 package org.cloudifysource.quality.iTests.framework.utils;
 
+import org.eclipse.jgit.api.CheckoutCommand;
+import org.eclipse.jgit.api.CreateBranchCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.Repository;
 
 import java.io.File;
+import java.io.IOException;
 
 public class JGitUtils {
 
-    public static void clone(String localGitRepoPath, String repositoryUrl) {
-            if (!new File(localGitRepoPath).exists()) {
-                try {
-                    Git.cloneRepository()
-                            .setURI(repositoryUrl)
-                            .setDirectory(new File(localGitRepoPath))
+    public static void clone(String localGitRepoPath, String repositoryUrl, String branchName) throws IOException {
+        if (!new File(localGitRepoPath).exists()) {
+            try {
+                LogUtils.log("Cloning cloudify-recipes repo to " + localGitRepoPath);
+                Git.cloneRepository()
+                        .setURI(repositoryUrl)
+                        .setDirectory(new File(localGitRepoPath))
+                        .call();
+                if (!branchName.equalsIgnoreCase("master")) {
+                    LogUtils.log("Branch under test is : " + branchName);
+                    Git git = Git.open(new File(localGitRepoPath));
+                    LogUtils.log("Current branch is : " + git.getRepository().getFullBranch());
+                    CheckoutCommand checkout = git.checkout();
+                    LogUtils.log("Checking out to " + branchName);
+                    checkout.setCreateBranch(true)
+                            .setName(branchName)
+                            .setUpstreamMode(CreateBranchCommand.SetupUpstreamMode.TRACK).
+                            setStartPoint("origin/" + branchName)
                             .call();
-                } catch (Exception e) {
-                    throw new RuntimeException("Failed to clone "+repositoryUrl, e);
+                    LogUtils.log("Current branch is : " + git.getRepository().getFullBranch());
                 }
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to clone " + repositoryUrl, e);
             }
+        }
     }
 
     public static void commit(Repository localRepo, String msg) {

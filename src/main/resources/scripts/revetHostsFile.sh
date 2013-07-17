eefd0407bbe02dd7e3ae0d2c3456b30018714506
barakm <barakme@gigaspaces.com>
2013-07-17 13:18:51 +0300

    CLOUDIFY-1887 Added hosts file scripts

---
+++ src/main/resources/scripts/revetHostsFile.sh
@@ -0,0 +1,24 @@
+###########################################################
+## Script file to replace hosts file with a different file
+## using sudo commands. Executed from sgtest cloud suites
+## that modify hosts file on startup.
+##
+## Author: Barak Merimovich
+###########################################################
+
+hostsFile = /etc/hosts
+backupFile = $hostsFile.sgtest.backup
+
+echo Verifying passwordless sudo
+
+sudo -n ls > /dev/null 
+if [$? -ne 0]; then
+	echo "Current user is not a passwordless sudoer"
+	exit 1
+fi
+
+if [ -f $backupFile]; then
+	echo Found existing hosts backup file - reverting hosts
+	sudo cp $backupFile $hostsFile
+	sudo rm $backupFile
+fi

28f49225ea81f7d86fb3d41e430df9c7b9274229
boris <boris@gigaspaces.com>
2013-08-19 10:37:09 +0300

    CLOUDIFY-1984 added groovy-inner-error test case

--- src/main/java/org/cloudifysource/quality/iTests/test/cli/cloudify/SelfHealingDisabledTest.java
+++ src/main/java/org/cloudifysource/quality/iTests/test/cli/cloudify/SelfHealingDisabledTest.java
@@ -1,10 +1,7 @@
 package org.cloudifysource.quality.iTests.test.cli.cloudify;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.UnknownHostException;
-import java.util.concurrent.TimeUnit;
-
+import iTests.framework.utils.AssertUtils;
+import iTests.framework.utils.LogUtils;
 import org.cloudifysource.dsl.internal.CloudifyConstants.USMState;
 import org.cloudifysource.dsl.internal.DSLException;
 import org.cloudifysource.dsl.internal.ServiceReader;
@@ -16,17 +13,25 @@ import org.openspaces.admin.pu.ProcessingUnit;
 import org.openspaces.admin.pu.ProcessingUnitInstance;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.concurrent.TimeUnit;
+
 
 public class SelfHealingDisabledTest extends AbstractLocalCloudTest {
 
-	final private String RECIPE_DIR_PATH = CommandTestUtils
-			.getPath("src/main/resources/apps/USM/usm/failedGroovy");
+	final private String INNER_RECIPE_DIR_PATH = CommandTestUtils
+			.getPath("src/main/resources/apps/USM/usm/groovy-inner-error");
+
+    final private String RECIPE_DIR_PATH = CommandTestUtils
+            .getPath("src/main/resources/apps/USM/usm/failedGroovy");
 
 	@Test(timeOut = DEFAULT_TEST_TIMEOUT, groups = "1", enabled = true)
 	public void testNoSelfHealing() throws IOException, InterruptedException,
 			PackagingException, DSLException, RestException {
 
-		final String result = installService();
+		final String result = installService(false);
 		assertTrue("Could not find expected error message",
 				result.contains("This is a failure test"));
 
@@ -38,8 +43,58 @@ public class SelfHealingDisabledTest extends AbstractLocalCloudTest {
 		assertEquals(USMState.ERROR, state);
 
 		uninstallService();
-
 	}
+
+    @Test(timeOut = DEFAULT_TEST_TIMEOUT, groups = "1", enabled = true)
+    public void testInnerNoSelfHealing() throws IOException, InterruptedException,
+            PackagingException, DSLException, RestException {
+        long timeout = 120000;
+        String failMsg = "attribute changed to more than 01 - test failed because service restarted";
+        CommandTestUtils.runCommandUsingFile("connect " + restUrl + ";" +
+                "set-attributes -scope global '{\"GlobalAttribute\":\"0\"}';");
+
+        new Thread("New Thread") {
+            public void run(){
+                try {
+                    installService(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+        try {
+            repetitiveAssertTrue("Global attribute changed - service restarted", new AssertUtils.RepetitiveConditionProvider() {
+                @Override
+                public boolean getCondition() {
+                    String globalAttributeResult = null;
+                    boolean success = false;
+                    try {
+                        globalAttributeResult = runCommand("connect " + restUrl
+                                + ";" + "list-attributes -scope global;");
+                        success = globalAttributeResult.toLowerCase().contains("{\"globalattribute\":\"011\"}");
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        AssertFail("Failed to get global attribute");
+                    }
+                    finally {
+                        return success;
+                    }
+                }
+            }, timeout);
+
+            AssertFail(failMsg);
+        }
+        catch (AssertionError e){
+            if (e.getMessage().equals(failMsg)){
+                uninstallService();
+                AssertFail(failMsg);
+            }
+            uninstallService();
+            LogUtils.log("Caught an assertion error as expected - check passed");
+        }
+    }
 
 	private ProcessingUnitInstance findPUI(final ProcessingUnit pu)
 			throws UnknownHostException {
@@ -59,14 +114,15 @@ public class SelfHealingDisabledTest extends AbstractLocalCloudTest {
 		return pu;
 	}
 
-	private String installService() throws PackagingException, IOException,
+	private String installService(boolean inner) throws PackagingException, IOException,
 			InterruptedException, DSLException {
-		final File serviceDir = new File(RECIPE_DIR_PATH);
+        final File serviceDir;
+        serviceDir = inner ? new File(INNER_RECIPE_DIR_PATH) : new File(RECIPE_DIR_PATH);
+        String path = inner ? INNER_RECIPE_DIR_PATH : RECIPE_DIR_PATH;
 		ServiceReader.getServiceFromDirectory(serviceDir).getService();
-
 		return CommandTestUtils.runCommandExpectedFail("connect " + restUrl
 				+ ";install-service -disableSelfHealing --verbose "
-				+ RECIPE_DIR_PATH);
+				+ path);
 	}
 
 	private String uninstallService() throws PackagingException, IOException,

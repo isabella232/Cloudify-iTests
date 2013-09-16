1fe8d3eee2cc264bac0f503c788bd8dd45cc260f
nirb <bv>
2013-09-16 12:15:28 +0300

    removed debug flag (-X)

--- src/main/scripts/bin/sgtest-cmd.sh
+++ src/main/scripts/bin/sgtest-cmd.sh
@@ -27,7 +27,7 @@ echo clouds=$SUPPORTED_CLOUDS
 mkdir ${BUILD_DIR}/../${SUITE_NAME}
 cd ${BUILD_DIR}/../Cloudify-iTests
 
-mvn test -X -e -U -P tgrid-cloudify-iTests \
+mvn test -e -U -P tgrid-cloudify-iTests \
 -DiTests.cloud.enabled=false \
 -DiTests.buildNumber=${BUILD_NUMBER} \
 -DiTests.enableLogstash=${ENABLE_LOGSTASH} \

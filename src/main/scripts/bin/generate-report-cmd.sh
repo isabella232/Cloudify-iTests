35f32f2c7d68ff9371887fe01cd32351a2eea13e
iliapolo <elip@gigaspaces.com>
2013-10-23 15:26:17 +0300

    run maven exec with appropriate profile

--- src/main/scripts/bin/generate-report-cmd.sh
+++ src/main/scripts/bin/generate-report-cmd.sh
@@ -16,13 +16,16 @@ pushd ../local-builds/${BUILD_NUMBER}/Cloudify-iTests
 
 TYPE=cloudify
 
+PROFILE=tgrid-cloudify-iTests
+if [ ! -z `echo ${SUITE_NAME} | grep BigData` ] ; then PROFILE=tgrid-cloudify-iTests-bigdata; fi
+
 if [ ! -z `echo ${SUITE_NAME} | grep BigData` ] ; then TYPE=xap-premium; fi
 
 CLOUDIFY_HOME=/export/tgrid/sgtest3.0-cloudify/deploy/local-builds/${BUILD_NUMBER}/gigaspaces-${TYPE}-${MAJOR_VERSION}-${MINOR_VERSION}
 
-mvn exec:java -Dexec.mainClass="iTests.framework.testng.report.TestsReportMerger" -Dexec.args="${SUITE_NAME} ../${SUITE_NAME} ../${SUITE_NAME}" -Dcloudify.home=${CLOUDIFY_HOME} -Dmaven.repo.local=${MAVEN_REPO_LOCAL} -DgsVersion=${MAVEN_PROJECTS_VERSION_XAP} -DcloudifyVersion=${MAVEN_PROJECTS_VERSION_CLOUDIFY}
+mvn exec:java -P ${PROFILE} -Dexec.mainClass="iTests.framework.testng.report.TestsReportMerger" -Dexec.args="${SUITE_NAME} ../${SUITE_NAME} ../${SUITE_NAME}" -Dcloudify.home=${CLOUDIFY_HOME} -Dmaven.repo.local=${MAVEN_REPO_LOCAL} -DgsVersion=${MAVEN_PROJECTS_VERSION_XAP} -DcloudifyVersion=${MAVEN_PROJECTS_VERSION_CLOUDIFY}
 
-mvn exec:java -Dexec.mainClass="iTests.framework.testng.report.wiki.WikiReporter" -Dexec.args="../${SUITE_NAME} ${SUITE_TYPE} ${BUILD_NUMBER} ${MAJOR_VERSION} ${MINOR_VERSION} ${BUILD_LOG_URL}" -Dcloudify.home=${CLOUDIFY_HOME} -Dmysql.host="192.168.9.44" -Dmysql.user="sa" -Dmysql.pass="" -Dmaven.repo.local=${MAVEN_REPO_LOCAL} -DgsVersion=${MAVEN_PROJECTS_VERSION_XAP} -DcloudifyVersion=${MAVEN_PROJECTS_VERSION_CLOUDIFY} -DiTests.enableLogstash=${ENABLE_LOGSTASH}
+mvn exec:java -P ${PROFILE} -Dexec.mainClass="iTests.framework.testng.report.wiki.WikiReporter" -Dexec.args="../${SUITE_NAME} ${SUITE_TYPE} ${BUILD_NUMBER} ${MAJOR_VERSION} ${MINOR_VERSION} ${BUILD_LOG_URL}" -Dcloudify.home=${CLOUDIFY_HOME} -Dmysql.host="192.168.9.44" -Dmysql.user="sa" -Dmysql.pass="" -Dmaven.repo.local=${MAVEN_REPO_LOCAL} -DgsVersion=${MAVEN_PROJECTS_VERSION_XAP} -DcloudifyVersion=${MAVEN_PROJECTS_VERSION_CLOUDIFY} -DiTests.enableLogstash=${ENABLE_LOGSTASH}
 
 
 popd
--- src/main/scripts/deploy/bin/windows/generate-report.bat
+++ src/main/scripts/deploy/bin/windows/generate-report.bat
@@ -16,8 +16,13 @@ pushd %SGTEST_HOME%
 
 set CLOUDIFY_HOME=%BUILD_LOCATION%
 
-call mvn exec:java -Dexec.mainClass="iTests.framework.testng.report.TestsReportMerger" -Dexec.args="%SUITE_NAME% %SGTEST_HOME%\..\%SUITE_NAME% %SGTEST_HOME%\..\%SUITE_NAME%" -Dcloudify.home=%CLOUDIFY_HOME% -DgsVersion=%MAVEN_PROJECTS_VERSION_XAP% -DcloudifyVersion=%MAVEN_PROJECTS_VERSION_CLOUDIFY%
+set PROFILE=tgrid-cloudify-iTests
+if %SUITE_NAME% == BigData-LocalCloud (
+    set PROFILE=tgrid-cloudify-iTests-bigdata
+)
 
-call mvn exec:java -Dexec.mainClass="iTests.framework.testng.report.wiki.WikiReporter" -Dexec.args="%SGTEST_HOME%\..\%SUITE_NAME% %SUITE_TYPE% %BUILD_NUMBER% %MAJOR_VERSION% %MINOR_VERSION% %BUILD_LOG_URL%" -Dcloudify.home=%CLOUDIFY_HOME% -DgsVersion=%MAVEN_PROJECTS_VERSION_XAP% -DcloudifyVersion=%MAVEN_PROJECTS_VERSION_CLOUDIFY% -DiTests.enableLogstash=${ENABLE_LOGSTASH}
+call mvn exec:java -P %PROFILE% -Dexec.mainClass="iTests.framework.testng.report.TestsReportMerger" -Dexec.args="%SUITE_NAME% %SGTEST_HOME%\..\%SUITE_NAME% %SGTEST_HOME%\..\%SUITE_NAME%" -Dcloudify.home=%CLOUDIFY_HOME% -DgsVersion=%MAVEN_PROJECTS_VERSION_XAP% -DcloudifyVersion=%MAVEN_PROJECTS_VERSION_CLOUDIFY%
+
+call mvn exec:java -P %PROFILE% -Dexec.mainClass="iTests.framework.testng.report.wiki.WikiReporter" -Dexec.args="%SGTEST_HOME%\..\%SUITE_NAME% %SUITE_TYPE% %BUILD_NUMBER% %MAJOR_VERSION% %MINOR_VERSION% %BUILD_LOG_URL%" -Dcloudify.home=%CLOUDIFY_HOME% -DgsVersion=%MAVEN_PROJECTS_VERSION_XAP% -DcloudifyVersion=%MAVEN_PROJECTS_VERSION_CLOUDIFY% -DiTests.enableLogstash=${ENABLE_LOGSTASH}
 
 popd

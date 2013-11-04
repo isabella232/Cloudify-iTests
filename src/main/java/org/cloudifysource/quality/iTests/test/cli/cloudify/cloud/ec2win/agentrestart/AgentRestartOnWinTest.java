b391c85187b2efa03dbdfded4870acd603c5e7d7
adaml <adaml@gigaspaces.com>
2013-11-04 14:18:57 +0200

    added disclaimer

--- src/main/java/org/cloudifysource/quality/iTests/test/cli/cloudify/cloud/ec2win/agentrestart/AgentRestartOnWinTest.java
+++ src/main/java/org/cloudifysource/quality/iTests/test/cli/cloudify/cloud/ec2win/agentrestart/AgentRestartOnWinTest.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2013 GigaSpaces Technologies Ltd. All rights reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package org.cloudifysource.quality.iTests.test.cli.cloudify.cloud.ec2win.agentrestart;
 
 import iTests.framework.utils.AssertUtils;
@@ -21,9 +36,13 @@ import org.testng.annotations.Test;
 
 import com.j_spaces.kernel.PlatformVersion;
 
+/**
+ * tests agent maintenance mode on windows machines.
+ * 
+ * @author adaml
+ *
+ */
 public class AgentRestartOnWinTest extends AbstractAgentMaintenanceModeTest {
-
-	
 	private static final int INSTALL_SERVICE_TIMEOUT_MIN = 30;
 	private RestClient newClient;
 	private GSRestClient oldClient;

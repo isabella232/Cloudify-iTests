9a69b025c6004fe17c661c05e03437dd40154baa
Itai Frenkel <itaif@gigaspaces.com>
2013-11-11 10:20:15 +0200

    CLOUDIFY-2180 Added failover aware (reattach use case) component test

---
+++ src/main/java/org/cloudifysource/quality/iTests/test/esm/stateless/manual/memory/DedicatedStatelessManualFailoverAwareTest.java
@@ -0,0 +1,115 @@
+package org.cloudifysource.quality.iTests.test.esm.stateless.manual.memory;
+
+import iTests.framework.utils.DeploymentUtils;
+import iTests.framework.utils.GsmTestUtils;
+import iTests.framework.utils.LogUtils;
+import iTests.framework.utils.AssertUtils.RepetitiveConditionProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.cloudifysource.esc.driver.provisioning.byon.ByonProvisioningDriver;
+import org.cloudifysource.quality.iTests.test.cli.cloudify.CloudTestUtils;
+import org.cloudifysource.quality.iTests.test.esm.AbstractFromXenToByonGSMTest;
+import org.openspaces.admin.esm.ElasticServiceManager;
+import org.openspaces.admin.gsa.GridServiceAgent;
+import org.openspaces.admin.pu.ProcessingUnit;
+import org.openspaces.admin.pu.elastic.ElasticStatelessProcessingUnitDeployment;
+import org.openspaces.admin.pu.elastic.config.ManualCapacityScaleConfigurer;
+import org.openspaces.core.util.MemoryUnit;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.gigaspaces.log.LogEntries;
+import com.gigaspaces.log.LogEntryMatcher;
+import com.gigaspaces.log.LogEntryMatchers;
+
+public class DedicatedStatelessManualFailoverAwareTest extends AbstractFromXenToByonGSMTest {
+
+    private String EXPECTED_ESM_LOG_STATEMENT ="failover-aware-provisioning-driver";
+
+	@BeforeMethod
+    public void beforeTest() {
+        super.beforeTestInit();
+    }
+
+    @BeforeClass
+    protected void bootstrap() throws Exception {
+        super.bootstrapBeforeClass();
+    }
+
+    @AfterMethod
+    public void afterTest() {
+        super.afterTest();
+    }
+
+    @AfterClass(alwaysRun = true)
+    protected void teardownAfterClass() throws Exception {
+        super.teardownAfterClass();
+    }
+
+    /**
+     * CLOUDIFY-2180
+     * Tests that after failover the cloud driver #start() method receives MachineInfo of the failed machine.
+     */
+    @Test(timeOut = DEFAULT_TEST_TIMEOUT, enabled=true)
+    public void testFailedMachineDetails() throws Exception {
+    	repetitiveAssertNumberOfGSAsRemoved(0, OPERATION_TIMEOUT);
+    	
+        final File archive = DeploymentUtils.getArchive("simpleStatelessPu.jar");
+        final ElasticStatelessProcessingUnitDeployment deployment =
+                new ElasticStatelessProcessingUnitDeployment(archive)
+                .memoryCapacityPerContainer(1, MemoryUnit.GIGABYTES)
+        		.scale(
+	                new ManualCapacityScaleConfigurer()
+	                .memoryCapacity(1, MemoryUnit.GIGABYTES)
+	                .create());
+        
+        deployment.dedicatedMachineProvisioning(getMachineProvisioningConfig());
+
+        final ProcessingUnit pu = super.deploy(deployment);
+        GsmTestUtils.waitForScaleToCompleteIgnoreCpuSla(pu, 1, OPERATION_TIMEOUT);
+        repetitiveAssertNumberOfGSAsRemoved(0, OPERATION_TIMEOUT);
+    	        
+        // stop machine and check ESM log that it starts a machine that is aware of the failed machine.
+        stopByonMachine(getElasticMachineProvisioningCloudifyAdapter(), getAgent(pu), OPERATION_TIMEOUT, TimeUnit.MILLISECONDS);
+        repetitiveAssertNumberOfGSAsRemoved(1, OPERATION_TIMEOUT);
+        repetitiveAssertFailoverAware();
+        GsmTestUtils.waitForScaleToCompleteIgnoreCpuSla(pu, 1, OPERATION_TIMEOUT);
+        assertUndeployAndWait(pu);        
+    }
+
+    private GridServiceAgent getAgent(ProcessingUnit pu) {
+    	return pu.getInstances()[0].getGridServiceContainer().getGridServiceAgent();
+    }
+    
+    @Override
+    public void beforeBootstrap() throws IOException {
+    	CloudTestUtils.replaceCloudDriverImplementation(
+    			getService(),
+    			ByonProvisioningDriver.class.getName(), //old class
+    			"org.cloudifysource.quality.iTests.FailoverAwareByonProvisioningDriver", //new class
+    			"location-aware-provisioning-byon", "2.0-SNAPSHOT"); //jar
+    }
+
+    private void repetitiveAssertFailoverAware() {
+    	
+    	final ElasticServiceManager esm = admin.getElasticServiceManagers().waitForAtLeastOne(OPERATION_TIMEOUT, TimeUnit.MILLISECONDS);
+        LogUtils.log("Waiting for 1 "+ EXPECTED_ESM_LOG_STATEMENT  + " log entry before undeploying PU");
+        final LogEntryMatcher logMatcher = LogEntryMatchers.containsString(EXPECTED_ESM_LOG_STATEMENT);
+        repetitiveAssertTrue("Expected " + EXPECTED_ESM_LOG_STATEMENT +" log", new RepetitiveConditionProvider() {
+            
+            @Override
+            public boolean getCondition() {
+                final LogEntries logEntries = esm.logEntries(logMatcher);
+                final int count = logEntries.logEntries().size();
+                LogUtils.log("Exepcted at least one "+ EXPECTED_ESM_LOG_STATEMENT + " log entries. Actual :" + count);
+                return count > 0;
+            }
+        } , OPERATION_TIMEOUT);
+    }
+}

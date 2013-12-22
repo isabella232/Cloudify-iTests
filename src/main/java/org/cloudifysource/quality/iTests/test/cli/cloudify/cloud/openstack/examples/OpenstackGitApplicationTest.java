9d3751759de612d714dd4f0fc9f8313054917d32
kobi <kobkis@gmail.com>
2013-12-22 08:20:06 +0200

    Added test annotation (bug in TestNG)

--- src/main/java/org/cloudifysource/quality/iTests/test/cli/cloudify/cloud/openstack/examples/OpenstackGitApplicationTest.java
+++ src/main/java/org/cloudifysource/quality/iTests/test/cli/cloudify/cloud/openstack/examples/OpenstackGitApplicationTest.java
@@ -1,8 +1,9 @@
 package org.cloudifysource.quality.iTests.test.cli.cloudify.cloud.openstack.examples;
 
 import org.cloudifysource.quality.iTests.test.cli.cloudify.cloud.ec2.examples.Ec2GitApplicationsTest;
+import org.testng.annotations.Test;
 
-
+@Test
 public class OpenstackGitApplicationTest extends Ec2GitApplicationsTest {
 
     @Override

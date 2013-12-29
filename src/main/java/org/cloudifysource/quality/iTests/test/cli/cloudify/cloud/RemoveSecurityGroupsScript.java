f58e9d1e27651d106696a4ba4564dedc9a40d9ce
iliapolo <elip@gigaspaces.com>
2013-12-29 17:20:46 +0200

    fix compilation

--- src/main/java/org/cloudifysource/quality/iTests/test/cli/cloudify/cloud/RemoveSecurityGroupsScript.java
+++ src/main/java/org/cloudifysource/quality/iTests/test/cli/cloudify/cloud/RemoveSecurityGroupsScript.java
@@ -1,21 +1,21 @@
 package org.cloudifysource.quality.iTests.test.cli.cloudify.cloud;
 
-//import java.util.Set;
-//
-//import org.jclouds.ContextBuilder;
-//import org.jclouds.aws.ec2.AWSEC2AsyncClient;
-//import org.jclouds.aws.ec2.AWSEC2Client;
-//import org.jclouds.aws.ec2.services.AWSSecurityGroupClient;
-//import org.jclouds.compute.ComputeServiceContext;
-//import org.jclouds.ec2.domain.SecurityGroup;
-//import org.jclouds.rest.RestContext;
+import org.jclouds.ContextBuilder;
+import org.jclouds.aws.ec2.AWSEC2AsyncClient;
+import org.jclouds.aws.ec2.AWSEC2Client;
+import org.jclouds.aws.ec2.services.AWSSecurityGroupClient;
+import org.jclouds.compute.ComputeServiceContext;
+import org.jclouds.ec2.domain.SecurityGroup;
+import org.jclouds.rest.RestContext;
+
+import java.util.Set;
 
 public class RemoveSecurityGroupsScript {
 
 	
 	private static String region = "eu-west-1";
 	//add creds here.
-	private static String userName = "****";
+	private static String userName = "***";
 	private static String apiKey = "***";
 	private static String cloudProvider = "aws-ec2";
 
@@ -23,46 +23,48 @@ public class RemoveSecurityGroupsScript {
 		
 		boolean cleaned = false;
 		while (!cleaned) {
-//			cleaned = cleanSecurityGroups();
+			cleaned = cleanSecurityGroups();
 		}
 	}
 
-//	static boolean cleanSecurityGroups() {
-//		ContextBuilder contextBuilder;
-//		ComputeServiceContext buildView = null;
-//		try {
-//			contextBuilder = ContextBuilder.newBuilder(cloudProvider);
-//			contextBuilder.credentials(userName, apiKey);
-//			buildView = contextBuilder.buildView(ComputeServiceContext.class);
-//		
-//			RestContext<AWSEC2Client, AWSEC2AsyncClient> restContext = buildView.unwrap(RestContext.class);
-//			AWSEC2Client api = restContext.getApi();
-//			Set<String> configuredRegions = api.getConfiguredRegions();
-//			for (String string : configuredRegions) {
-//				System.out.println(string);
-//			}
-//
-//			AWSSecurityGroupClient securityGroupServices = api.getSecurityGroupServices();
-//			Set<SecurityGroup> describeSecurityGroupsInRegion = securityGroupServices.describeSecurityGroupsInRegion(region);
-//			for (SecurityGroup securityGroup : describeSecurityGroupsInRegion) {
-//				System.out.println(securityGroup.getName());
-//				if (securityGroup.getName().startsWith("jclouds#")) {
-//					try {
-//						securityGroupServices.deleteSecurityGroupInRegion(region, securityGroup.getName());
-//						
-//					} catch (Exception e) {
-//						// we ignore this exception. Usually means request limit was exceeded.
-//						System.out.println("Exception " + e.getMessage() );
-//					}
-//					Thread.sleep(500);
-//				}
-//			}
-//			return true;
-//		} catch (Exception e) {
-//			if (buildView != null) {
-//				buildView.close();
-//			}
-//			return false;
-//		}
-//	}
+	static boolean cleanSecurityGroups() {
+		ContextBuilder contextBuilder;
+		ComputeServiceContext buildView = null;
+		try {
+			contextBuilder = ContextBuilder.newBuilder(cloudProvider);
+			contextBuilder.credentials(userName, apiKey);
+			buildView = contextBuilder.buildView(ComputeServiceContext.class);
+
+			RestContext<AWSEC2Client, AWSEC2AsyncClient> restContext = buildView.unwrap();
+			AWSEC2Client api = restContext.getApi();
+			Set<String> configuredRegions = api.getConfiguredRegions();
+			for (String string : configuredRegions) {
+				System.out.println(string);
+			}
+
+			AWSSecurityGroupClient securityGroupServices = api.getSecurityGroupServices();
+			Set<SecurityGroup> describeSecurityGroupsInRegion = securityGroupServices.describeSecurityGroupsInRegion(region);
+			for (SecurityGroup securityGroup : describeSecurityGroupsInRegion) {
+				System.out.println(securityGroup.getName());
+				if (securityGroup.getName().startsWith("jclouds#")) {
+					try {
+                  String name = securityGroup.getName();
+                  System.out.println("deleting security group " + name);
+                  securityGroupServices.deleteSecurityGroupInRegion(region, name);
+
+					} catch (Exception e) {
+						// we ignore this exception. Usually means request limit was exceeded.
+						System.out.println("Exception " + e.getMessage() );
+					}
+					Thread.sleep(500);
+				}
+			}
+			return true;
+		} catch (Exception e) {
+			if (buildView != null) {
+				buildView.close();
+			}
+			return false;
+		}
+	}
 }

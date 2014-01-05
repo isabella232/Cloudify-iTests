ccea946badcd4c77fd3d2214e93835eaaa20aeda
yohanakh <yohana@yohanakhoury.com>
2014-01-05 14:11:59 +0200

    CLOUDIFY-2287 Updated SG-Validator recipe to wait 5 minutes

--- src/main/resources/apps/cloudify/recipes/sgvalidator/sg-validator-service.groovy
+++ src/main/resources/apps/cloudify/recipes/sgvalidator/sg-validator-service.groovy
@@ -25,32 +25,32 @@ service {
     customCommands ([
             "get-datagrid-instances" : {gridname,lookuplocators,waitfor->
                 Admin admin = new AdminFactory().useDaemonThreads(true).addLocators(lookuplocators).createAdmin();
-                admin.getProcessingUnits().waitFor(gridname,3, TimeUnit.MINUTES).waitFor(Integer.valueOf(waitfor));
+                admin.getProcessingUnits().waitFor(gridname,5, TimeUnit.MINUTES).waitFor(Integer.valueOf(waitfor));
                 return admin.getProcessingUnits().getProcessingUnit(gridname).getInstances().length;
             },
             "get-datagrid-partitions" : {gridname,lookuplocators,waitfor->
                 Admin admin = new AdminFactory().useDaemonThreads(true).addLocators(lookuplocators).createAdmin();
-                admin.getProcessingUnits().waitFor(gridname,3, TimeUnit.MINUTES).waitFor(Integer.valueOf(waitfor));
+                admin.getProcessingUnits().waitFor(gridname,5, TimeUnit.MINUTES).waitFor(Integer.valueOf(waitfor));
                 return admin.getProcessingUnits().getProcessingUnit(gridname).getPartitions().length;
             },
             "get-datagrid-backups" : {gridname,lookuplocators,waitfor->
                 Admin admin = new AdminFactory().useDaemonThreads(true).addLocators(lookuplocators).createAdmin();
-                admin.getProcessingUnits().waitFor(gridname,3, TimeUnit.MINUTES).waitFor(Integer.valueOf(waitfor));
+                admin.getProcessingUnits().waitFor(gridname,5, TimeUnit.MINUTES).waitFor(Integer.valueOf(waitfor));
                 return admin.getProcessingUnits().getProcessingUnit(gridname).getNumberOfBackups();
             },
             "get-datagrid-deploymentstatus" : {gridname,lookuplocators,waitfor->
                 Admin admin = new AdminFactory().useDaemonThreads(true).addLocators(lookuplocators).createAdmin();
-                admin.getProcessingUnits().waitFor(gridname,3, TimeUnit.MINUTES).waitFor(Integer.valueOf(waitfor));
+                admin.getProcessingUnits().waitFor(gridname,5, TimeUnit.MINUTES).waitFor(Integer.valueOf(waitfor));
                 return admin.getProcessingUnits().getProcessingUnit(gridname).getStatus().toString();
             },
             "get-datagrid-maxinstancespermachine" : {gridname,lookuplocators,waitfor->
                 Admin admin = new AdminFactory().useDaemonThreads(true).addLocators(lookuplocators).createAdmin();
-                admin.getProcessingUnits().waitFor(gridname,3, TimeUnit.MINUTES).waitFor(Integer.valueOf(waitfor));
+                admin.getProcessingUnits().waitFor(gridname,5, TimeUnit.MINUTES).waitFor(Integer.valueOf(waitfor));
                 return admin.getProcessingUnits().getProcessingUnit(gridname).getMaxInstancesPerMachine();
             },
             "get-datagrid-maxinstancespervm" : {gridname,lookuplocators,waitfor->
                 Admin admin = new AdminFactory().useDaemonThreads(true).addLocators(lookuplocators).createAdmin();
-                admin.getProcessingUnits().waitFor(gridname,3, TimeUnit.MINUTES).waitFor(Integer.valueOf(waitfor));
+                admin.getProcessingUnits().waitFor(gridname,5, TimeUnit.MINUTES).waitFor(Integer.valueOf(waitfor));
                 return admin.getProcessingUnits().getProcessingUnit(gridname).getMaxInstancesPerVM();
             }
     ])

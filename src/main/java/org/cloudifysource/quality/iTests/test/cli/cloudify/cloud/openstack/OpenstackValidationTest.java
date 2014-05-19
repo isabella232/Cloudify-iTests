d1cf1118b97d42d2c6bb6feeae8b788bfe341349
noak <noak@gigaspaces.com>
2014-05-19 13:47:07 +0300

    disabling suspicious test temporarily

--- src/main/java/org/cloudifysource/quality/iTests/test/cli/cloudify/cloud/openstack/OpenstackValidationTest.java
+++ src/main/java/org/cloudifysource/quality/iTests/test/cli/cloudify/cloud/openstack/OpenstackValidationTest.java
@@ -7,27 +7,27 @@ import org.testng.annotations.Test;
 public class OpenstackValidationTest extends DefaultBootstrapValidationTest {
 
 
-	@Test(timeOut = DEFAULT_TEST_TIMEOUT, enabled = true)
+	@Test(timeOut = DEFAULT_TEST_TIMEOUT, enabled = false)
 	public void wrongImageTest() throws Exception {
 		super.wrongImageTest("wrongimage-openstack-cloud.groovy");
 	}
 	
-	@Test(timeOut = DEFAULT_TEST_TIMEOUT, enabled = true)
+	@Test(timeOut = DEFAULT_TEST_TIMEOUT, enabled = false)
 	public void wrongHardwareTest() throws Exception {
 		super.wrongHardwareTest("wronghardware-openstack-cloud.groovy");
 	}
 	
-	@Test(timeOut = DEFAULT_TEST_TIMEOUT, enabled = true)
+	@Test(timeOut = DEFAULT_TEST_TIMEOUT, enabled = false)
 	public void wrongSecurityGroupTest() throws Exception {
 		super.wrongSecurityGroupTest("wrongsecuritygroup-openstack-cloud.groovy");
 	}
 	
-	@Test(timeOut = DEFAULT_TEST_TIMEOUT, enabled = true)
+	@Test(timeOut = DEFAULT_TEST_TIMEOUT, enabled = false)
 	public void wrongKeyPairTest() throws Exception {
 		super.wrongKeyPairTest("wrongkeypair-openstack-cloud.groovy");
 	}
 	
-	@Test(timeOut = DEFAULT_TEST_TIMEOUT, enabled = true)
+	@Test(timeOut = DEFAULT_TEST_TIMEOUT, enabled = false)
 	public void wrongCloudifyUrlTest() throws Exception {
 		super.wrongCloudifyUrlTest("wrongcloudifyurl-openstack-cloud.groovy");
 	}
